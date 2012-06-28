2756e74df78b8231125f8c08d6686f251985197b
florian <florian@florian-ThinkPad-X220>
2012-06-28 18:15:22 +0200

    bugfixes in the sat and cnfer module

--- src/modules/CNFerModule.cpp
+++ src/modules/CNFerModule.cpp
@@ -218,7 +218,7 @@ namespace smtrat
 		        		phis.push_back( currentFormula->pruneBack() );
 		        	}
                     unsigned formulasToAssertSizeBefore = _formulasToAssert.size();
-                    Formula::iterator lastPassedSubformula = mpPassedFormula->last();
+                    Formula::iterator lastPassedSubformula = ( mpPassedFormula->empty() ? mpPassedFormula->end() : mpPassedFormula->last() );
 		        	while( !phis.empty() )
 		        	{
 		        		Formula* currentSubformula = phis.back();
