498a66d9db614e8473bd7849e1f03873bb54bb84
Ulrich Loup <loup@cs.rwth-aachen.de>
2012-08-17 02:59:09 +0200

    Slight improvement in assumption utility.

--- utilities/record_assumptions.sh
+++ utilities/record_assumptions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "" > testrun_wrongresults.log
-for f in ~/Dokumente/InformatikStudium/Doktorarbeit/reals_diss/software/benchmarks/sets/smtlib/qf_nra/bugs/wrong_results/*; do echo -ne "Checking $f..." >> testrun_wrongresults.log && ./smtratSolver $f >> testrun_wrongresults.log 2>>testrun_wrongresults.log && mv -v assumptions_to_check.smt2 certification_$(basename $f).smt2 >>testrun_wrongresults.log; done
+for f in ~/Dokumente/InformatikStudium/Doktorarbeit/reals_diss/software/benchmarks/sets/smtlib/qf_nra/bugs/wrong_results/*; do echo -e "\r\nChecking $f...\r\n" >> testrun_wrongresults.log && ./smtratSolver $f >> testrun_wrongresults.log 2>>testrun_wrongresults.log && mv -v assumptions_to_check.smt2 certification_$(basename $f).smt2 >>testrun_wrongresults.log; done
 
