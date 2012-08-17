de0404bfa19b7958d2badc58f3442578f920f42f
Ulrich Loup <loup@cs.rwth-aachen.de>
2012-08-17 16:24:50 +0200

    Extension to certification utils.

--- utilities/record_assumptions.sh
+++ utilities/record_assumptions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "" > testrun_wrongresults.log
-for f in ~/Dokumente/InformatikStudium/Doktorarbeit/reals_diss/software/benchmarks/sets/smtlib/qf_nra/bugs/wrong_results/*; do echo -e "\r\nChecking $f...\r\n" >> testrun_wrongresults.log && ./smtratSolver $f >> testrun_wrongresults.log 2>>testrun_wrongresults.log && mv -v assumptions_to_check.smt2 certification_$(basename $f).smt2 >>testrun_wrongresults.log; done
+OUTPUTFILE=$1
+SOLVER=$2
+LOGFILE="$OUTPUTFILE.log"
+echo -ne "Recording certifications from $SOLVER in certification\_$OUTPUTFILE\_*.smt2."
+echo "" > $LOGFILE
+for f in ~/Dokumente/InformatikStudium/Doktorarbeit/reals_diss/software/benchmarks/sets/smtlib/qf_nra/bugs/wrong_results/*; do echo -ne "Checking $f..." >> $LOGFILE && ./smtratSolver $f >> $LOGFILE 2>>$LOGFILE && mv -v assumptions_to_check.smt2 certification\_$OUTPUTFILE\_$(basename $f) >>$LOGFILE; done
 
