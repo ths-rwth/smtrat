1192d85827c684c367bf5fd0584c301bcc65a2bc
Florian Corzilius <corzilius@cs.rwth-aachen.de>
2012-11-27 15:43:27 +0100

    added exitcodes

---
+++ src/solver/ExitCodes.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   ExitCodes.h
+ * Author: Sebastian Junges
+ *
+ * Created on November 19, 2012, 10:42 AM
+ */
+
+#ifndef EXITCODES_H
+#define	EXITCODES_H
+
+#define SMTRAT_EXIT_SUCCESS             0
+#define SMTRAT_EXIT_GENERALERROR        1
+#define SMTRAT_EXIT_USERABORT           2
+#define SMTRAT_EXIT_UNEXPECTEDANSWER    3
+#define SMTRAT_EXIT_NOSUCHFILE          4
+#define SMTRAT_EXIT_PARSERFAILURE       5
+
+
+#endif	/* EXITCODES_H */
+
