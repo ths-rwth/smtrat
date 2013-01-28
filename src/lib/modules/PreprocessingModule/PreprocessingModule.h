291dbebd8e3c385d42a5a53a72b7404adde9cbe6
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-01-28 17:35:02 +0100

    Refactored the addLinearDeductions (and extended meanwhile)

--- src/lib/modules/PreprocessingModule/PreprocessingModule.h
+++ src/lib/modules/PreprocessingModule/PreprocessingModule.h
@@ -41,8 +41,10 @@ namespace smtrat
         static const unsigned squared = i * i; 
     };
 
-    const int squaresArray[12]  = { 0*0 , 1*1, 2*2, 3*3, 4*4, 5*5, 6*6, 7*7, 8*8, 9*9, 10*10, 11*11 };
-    const std::vector<int> squares(squaresArray, squaresArray+12);
+    const int squaresArray[30]  = { 0*0 , 1*1, 2*2, 3*3, 4*4, 5*5, 6*6, 7*7, 8*8, 9*9, 
+                                    10*10, 11*11, 12*12, 13*13, 14*14, 15*15, 16*16, 17*17, 18*18, 19*19, 
+                                    20*20, 21*21, 22*22, 23*23, 24*24, 25*25, 26*26, 27*27, 28*28, 29*29};
+    const std::vector<int> squares(squaresArray, squaresArray+30);
     
     class PreprocessingModule : public Module
     {
@@ -73,6 +75,8 @@ namespace smtrat
             void RewritePotentialInequalities( Formula* formula, bool invert = false);
             void assignActivitiesToPassedFormula();
             void addLinearDeductions( Formula* formula );
+            void addUpperBounds( Formula* formula, const GiNaC::symtab& symbols, GiNaC::numeric boundary, bool strict ) const;
+            GiNaC::numeric determineUpperBounds( unsigned degree, const GiNaC::numeric& constPart ) const;
     };
 
 }    // namespace smtrat
