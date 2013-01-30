f37783035371c62cb39864aa845b33c0632e307b
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-01-30 12:40:33 +0100

    Added cachemodule

---
+++ src/lib/modules/CacheModule/CacheModule.cpp
@@ -0,0 +1,92 @@
+/*
+ * SMT-RAT - Satisfiability-Modulo-Theories Real Algebra Toolbox
+ * Copyright (C) 2012 Florian Corzilius, Ulrich Loup, Erika Abraham, Sebastian Junges
+ *
+ * This file is part of SMT-RAT.
+ *
+ * SMT-RAT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SMT-RAT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SMT-RAT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/**
+ * @file   VRWModule.cpp
+ * @author: Sebastian Junges
+ *
+ *
+ */
+
+#include "CacheModule.h"
+#include "../../../solver/ExitCodes.h"
+
+
+namespace smtrat {
+CacheModule::CacheModule( ModuleType _type, const Formula* const _formula, RuntimeSettings* _settings, Manager* const _tsManager )
+    :
+    Module( _type, _formula, _tsManager )
+    {
+
+    }
+
+    /**
+     * Destructor:
+     */
+    CacheModule::~CacheModule(){}
+
+    /**
+     * Methods:
+     */
+
+    /**
+     * Adds a constraint to this module.
+     *
+     * @param _constraint The constraint to add to the already added constraints.
+     *
+     * @return true
+     */
+    bool CacheModule::assertSubformula( Formula::const_iterator _subformula )
+    {
+        Module::assertSubformula( _subformula );
+        addReceivedSubformulaToPassedFormula(_subformula);
+        return true;
+    }
+
+    /**
+     * Checks the so far received constraints for consistency.
+     */
+    Answer CacheModule::isConsistent()
+    {
+       
+        
+        Answer ans = runBackends();
+        if( ans == False )
+        {
+            getInfeasibleSubsets();
+        }
+        mSolverState = ans;
+        return ans;
+    }
+
+    /**
+     * Removes a everything related to a sub formula of the received formula.
+     *
+     * @param _subformula The sub formula of the received formula to remove.
+     */
+    void CacheModule::removeSubformula( Formula::const_iterator _subformula )
+    {
+        Module::removeSubformula( _subformula );
+    }
+    
+
+}
---
+++ src/lib/modules/CacheModule/CacheModule.h
@@ -0,0 +1,65 @@
+/*
+ * SMT-RAT - Satisfiability-Modulo-Theories Real Algebra Toolbox
+ * Copyright (C) 2012 Florian Corzilius, Ulrich Loup, Erika Abraham, Sebastian Junges
+ *
+ * This file is part of SMT-RAT.
+ *
+ * SMT-RAT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SMT-RAT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SMT-RAT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/** 
+ * @file   PreprocessingModule.h
+ *         Created on January 10, 2013, 9:59 PM
+ * @author: Sebastian Junges
+ *
+ * 
+ */
+
+#pragma once
+
+#include "../../Module.h"
+
+namespace smtrat
+{
+
+    class CacheModule : public Module
+    {
+        protected:
+            
+        public:
+            /**
+             * Constructors:
+             */
+            CacheModule( ModuleType, const Formula* const,  RuntimeSettings*, Manager* const _tsManager );
+
+            /**
+             * Destructor:
+             */
+            virtual ~CacheModule();
+
+            /**
+             * Methods:
+             */
+
+            // Interfaces.
+            bool assertSubformula( Formula::const_iterator );
+            Answer isConsistent();
+            void removeSubformula( Formula::const_iterator );
+            
+    };
+
+}    // namespace smtrat
+
