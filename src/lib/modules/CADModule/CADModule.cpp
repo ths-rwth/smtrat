8dc6298810a6573f26d315bc72e3f2e304fae5af
Ulrich Loup <loup@cs.rwth-aachen.de>
2013-02-05 13:07:31 +0100

    Fixed bug in CADModule in which the MIS was not constructed correctly in bounds-mode. Switched theory propagation off.

--- src/lib/modules/CADModule/CADModule.cpp
+++ src/lib/modules/CADModule/CADModule.cpp
@@ -59,7 +59,7 @@ using namespace std;
 //#define SMTRAT_CAD_DISABLEEQUATIONDETECT_SETTING
 //#define SMTRAT_CAD_GENERIC_SETTING
 //#define SMTRAT_CAD_DISABLE_SMT
-//#define SMTRAT_CAD_DISABLE_THEORYPROPAGATION
+#define SMTRAT_CAD_DISABLE_THEORYPROPAGATION
 //#define SMTRAT_CAD_DISABLE_MIS
 
 #ifdef SMTRAT_CAD_DISABLE_SMT
@@ -96,9 +96,11 @@ namespace smtrat
                 #else
                 setting = GiNaCRA::CADSettings::getSettings( GiNaCRA::EQUATIONDETECT_CADSETTING ); // standard
                 #endif
-                setting.warmRestart = true;
+                setting.computeConflictGraph    = true;
+                setting.numberOfDeductions      = 1;
+                setting.warmRestart             = true;
                 setting.simplifyByFactorization = true;
-                setting.simplifyByRootcounting= true;
+                setting.simplifyByRootcounting  = true;
             #endif
         #endif
 
@@ -221,8 +223,16 @@ namespace smtrat
             cout << "conflict graph: " << endl << conflictGraph << endl << endl;
             #endif
             vec_set_const_pFormula infeasibleSubsets = extractMinimalInfeasibleSubsets_GreedyHeuristics( conflictGraph );
+            #ifdef CAD_USE_VARIABLE_BOUNDS
+            set<Formula*> boundConstraints = mVariableBounds.getOriginsOfBounds();
+            #endif
             for( vec_set_const_pFormula::const_iterator i = infeasibleSubsets.begin(); i != infeasibleSubsets.end(); ++i )
+            {
                 mInfeasibleSubsets.push_back( *i );
+                #ifdef CAD_USE_VARIABLE_BOUNDS
+                mInfeasibleSubsets.back().insert( boundConstraints.begin(), boundConstraints.end() );
+                #endif
+            }
             #endif
             #ifdef MODULE_VERBOSE
             cout << endl << "#Samples: " << mCAD.samples().size() << endl;
