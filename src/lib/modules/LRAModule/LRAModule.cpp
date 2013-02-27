b1be50673ce7972e0b46ecebf26177d3b24b4381
Florian Corzilius <corzilius@cs.rwth-aachen.de>
2013-02-27 14:29:21 +0100

    corrected some debugging problem and put everything within the compiler flag of the branch and bound extension of the lramodule, which is currently deactivated

--- src/lib/modules/LRAModule/LRAModule.cpp
+++ src/lib/modules/LRAModule/LRAModule.cpp
@@ -119,7 +119,7 @@ namespace smtrat
     }
 
     /**
-     * 
+     *
      * Adds a sub-formula/constraint to the so far received sub-formula/constraints.
      *
      * @param _subformula The position of the constraint within the received constraints.
@@ -510,8 +510,6 @@ namespace smtrat
         learnRefinements();
         return foundAnswer( True );
     }
-    
-    }    
 
     /**
      * Updates the current assignment into the model. Note, that this is a unique but symbolic assignment still containing delta as a variable.
@@ -1319,23 +1317,25 @@ namespace smtrat
             #endif
         }
     }
-    
+
+    #ifdef LRA_BRANCH_AND_BOUND
     /**
      * Checks Integer-Consistency via
      * Branch and Bound.
      */
-    
     Answer LRAModule::isIntegerConsistent()
     {
-    Answer RealConsistency = isConsistent();
-    if (RealConsistency == Unknown ) return foundAnswer( Unknown );
-    else if (RealConsistency == False ) return foundAnswer( False );  
+        Answer RealConsistency = isConsistent();
+        if (RealConsistency == Unknown ) return foundAnswer( Unknown );
+        else if (RealConsistency == False ) return foundAnswer( False );
+        else return foundAnswer( Unknown );
     }
     /* To-Do: Check, whether the solution is already integer-valued.
      * Yes: return true.
      * No: Fix the non-integer values recursively.
-     * /
-    
+     */
+    #endif
+
 
     /**
      * Prints all linear constraints.
