2d63816ba02f21ae74f9ccbfdb1fe1ea44647e18
Florian Corzilius <corzilius@cs.rwth-aachen.de>
2013-03-12 14:58:42 +0100

    removed compileinfo.cpp

--- src/lib/CompileInfo.cpp
+++
@@ -1,9 +0,0 @@
-#include "CompileInfo.h"
-
-namespace smtrat 
-{
-const std::string CompileInfo::SystemName = "Linux";
-const std::string CompileInfo::SystemVersion = "3.6.9-1-ARCH";
-const std::string CompileInfo::BuildType = "Debug";
-const std::string CompileInfo::CXXCompiler = "4.7.2";
-}
