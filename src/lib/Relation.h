a3323d6c6629af6fffa6bbb25b56e3686be701f6
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2014-03-13 17:52:10 +0100

    is strict added to relations

--- src/lib/Relation.h
+++ src/lib/Relation.h
@@ -10,5 +10,8 @@ namespace smtrat
 {
 enum class Relation : unsigned { EQ = 0, NEQ = 1, LESS = 2, GREATER = 3, LEQ = 4, GEQ = 5 };
 
-bool relationIsStrict(Relation r);
+inline bool relationIsStrict(Relation r)
+{
+	return r == Relation::LESS || r == Relation::GREATER || r == Relation::NEQ;
+}
 }
