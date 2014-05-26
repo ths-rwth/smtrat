96126fe048c797644e32faf910283722be515455
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-05-26 12:45:10 +0200

    Added VariableWrapper.

--- src/solver/newparser/ParserUtils.h
+++ src/solver/newparser/ParserUtils.h
@@ -20,6 +20,27 @@ namespace px = boost::phoenix;
 namespace smtrat {
 namespace parser {
 
+class VariableWrapper {
+private:
+	carl::Variable* var;
+public:
+	VariableWrapper() : var(nullptr) {
+		//std::cout << this << " wrapping nullptr" << std::endl;
+	}
+	VariableWrapper(const VariableWrapper& v) : var(nullptr) {
+		if (v.var != nullptr) this->var = new carl::Variable(*(v.var));
+		//std::cout << this << " copying " << &v << std::endl;
+	}
+	VariableWrapper(const carl::Variable& v) : var(new carl::Variable(v)) {
+		//std::cout << this << " wrapping " << v << std::endl;
+	}
+	~VariableWrapper() { delete var; }
+	operator carl::Variable() const {
+		//std::cout << this << " extracting " << *var << std::endl;
+		return carl::Variable(*var);
+	}
+};
+
 struct SuccessHandler {	
 	template<typename Parser, typename Rule, typename Entity>
 	void operator()(Parser& p, const Rule& rule, const Entity& entity) const {
