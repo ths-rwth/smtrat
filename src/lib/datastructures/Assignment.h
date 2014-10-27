dded75a3d1483417b022e771f8454f0781e1c5fc
Florian Corzilius <flo@localhost>
2014-10-27 17:25:41 +0100

    extended assignment by the SortValue and UFModel class; some progress in the description of the external interfaces of smtrat in the manual

--- src/lib/datastructures/Assignment.h
+++ src/lib/datastructures/Assignment.h
@@ -1,6 +1,30 @@
+/*
+ *  SMT-RAT - Satisfiability-Modulo-Theories Real Algebra Toolbox
+ * Copyright (C) 2012 Florian Corzilius, Ulrich Loup, Erika Abraham, Sebastian Junges
+ *
+ * This file is part of SMT-RAT.
+ *
+ * SMT-RAT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SMT-RAT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SMT-RAT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 /**
  * @file Assignment.h
  * @author Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
+ * @author Florian Corzilius <corzilius@cs.rwth-aachen.de>
+ * @since 2014-01-14
+ * @version 2014-10-27
+
  */
 
 #pragma once
@@ -10,33 +34,37 @@
 #include "../Formula.h"
 #include "vs/SqrtEx.h"
 #include "carl/core/RealAlgebraicNumber.h"
+#include "SortValue.h"
+#include "UFModel.h"
 
-namespace smtrat {
+namespace smtrat
+{
 
     /**
      * This class represents some value that is assigned to some variable.
      * It is implemented as subclass of a boost::variant.
      * Possible value types are bool, vs::SqrtEx and carl::RealAlgebraicNumberPtr.
      */
-    class Assignment: public boost::variant<bool, vs::SqrtEx, carl::RealAlgebraicNumberPtr<smtrat::Rational>> {
+    class Assignment: public boost::variant<bool, vs::SqrtEx, carl::RealAlgebraicNumberPtr<smtrat::Rational>, SortValue, UFModel>
+    {
         /**
          * Base type we are deriving from.
          */
-        typedef boost::variant<bool, vs::SqrtEx, carl::RealAlgebraicNumberPtr<smtrat::Rational>> Super;
+        typedef boost::variant<bool, vs::SqrtEx, carl::RealAlgebraicNumberPtr<smtrat::Rational>, SortValue, UFModel> Super;
         
     public:
         /**
          * Default constructor.
          */
-        Assignment(): Super() {
-        }
+        Assignment(): Super()
+        {}
 
         /**
          * Initializes the Assignment from some valid type of the underlying variant.
          */
         template<typename T>
-        Assignment(const T& t): Super(t) {
-        }
+        Assignment(const T& _t): Super(_t)
+        {}
 
         /**
          * Assign some value to the underlying variant.
@@ -44,8 +72,9 @@ namespace smtrat {
          * @return *this.
          */
         template<typename T>
-        Assignment& operator=(const T& t) {
-            Super::operator=(t);
+        Assignment& operator=( const T& _t )
+        {
+            Super::operator=(_t);
             return *this;
         }
 
@@ -56,86 +85,116 @@ namespace smtrat {
          * If both Assignments are not bools, the check may return false although they represent the same value.
          * If both are numbers in different representations, this comparison is only done as a "best effort".
          * 
-         * @param a Another Assignment.
+         * @param _ass Another Assignment.
          * @return *this == a.
          */
-        bool operator==(const Assignment& a) {
-            // Handle bools
-            if (this->isBool()) {
-                return (a.isBool()) && (this->asBool() == a.asBool());
-            } else if (a.isBool()) {
-                return false;
+        bool operator==( const Assignment& _ass )
+        {
+            if( isBool() && _ass.isBool() )
+            {
+                return asBool() == _ass.asBool();
             }
-
-            // Handle numbers
-            if (this->isSqrtEx()) {
-                if (a.isSqrtEx()) {
-                    return this->asSqrtEx() == a.asSqrtEx();
-                } else if (a.isRAN()) {
-                    return false;
-                }
-            } else if (this->isRAN()) {
-                if (a.isSqrtEx()) {
-                    return false;
-                } else if (a.isRAN()) {
-                    return std::equal_to<carl::RealAlgebraicNumberPtr<smtrat::Rational>>()(this->asRAN(), a.asRAN());
-                }
+            else if( isSqrtEx() && _ass.isSqrtEx() )
+            {
+                return asSqrtEx() == _ass.asSqrtEx();
+            } 
+            else if( isRAN() & _ass.isRAN() )
+            {
+                return std::equal_to<carl::RealAlgebraicNumberPtr<smtrat::Rational>>()(asRAN(), _ass.asRAN());
+            }
+            else if( isSortValue() & _ass.isSortValue() )
+            {
+                return asSortValue() == _ass.asSortValue();
+            }
+            else if( isUFModel() & _ass.isUFModel() )
+            {
+                return asUFModel() == _ass.asUFModel();
             }
             return false;
         }
 
         /**
-         * Checks if the stored value is a bool.
-         * @return 
+         * @return true, if the stored value is a bool.
          */
-        bool isBool() const {
-            return this->type() == typeid(bool);
+        bool isBool() const
+        {
+            return type() == typeid(bool);
         }
         
         /**
-         * Checks if the stored value is a vs::SqrtEx.
-         * @return 
+         * @return true, if the stored value is a square root expression.
          */
-        bool isSqrtEx() const {
-            return this->type() == typeid(vs::SqrtEx);
+        bool isSqrtEx() const
+        {
+            return type() == typeid(vs::SqrtEx);
         }
         
         /**
-         * Checks if the stored value is a carl::RealAlgebraicNumberPtr.
-         * @return 
+         * @return true, if the stored value is a real algebraic number.
          */
-        bool isRAN() const {
-            return this->type() == typeid(carl::RealAlgebraicNumberPtr<smtrat::Rational>);
+        bool isRAN() const
+        {
+            return type() == typeid(carl::RealAlgebraicNumberPtr<smtrat::Rational>);
+        }
+        
+        /**
+         * @return true, if the stored value is a sort value.
+         */
+        bool isSortValue() const
+        {
+            return type() == typeid(SortValue);
+        }
+        
+        /**
+         * @return true, if the stored value is a uninterpreted function model.
+         */
+        bool isUFModel() const {
+            return type() == typeid(UFModel);
         }
 
         /**
-         * Returns the stored value as a boolean.
-         * Asserts that it is in fact a boolean.
-         * @return 
+         * @return The stored value as a bool.
          */
-        bool asBool() const {
-            assert(this->isBool());
+        bool asBool() const
+        {
+            assert( isBool() );
             return boost::get<bool>(*this);
         }
         
         /**
-         * Returns the stored value as a vs::SqrtEx.
-         * Asserts that it is in fact a vs::SqrtEx.
-         * @return 
+         * @return The stored value as a square root expression.
          */
-        vs::SqrtEx asSqrtEx() const {
-            assert(this->isSqrtEx());
+        const vs::SqrtEx& asSqrtEx() const
+        {
+            assert( isSqrtEx() );
             return boost::get<vs::SqrtEx>(*this);
         }
         
         /**
-         * Returns the stored value as a carl::RealAlgebraicNumberPtr.
-         * Asserts that it is in fact a carl::RealAlgebraicNumberPtr.
-         * @return 
+         * @return The stored value as a real algebraic number.
          */
-        carl::RealAlgebraicNumberPtr<smtrat::Rational> asRAN() const {
-            assert(this->isRAN());
+        carl::RealAlgebraicNumberPtr<smtrat::Rational> asRAN() const
+        {
+            assert( isRAN() );
             return boost::get<carl::RealAlgebraicNumberPtr<smtrat::Rational>>(*this);
+        }
+        
+        /**
+         * @return The stored value as a sort value.
+         */
+        const SortValue& asSortValue() const
+        {
+            assert( isSortValue() );
+            return boost::get<SortValue>(*this);
+        }
+        
+        /**
+         * @return The stored value as a uninterpreted function model.
+         */
+        const UFModel& asUFModel() const
+        {
+            assert( isUFModel() );
+            return boost::get<UFModel>(*this);
         }
     };
 
