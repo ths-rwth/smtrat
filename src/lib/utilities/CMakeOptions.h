442b8fbecfbf2b4f08f61bb28dd623f6ef8191ad
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-11-28 10:04:59 +0100

    Added functionality to print cmake options active at compile time.

---
+++ src/lib/utilities/CMakeOptions.h
@@ -0,0 +1,14 @@
+/**
+ * @file CMakeOptions.h.in
+ * @author Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
+ */
+
+#pragma once
+
+#include <iostream>
+
+namespace smtrat {
+
+void printCMakeOptions(std::ostream& os);
+
+}
