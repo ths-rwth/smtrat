a28899917110d3ab0023ee03ee893f46578f03c6
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-02-12 12:07:51 +0100

    Added utils/ folder for common methods.

---
+++ src/benchmax/utils/regex.h
@@ -0,0 +1,23 @@
+/**
+ * @file regex.h
+ * @author Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
+ */
+
+#pragma once
+
+#ifdef USE_BOOST_REGEX
+#include <boost/regex.hpp>
+namespace benchmax {
+	using boost::regex;
+	using boost::regex_match;
+	using boost::regex_iterator;
+}
+#else
+#include <stdexcept>
+#include <regex>
+namespace benchmax {
+	using std::regex;
+	using std::regex_match;
+	using std::sregex_iterator;
+}
+#endif
