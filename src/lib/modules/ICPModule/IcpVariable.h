19731d7c939c508f9e0d31c39c536cd01181320a
Florian Corzilius <flo@florians-mbp.fritz.box>
2015-05-08 09:57:46 +0200

    preparing the release: old license removed from all files in SMT-RAT

--- src/lib/modules/ICPModule/IcpVariable.h
+++ src/lib/modules/ICPModule/IcpVariable.h
@@ -1,34 +1,11 @@
 /*
- *  SMT-RAT - Satisfiability-Modulo-Theories Real Algebra Toolbox
- * Copyright (C) 2012 Florian Corzilius, Ulrich Loup, Erika Abraham, Sebastian Junges
- *
- * This file is part of SMT-RAT.
- *
- * SMT-RAT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SMT-RAT is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SMT-RAT.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
-
-/*
  * File:   variable.h
  * Author: Stefan Schupp
  *
  * Created on January 15, 2013, 10:40 AM
  */
 
-#ifndef VARIABLE_H
-#define VARIABLE_H
+#pragma once
 
 #include "ContractionCandidate.h"
 #include "../../Common.h"
@@ -357,4 +334,3 @@ namespace icp
     typedef std::set<const IcpVariable*, icpVariableComp>  set_icpVariable;
 }    // namespace icp
 } // namespace smtrat
-#endif   /* VARIABLE_H */
--- src/lib/solver/ThreadPool.h
+++ src/lib/solver/ThreadPool.h
@@ -1,24 +1,3 @@
-/*
- * SMT-RAT - Satisfiability-Modulo-Theories Real Algebra Toolbox
- * Copyright (C) 2013 Florian Corzilius, Ulrich Loup, Erika Abraham, Sebastian Junges
- *
- * This file is part of SMT-RAT.
- *
- * SMT-RAT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SMT-RAT is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SMT-RAT. If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 /**
  * @file ThreadPool.h
  *
@@ -27,8 +6,7 @@
  * @version 2013-02-01
  */
 
-#ifndef THREADPOOL_H
-#define	THREADPOOL_H
+#pragma once
 
 #include <atomic>
 #include <future>
@@ -130,5 +108,3 @@ namespace smtrat
             std::future<Answer> submitBackend( Module*, bool );
     };
 }    // namespace smtrat
-
-#endif	/* THREADPOOL_H */
