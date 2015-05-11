75211c14892496b0e0d85cc5b0355eb63eb445ef
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-11 10:04:16 +0200

    Do not build carl if in doxygen mode.

--- .travis/setup_travis.sh
+++ .travis/setup_travis.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/nafur/carl.git
-cd carl/.travis/
-source setup_travis.sh
-cd ../
-source .travis/build.sh
-cd ../
+if [[ ${TASK} == "doxygen" ]]; then
+
+else
+	git clone https://github.com/nafur/carl.git
+	cd carl/.travis/
+	source setup_travis.sh
+	cd ../
+	source .travis/build.sh
+	cd ../
+fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
 
