b4d16541e8afdcf50853f52d99e3236595620391
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-11 13:29:12 +0200

    Added proper error handling for doxygen build.

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,18 +5,18 @@ cd build/ || return 1
 cmake -D DEVELOPER=ON ../ || return 1
 
 if [[ ${TASK} == "doxygen" ]]; then
-	make doc
+	make doc || return 1
 	
 	git config --global user.email "gereon.kremer@cs.rwth-aachen.de"
 	git config --global user.name "Travis doxygen daemon"
 	
 	git clone https://219fc41efb80a7a8f102f5ca9147baf58514d734@github.com/smtrat/smtrat.github.io.git
-	cd smtrat.github.io/
+	cd smtrat.github.io/ || return 1
 	
-	cp ../doc/html/* ./
-	git add .
-	git commit -m "Updated documentation for SMT-RAT"
-	git push origin master
+	cp ../doc/html/* ./ || return 1
+	git add . || return 1
+	git commit -m "Updated documentation for SMT-RAT" || return 1
+	git push origin master || return 1
 
 else
 	make -j1 || return 1
