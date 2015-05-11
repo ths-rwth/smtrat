3db29136ca6cfe7438282f94a940f7068399ded6
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-11 10:58:31 +0200

    Correctly setting USE and TASK.

--- .travis/setup_travis.sh
+++ .travis/setup_travis.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/nafur/carl.git
-cd carl/.travis/
-source setup_travis.sh
-cd ../
-source .travis/build.sh
-cd ../
+if [[ ${TASK} == "doxygen" ]]; then
+	export TASK="clang++-3.5"
+	export USE="clang++-3.5"
+	git clone https://github.com/nafur/carl.git
+	cd carl/.travis/
+	source setup_travis.sh
+	cd ../
+	source .travis/build.sh
+	cd ../
+	export TASK="doxygen"
+else
+	git clone https://github.com/nafur/carl.git
+	cd carl/.travis/
+	source setup_travis.sh
+	cd ../
+	source .travis/build.sh
+	cd ../
+fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
 
