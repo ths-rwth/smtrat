aea24aa803eaf4989db0929dddff77b622c1df1e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-12 09:48:33 +0200

    more error handling.

--- .travis/setup_travis.sh
+++ .travis/setup_travis.sh
@@ -5,18 +5,18 @@ if [[ ${TASK} == "doxygen" ]]; then
 	export USE="clang++-3.5"
 	git clone https://github.com/nafur/carl.git
 	cd carl/.travis/
-	source setup_travis.sh
+	source setup_travis.sh || return 1
 	cd ../
-	source .travis/build.sh
+	source .travis/build.sh || return 1
 	cd ../
 	export TASK="doxygen"
 	export USE="doxygen"
 else
 	git clone https://github.com/nafur/carl.git
 	cd carl/.travis/
-	source setup_travis.sh
+	source setup_travis.sh || return 1
 	cd ../
-	source .travis/build.sh
+	source .travis/build.sh || return 1
 	cd ../
 fi
 
