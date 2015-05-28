df05b31183ab4805fb11d50b39703043f748846f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-28 13:17:04 +0200

    Fixed scoping. Now, we have ExpressionScopes and ScriptScopes.

--- src/cli/newparser/theories/ParserState.h
+++ src/cli/newparser/theories/ParserState.h
@@ -16,61 +16,83 @@ namespace parser {
 	
 	struct ParserState {
 
-		struct Scope {
+		struct ExpressionScope {
 		private:
-			std::map<std::string, types::VariableType> variables;
 			std::map<std::string, types::TermType> bindings;
-			std::map<std::string, carl::UninterpretedFunction> declared_functions;
 		public:
-			Scope(const ParserState& state)
+			ExpressionScope(const ParserState& state)
 			{
-				this->variables = state.variables;
 				this->bindings = state.bindings;
-				this->declared_functions = state.declared_functions;
 			}
-			void restore(ParserState& state) {
-				state.variables = this->variables;
-				state.bindings = this->bindings;
-				state.declared_functions = this->declared_functions;
+			void discharge(ParserState& state) {
+				state.bindings = std::move(this->bindings);
 			}
 		};
 		
-		std::map<std::string, types::VariableType> variables;
+		struct ScriptScope {
+		private:
+			std::map<std::string, types::TermType> constants;
+			std::map<std::string, types::VariableType> variables;
+			std::map<std::string, carl::UninterpretedFunction> declared_functions;
+			std::map<std::string, const FunctionInstantiator*> defined_functions;
+			std::map<std::string, const IndexedFunctionInstantiator*> defined_indexed_functions;
+			std::map<std::string, const UserFunctionInstantiator*> defined_user_functions;
+		public:
+			ScriptScope(const ParserState& state)
+			{
+				this->constants = state.constants;
+				this->variables = state.variables;
+				this->declared_functions = state.declared_functions;
+				this->defined_functions = state.defined_functions;
+				this->defined_indexed_functions = state.defined_indexed_functions;
+				this->defined_user_functions = state.defined_user_functions;
+			}
+			void discharge(ParserState& state) {
+				state.constants = std::move(this->constants);
+				state.variables = std::move(this->variables);
+				state.declared_functions = std::move(this->declared_functions);
+				state.defined_functions = std::move(this->defined_functions);
+				state.defined_indexed_functions = std::move(this->defined_indexed_functions);
+				state.defined_user_functions = std::move(this->defined_user_functions);
+			}
+		};
+		
 		std::map<std::string, types::TermType> bindings;
+		std::map<std::string, types::TermType> constants;
+		std::map<std::string, types::VariableType> variables;
 		std::map<std::string, carl::UninterpretedFunction> declared_functions;
-		std::map<std::string, types::TermType> defined_constants;
 		std::map<std::string, const FunctionInstantiator*> defined_functions;
 		std::map<std::string, const IndexedFunctionInstantiator*> defined_indexed_functions;
 		std::map<std::string, const UserFunctionInstantiator*> defined_user_functions;
 	
-		//std::map<std::string, BooleanFunction> funmap_bool;
-		//std::map<std::string, ArithmeticFunction> funmap_arithmetic;
-		//std::map<std::string, carl::UninterpretedFunction> funmap_ufbool;
-		//std::map<std::string, carl::UninterpretedFunction> funmap_ufarithmetic;
-
-		FormulasT mArithmeticIteBindings;
-		FormulasT mUninterpretedEqualities;
-		std::map<types::TermType, carl::UVariable> mUninterpretedArguments;
-		std::map<carl::Variable, std::tuple<FormulaT, Poly, Poly>> mArithmeticItes;
 		FormulasT mGlobalFormulas;
 		
 		InstructionHandler* handler;
 		
-		std::stack<Scope> mScopeStack;
+		std::stack<ExpressionScope> expressionScopes;
+		std::stack<ScriptScope> scriptScopes;
 		
 		ParserState(InstructionHandler* ih): handler(ih) {
 		}
 		
-		void pushScope() {
-			mScopeStack.emplace(*this);
+		void pushExpressionScope() {
+			expressionScopes.emplace(*this);
 		}
-		void popScope() {
-			mScopeStack.top().restore(*this);
-			mScopeStack.pop();
+		void popExpressionScope() {
+			expressionScopes.top().discharge(*this);
+			expressionScopes.pop();
+		}
+		void pushScriptScope() {
+			assert(expressionScopes.empty());
+			scriptScopes.emplace(*this);
+		}
+		void popScriptScope() {
+			assert(expressionScopes.empty());
+			scriptScopes.top().discharge(*this);
+			scriptScopes.pop();
 		}
 
-		//bool checkArguments(const std::string& name, const std::vector<carl::Variable>& types, const Arguments& args, std::map<carl::Variable, FormulaT>& boolAssignments, std::map<carl::Variable, Poly>& theoryAssignments);
-
+		
 		void errorMessage(const std::string& msg) {
 			std::cerr << "Parser error: " << msg << std::endl;
 		}
@@ -79,7 +101,7 @@ namespace parser {
 				if (name == "true" || name == "false") out << "\"" << name << "\" is a reserved keyword.";
 				else if (variables.find(name) != variables.end()) out << "\"" << name << "\" has already been defined as a variable.";
 				else if (bindings.find(name) != bindings.end()) out << "\"" << name << "\" has already been defined as a binding to \"" << bindings[name] << "\".";
-				else if (defined_constants.find(name) != defined_constants.end()) out << "\"" << name << "\" has already been defined as a constant.";
+				else if (constants.find(name) != constants.end()) out << "\"" << name << "\" has already been defined as a constant.";
 				else if (declared_functions.find(name) != declared_functions.end()) out << "\"" << name << "\" has already been declared as a function.";
 				else if (defined_functions.find(name) != defined_functions.end()) out << "\"" << name << "\" has already been defined as a function.";
 				else if (defined_indexed_functions.find(name) != defined_indexed_functions.end()) out << "\"" << name << "\" has already been defined as a function.";
@@ -100,7 +122,7 @@ namespace parser {
 		bool resolveSymbol(const std::string& name, types::TermType& r) const {
 			if (resolveSymbol(name, variables, r)) return true;
 			if (resolveSymbol(name, bindings, r)) return true;
-			if (resolveSymbol(name, defined_constants, r)) return true;
+			if (resolveSymbol(name, constants, r)) return true;
 			return false;
 		}
 		
