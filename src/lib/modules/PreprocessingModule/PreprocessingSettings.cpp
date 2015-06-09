3296ba1511c77e5828fbaf1d3e101ce723c696aa
Florian Corzilius <flo@179-218.eduroam.rwth-aachen.de>
2015-06-09 10:19:22 +0200

    some bugfixes in the preprocessing applying substitutions in a formula and added a bound detection for for disjunction of constraints containing the same polynomial modulo bound

--- src/lib/modules/PreprocessingModule/PreprocessingSettings.cpp
+++ src/lib/modules/PreprocessingModule/PreprocessingSettings.cpp
@@ -4,10 +4,14 @@ constexpr bool smtrat::PreprocessingSettings::removeFactors;
 constexpr bool smtrat::PreprocessingSettings::checkBounds;
 constexpr bool smtrat::PreprocessingSettings::splitSOS;
 constexpr bool smtrat::PreprocessingSettings::eliminateSubstitutions;
+constexpr bool smtrat::PreprocessingSettings::extractBounds;
+constexpr bool smtrat::PreprocessingSettings::removeUnboundedVars;
 
 const bool smtrat::PreprocessingSettings::dummy = SettingsManager::addModule("Preprocessing",
 	"removeFactors", true, smtrat::PreprocessingSettings::removeFactors,
 	"checkBounds", true, smtrat::PreprocessingSettings::checkBounds,
 	"splitSOS", true, smtrat::PreprocessingSettings::splitSOS,
-	"eliminateSubstitutions", false, smtrat::PreprocessingSettings::eliminateSubstitutions
+	"eliminateSubstitutions", false, smtrat::PreprocessingSettings::eliminateSubstitutions,
+	"extractBounds", true, smtrat::PreprocessingSettings::extractBounds,
+	"removeUnboundedVars", true, smtrat::PreprocessingSettings::removeUnboundedVars
 );
