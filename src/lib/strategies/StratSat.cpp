9e0a58f5d25efbd7323174d0cd454a0457d0ab2e
Matthias Volk <Matthias.Volk@rwth-aachen.de>
2015-07-20 15:18:52 +0200

    Added preprocessing module in StratSat

--- src/lib/strategies/StratSat.cpp
+++ src/lib/strategies/StratSat.cpp
@@ -11,8 +11,9 @@ namespace smtrat
 	StratSat::StratSat() :
         Manager()
     {
-        addBackendIntoStrategyGraph( 0, MT_CNFerModule );
-        addBackendIntoStrategyGraph( 1, MT_SATModule );
+        addBackendIntoStrategyGraph( 0, MT_PreprocessingModule );
+        addBackendIntoStrategyGraph( 1, MT_CNFerModule );
+        addBackendIntoStrategyGraph( 2, MT_SATModule );
     }
 
 	StratSat::~StratSat(){}
