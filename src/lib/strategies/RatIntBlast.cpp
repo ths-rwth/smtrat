0e2d44990283deec30e422e7ecbac6594a190446
Andreas Krüger <andreas.krueger@rwth-aachen.de>
2015-08-13 16:44:19 +0200

    Moved IntBlasting to separate strategy for testing

--- src/lib/strategies/RatIntBlast.cpp
+++ src/lib/strategies/RatIntBlast.cpp
@@ -1,20 +1,17 @@
 /**
- * @file RatOne.cpp
+ * @file RatIntBlast.cpp
  */
 
-#include "RatOne.h"
+#include "RatIntBlast.h"
 #include "config.h"
 
 namespace smtrat
 {
 
-    RatOne::RatOne():
+    RatIntBlast::RatIntBlast():
         Manager()
     {
         size_t position = 0;
-        #ifdef SMTRAT_ENABLE_BVModule
-        position = addBackendIntoStrategyGraph( position, MT_BVModule );
-        #endif
         #ifdef SMTRAT_ENABLE_PreprocessingModule
         position = addBackendIntoStrategyGraph( position, MT_PreprocessingModule );
         #else
@@ -22,20 +19,17 @@ namespace smtrat
         position = addBackendIntoStrategyGraph( position, MT_CNFerModule );
         #endif
         #endif
-	#ifdef SMTRAT_ENABLE_SATModule
+        #ifdef SMTRAT_ENABLE_SATModule
         position = addBackendIntoStrategyGraph( position, MT_SATModule );
-	#endif
-        #ifdef SMTRAT_ENABLE_IntEqModule
-//        position = addBackendIntoStrategyGraph( position, MT_IntEqModule );
         #endif
-        #ifdef SMTRAT_ENABLE_FouMoModule
-//        position = addBackendIntoStrategyGraph( position, MT_FouMoModule );
+        #ifdef SMTRAT_ENABLE_ICPModule
+        position = addBackendIntoStrategyGraph( position, MT_ICPModule );
+        #endif
+        #ifdef SMTRAT_ENABLE_IntBlastModule
+        position = addBackendIntoStrategyGraph( position, MT_IntBlastModule );
         #endif
         #ifdef SMTRAT_ENABLE_LRAModule
         position = addBackendIntoStrategyGraph( position, MT_LRAModule );
-        #endif
-        #ifdef SMTRAT_ENABLE_GroebnerModule
-//        position = addBackendIntoStrategyGraph( position, MT_GroebnerModule );
         #endif
         #ifdef SMTRAT_ENABLE_VSModule
         position = addBackendIntoStrategyGraph( position, MT_VSModule );
@@ -43,10 +37,9 @@ namespace smtrat
         #ifdef SMTRAT_ENABLE_CADModule
         position = addBackendIntoStrategyGraph( position, MT_CADModule );
         #endif
-//        #endif
     }
 
-    RatOne::~RatOne(){}
+    RatIntBlast::~RatIntBlast(){}
 
 }    // namespace smtrat
 
---
+++ src/lib/strategies/RatIntBlast.h
@@ -0,0 +1,27 @@
+/**
+ * @file RatIntBlast.h
+ */
+#pragma once
+
+#include "../solver/Manager.h"
+
+namespace smtrat
+{
+    /**
+     * Strategy description.
+     *
+     * @author
+     * @since
+     * @version
+     *
+     */
+    class RatIntBlast:
+        public Manager
+    {
+        public:
+            RatIntBlast();
+            ~RatIntBlast();
+
+    };
+
+}    // namespace smtrat
