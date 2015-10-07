3067d5985c6b9a2144cb10986770907c707a0675
Florian Corzilius <corzilius@cs.rwth-aachen.de>
2015-10-07 22:55:04 +0200

    changed settings in inblastmodule

--- src/lib/strategies/RatIntBlast.cpp
+++ src/lib/strategies/RatIntBlast.cpp
@@ -12,15 +12,15 @@ namespace smtrat
         Manager( _externalModuleFactoryAdding )
     {
         size_t position = 0;
-        position = addBackendIntoStrategyGraph( position, MT_PreprocessingModule );
-        position = addBackendIntoStrategyGraph( position, MT_SATModule );
+//        position = addBackendIntoStrategyGraph( position, MT_PreprocessingModule );
+//        position = addBackendIntoStrategyGraph( position, MT_SATModule );
         // position = addBackendIntoStrategyGraph( position, MT_ICPModule );
         position = addBackendIntoStrategyGraph( position, MT_IntBlastModule );
 //        position = addBackendIntoStrategyGraph( position, MT_CNFerModule );
 //        position = addBackendIntoStrategyGraph( position, MT_SATModule );
-        position = addBackendIntoStrategyGraph( position, MT_LRAModule );
-        position = addBackendIntoStrategyGraph( position, MT_VSModule );
-        position = addBackendIntoStrategyGraph( position, MT_CADModule );
+//        position = addBackendIntoStrategyGraph( position, MT_LRAModule );
+//        position = addBackendIntoStrategyGraph( position, MT_VSModule );
+//        position = addBackendIntoStrategyGraph( position, MT_CADModule );
     }
 
     RatIntBlast::~RatIntBlast(){}
