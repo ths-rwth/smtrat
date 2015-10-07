094923064fc14982b5181239a858f2b58039aeb0
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-10-07 14:53:25 +0200

    Working on integerHandling setting.

--- src/lib/modules/CADModule/CADSettings.h
+++ src/lib/modules/CADModule/CADSettings.h
@@ -10,40 +10,19 @@
 
 #include <iostream>
 
+#include <carl/cad/CADSettings.h>
+
 #include "../../utilities/SettingsManager.h"
 
 namespace smtrat
-{
-    enum class IntegerHandling {
-        /// Generate a full real solution and split on non-integral assignments in this solution.
-        SPLIT_SOLUTION,
-        /// Split if no integral sample is available for an integer variable.
-        SPLIT_LAZY,
-        /// Split if a non-integral sample is available for an integer variable.
-        SPLIT_EARLY,
-        /// Backtrack within the sampling phase.
-        BACKTRACK
-    };
-    inline std::ostream& operator<<(std::ostream& os, const IntegerHandling& ih) {
-        switch (ih) {
-            case IntegerHandling::SPLIT_SOLUTION: return os << "Split on Solution";
-            case IntegerHandling::SPLIT_LAZY: return os << "Split lazy";
-            case IntegerHandling::SPLIT_EARLY: return os << "Split early";
-            case IntegerHandling::BACKTRACK: return os << "Backtrack";
-        }
-        return os;
-    }
-    inline std::istream& operator>>(std::istream& in, IntegerHandling& ih) {
-        return in;
-    }
-    
+{    
 	enum class MISHeuristic {
 		TRIVIAL, GREEDY
 	};
     
     struct CADSettings1
     {
-        static constexpr IntegerHandling integer_handling = IntegerHandling::SPLIT_EARLY;
+        static constexpr carl::cad::IntegerHandling integerHandling = carl::cad::IntegerHandling::SPLIT_EARLY;
         
         static constexpr MISHeuristic mis_heuristic = MISHeuristic::GREEDY;
         static constexpr bool computeConflictGraph = (mis_heuristic != MISHeuristic::TRIVIAL);
