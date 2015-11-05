1b6b129420ae92aaf3d5db80486056ffae7afbbf
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-11-05 17:00:30 +0100

    using statistics, removing debug output in FPPModule.

--- src/lib/modules/FPPModule/FPPModule.cpp
+++ src/lib/modules/FPPModule/FPPModule.cpp
@@ -13,6 +13,9 @@ namespace smtrat
     template<class Settings>
     FPPModule<Settings>::FPPModule( const ModuleInput* _formula, RuntimeSettings*, Conditionals& _conditionals, Manager* _manager ):
         PModule( _formula, _conditionals, _manager ),
+#ifdef SMTRAT_DEVOPTION_Statistics
+		mStatistics(SettingsType::moduleName),
+#endif
         mIterations( 0 ),
         mFormulaAfterPreprocessing( carl::FormulaType::TRUE ),
         mPreprocessor()
@@ -60,45 +63,38 @@ namespace smtrat
     template<class Settings>
     Answer FPPModule<Settings>::checkCore( bool _full )
     {
-        if( mIterations > 0 ) // we did a check before hence the result is still stored in the preprocessor
-            mPreprocessor.pop();
+        //if( mIterations > 0 ) // we did a check before hence the result is still stored in the preprocessor
+        //    mPreprocessor.pop();
         mIterations = 0;
         Answer ans = Unknown;
         FormulaT formulaBeforePreprocessing = (FormulaT) rReceivedFormula();
         while( Settings::max_iterations < 0 || mIterations < Settings::max_iterations )
         {
-            if( mIterations > 0 )
-                mPreprocessor.pop();
             ++mIterations;
             // call the preprocessing on the current formula
             mPreprocessor.push();
-//            std::cout << "Try to preprocess:" << std::endl;
-//            std::cout << formulaBeforePreprocessing << std::endl;
             mPreprocessor.add( formulaBeforePreprocessing );
             ans = mPreprocessor.check( _full );
             // preprocessing detects satisfiabilty or unsatisfiability
-            if( ans != Unknown )
+            if( ans != Unknown ) {
+	            mPreprocessor.pop();
                 break;
+			}
             std::pair<bool,FormulaT> res = mPreprocessor.getInputSimplified();
-//                std::cout << "Preprocessing step led to: " << std::endl;
+	        mPreprocessor.pop();
             if( res.first )
             {
                 mFormulaAfterPreprocessing = res.second;
-//                std::cout << mFormulaAfterPreprocessing << std::endl;
             }
             else
             {
-//                std::cout << "No change:" << std::endl;
-//                std::cout << mFormulaAfterPreprocessing << std::endl;
                 break;
             }
             // after preprocessing is before preprocessing
             formulaBeforePreprocessing = mFormulaAfterPreprocessing;
         }
-//        std::cout << "ans after preprocessing: " << ans << std::endl; 
         if( ans == Unknown )
         {
-//            std::cout << "ask backends" << std::endl;
             // run the backends on the fix point of the iterative application of preprocessing
             // TODO: make this incremental
             clearPassedFormula();
