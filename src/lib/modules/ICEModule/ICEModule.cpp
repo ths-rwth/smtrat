8b7a0af4f618a2593c1f5804e2f2cea07ec96e8e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-11-24 18:35:34 +0100

    Added the new ICEModule which is a cleaned up version of LICModule with a new cycle finding algorithm.

---
+++ src/lib/modules/ICEModule/ICEModule.cpp
@@ -0,0 +1,212 @@
+/**
+ * @file ICE.cpp
+ * @author YOUR NAME <YOUR EMAIL ADDRESS>
+ *
+ * @version 2015-11-24
+ * Created on 2015-11-24.
+ */
+
+#include "ICEModule.h"
+
+namespace smtrat
+{
+	template<class Settings>
+	ICEModule<Settings>::ICEModule(const ModuleInput* _formula, RuntimeSettings*, Conditionals& _conditionals, Manager* _manager):
+		PModule( _formula, _conditionals, _manager )
+#ifdef SMTRAT_DEVOPTION_Statistics
+		, mStatistics(Settings::moduleName)
+#endif
+	{}
+	
+	template<class Settings>
+	ICEModule<Settings>::~ICEModule()
+	{}
+	
+	template<class Settings>
+	bool ICEModule<Settings>::informCore( const FormulaT& _constraint )
+	{
+		return true;
+	}
+	
+	template<class Settings>
+	void ICEModule<Settings>::init()
+	{}
+	
+	template<class Settings>
+	bool ICEModule<Settings>::addCore( ModuleInput::const_iterator _subformula )
+	{
+		addReceivedSubformulaToPassedFormula(_subformula);
+		addFormula(_subformula->formula());
+		return true;
+	}
+	
+	template<class Settings>
+	void ICEModule<Settings>::removeCore( ModuleInput::const_iterator _subformula )
+	{
+		removeFormula(_subformula->formula());
+	}
+	
+	template<class Settings>
+	void ICEModule<Settings>::updateModel() const
+	{
+		mModel.clear();
+		if (solverState() == True) {
+			getBackendsModel();
+		}
+	}
+	
+	template<class Settings>
+	Answer ICEModule<Settings>::checkCore( bool _full )
+	{
+		SMTRAT_LOG_DEBUG("smtrat.ice", "Obtained the following bounds: " << std::endl << mBounds);
+		Answer res = processConstraints();
+		if (res == False) return False;
+		
+		res = runBackends(_full);
+		if (res == False) getInfeasibleSubsets();
+		return res;
+	}
+	
+	template<class Settings>
+	void ICEModule<Settings>::addFormula(const FormulaT& f) {
+		switch (f.getType()) {
+			case carl::FormulaType::CONSTRAINT:
+				mBounds.addBound(f.constraint(), f);
+				if (!f.constraint().isBound()) {
+					mConstraints.insert(f.constraint());
+				}
+				break;
+			case carl::FormulaType::NOT: {
+				if (f.subformula().getType() == carl::FormulaType::CONSTRAINT) {
+					const ConstraintT& c = f.subformula().constraint();
+					ConstraintT newC(c.lhs(), invertRelation(c.relation()));
+					mBounds.addBound(newC, f);
+					if (!newC.isBound()) {
+						mConstraints.insert(newC);
+					}
+				}
+				break;
+			}
+			default:
+				break;
+		}
+	}
+	
+	template<class Settings>
+	void ICEModule<Settings>::removeFormula(const FormulaT& f) {
+		switch (f.getType()) {
+			case carl::FormulaType::CONSTRAINT:
+				mBounds.removeBound(f.constraint(), f);
+				if (!f.constraint().isBound()) {
+					mConstraints.erase(f.constraint());
+				}
+				break;
+			case carl::FormulaType::NOT: {
+				if (f.subformula().getType() == carl::FormulaType::CONSTRAINT) {
+					const ConstraintT& c = f.subformula().constraint();
+					ConstraintT newC(c.lhs(), invertRelation(c.relation()));
+					mBounds.removeBound(newC, f);
+					if (!newC.isBound()) {
+						mConstraints.erase(newC);
+					}
+				}
+				break;
+			}
+			default:
+				break;
+		}
+	}
+	
+	template<class Settings>
+	Answer ICEModule<Settings>::processConstraints() {
+		FHG graph;
+		
+		TermT src;
+		std::vector<TermT> dest;
+		Coefficient coeff;
+		for (const auto& c: mConstraints) {
+			if (isSuitable(c, src, dest, coeff)) {
+				auto& edge = graph.newEdge(graph.newVertex(src), EdgeProperty(coeff, c));
+				for (const auto& d: dest) {
+					edge.addOut(graph.newVertex(d));
+				}
+			}
+		}
+		if (Settings::dumpAsDot) {
+			SMTRAT_LOG_INFO("smtrat.ice", "Dumping resulting graph to " << Settings::dotFilename);
+			graph.toDot(Settings::dotFilename);
+		}
+
+		CycleCollector collector;
+		CycleEnumerator<FHG,CycleCollector> enumerator(graph, collector);
+		enumerator.findAll();
+		
+		if (!collector.mInfeasibleSubset.empty()) {
+			mInfeasibleSubsets.emplace_back(collector.mInfeasibleSubset);
+			return False;
+		}
+		for (const auto& lemma: collector.mLemmas) {
+			addSubformulaToPassedFormula(lemma.first, lemma.second);
+		}
+		return True;
+	}
+	
+	template<class Settings>
+	bool ICEModule<Settings>::isSuitable(const ConstraintT& c, TermT& src, std::vector<TermT>& dest, Coefficient& coeff) {
+		SMTRAT_LOG_FUNC("smtrat.ice", c);
+		bool invert = false;
+		src = TermT();
+		dest.clear();
+		coeff.strict = false;
+		coeff.r = 0;
+
+		switch (c.relation()) {
+			case carl::Relation::EQ: break;
+			case carl::Relation::NEQ: return false;
+			case carl::Relation::LESS:
+				invert = true;
+				coeff.strict = true;
+				break;
+			case carl::Relation::LEQ:
+				invert = true;
+				break;
+			case carl::Relation::GREATER:
+				coeff.strict = true;
+				break;
+			case carl::Relation::GEQ: break;
+		}
+		Poly p = c.lhs();
+		if (invert) p = -p;
+
+		for (const auto& term: p) {
+			if (term.isConstant()) {
+				coeff.r += term.coeff();
+				continue;
+			}
+			if (isZero(term)) {
+				SMTRAT_LOG_WARN("smtrat.ice", "Term " << term << " is zero. We'll ignore it.");
+				continue;
+			}
+			if (isSemiPositive(term)) {
+				if (!src.isZero()) {
+					SMTRAT_LOG_TRACE("smtrat.ice", "No: Already has a source, but " << term << " was found.");
+					return false;
+				}
+				src = term;
+			} else if (isSemiNegative(term)) {
+				dest.push_back(-term);
+			} else {
+				SMTRAT_LOG_TRACE("smtrat.ice", "No: Has indefinite term " << term << ".");
+				return false;
+			}
+		}
+		if (dest.empty()) {
+			SMTRAT_LOG_TRACE("smtrat.ice", "No: No destinations were found.");
+			return false;
+		}
+		SMTRAT_LOG_TRACE("smtrat.ice", "Yes: " << src << " -> " << dest << " with coefficient " << coeff << ".");
+		return true;
+	}
+}
+
+#include "Instantiation.h"
---
+++ src/lib/modules/ICEModule/ICEModule.h
@@ -0,0 +1,194 @@
+/**
+ * @file ICEModule.h
+ * @author YOUR NAME <YOUR EMAIL ADDRESS>
+ *
+ * @version 2015-11-24
+ * Created on 2015-11-24.
+ */
+
+#pragma once
+
+#include "../../solver/PModule.h"
+#include "../../datastructures/VariableBounds.h"
+
+#include "ICEStatistics.h"
+#include "ICESettings.h"
+#include "ForwardHyperGraph.h"
+
+namespace smtrat
+{
+	template<typename Settings>
+	class ICEModule : public PModule
+	{
+		struct Coefficient {
+			Rational r;
+			bool strict;
+			Coefficient(): r(0), strict(false) {}
+			Coefficient(Coefficient&& c): r(c.r), strict(c.strict) {}
+			Coefficient(const Coefficient& c): r(c.r), strict(c.strict) {}
+			Coefficient& operator=(const Coefficient& c) {
+				r = c.r;
+				strict = c.strict;
+				return *this;
+			}
+			Coefficient& operator+=(const Coefficient& c) {
+				r += c.r;
+				strict |= c.strict;
+				return *this;
+			}
+			friend std::ostream& operator<<(std::ostream& os, const Coefficient& c) {
+				return os << "(" << c.r << "," << std::boolalpha << c.strict << ")";
+			}
+		};
+
+		using VertexProperty = TermT;
+		struct EdgeProperty {
+			Coefficient coeff;
+			ConstraintT constraint;
+			EdgeProperty(const Coefficient& c, const ConstraintT& con): coeff(c), constraint(con) {}
+			friend std::ostream& operator<<(std::ostream& os, const EdgeProperty& ep) {
+				return os << ep.coeff;
+			}
+		};
+		using FHG = ForwardHyperGraph<VertexProperty, EdgeProperty>;
+		
+		struct CycleCollector {
+			FormulaSetT mInfeasibleSubset;
+			std::vector<std::pair<FormulaT,FormulaT>> mLemmas;
+			
+			FormulaT buildFormula(const std::vector<EdgeProperty>& edges) const {
+				FormulasT res;
+				std::transform(edges.begin(), edges.end(), std::back_inserter(res), [](const EdgeProperty& edge){ return FormulaT(edge.constraint); });
+				return FormulaT(carl::FormulaType::AND, std::move(res));
+			}
+			std::vector<VertexProperty> collectAdjacent(const std::vector<VertexProperty>& vertices, const std::vector<EdgeProperty>& edges) {
+				std::vector<VertexProperty> res;
+				return res;
+			}
+			
+			/**
+			 * Is called whenever a cycle is found.
+			 * If true is returned, the search for further cycles is aborted.
+			 */
+			bool operator()(const std::vector<VertexProperty>& vertices, const std::vector<EdgeProperty>& edges) {
+				Coefficient sum;
+				for (const auto& edge: edges) sum += edge.coeff;
+				
+				if (carl::isZero(sum.r)) {
+					FormulaT origin = buildFormula(edges);
+					if (sum.strict) {
+						// Sum is zero but there is a strict inequality -> UNSAT
+						mInfeasibleSubset = FormulaSetT(origin.subformulas().begin(), origin.subformulas().end());
+						return true;
+					} else {
+						// Sum is zero and all inequalities are weak
+						// -> Variables on the cycle
+						for (const auto& v: collectAdjacent(vertices, edges)) {
+							FormulaT lemma(Poly(v), carl::Relation::EQ);
+							SMTRAT_LOG_DEBUG("smtrat.ice", "Deducted " << lemma);
+							mLemmas.emplace_back(lemma, origin);
+						}
+						for (std::size_t i = 1; i < vertices.size(); i++) {
+							const auto& a = vertices[i-1];
+							const auto& b = vertices[i];
+							FormulaT lemma(a - b + edges[i-1].coeff.r, carl::Relation::EQ);
+							SMTRAT_LOG_DEBUG("smtrat.ice", "Deducted " << lemma);
+							mLemmas.emplace_back(lemma, origin);
+						}
+					}
+				} else if (sum.r > 0) {
+					SMTRAT_LOG_DEBUG("smtrat.ice", "What to do? sum > 0");
+				} else if (sum.r < 0) {
+					FormulaT origin = buildFormula(edges);
+					mInfeasibleSubset = FormulaSetT(origin.subformulas().begin(), origin.subformulas().end());
+					return true;
+				}
+				return false;
+			}
+		};
+
+		private:
+#ifdef SMTRAT_DEVOPTION_Statistics
+			ICEStatistics mStatistics;
+#endif
+			vb::VariableBounds<FormulaT> mBounds;
+			std::set<ConstraintT> mConstraints;
+			std::set<FormulaT> mOtherFormulas;
+			
+			void addFormula(const FormulaT& f);
+			void removeFormula(const FormulaT& f);
+			Answer processConstraints();
+			
+			bool isZero(const TermT& t) const {
+				return mBounds.getInterval(t).isZero();
+			}
+			bool isSemiPositive(const TermT& t) const {
+				return mBounds.getInterval(t).isSemiPositive();
+			}
+			bool isSemiNegative(const TermT& t) const {
+				return mBounds.getInterval(t).isSemiNegative();
+			}
+			bool isSuitable(const ConstraintT& c, TermT& src, std::vector<TermT>& dest, Coefficient& coeff);
+			
+		public:
+			typedef Settings SettingsType;
+			std::string moduleName() const {
+				return SettingsType::moduleName;
+			}
+			ICEModule(const ModuleInput* _formula, RuntimeSettings* _settings, Conditionals& _conditionals, Manager* _manager = nullptr);
+
+			~ICEModule();
+			
+			// Main interfaces.
+			/**
+			 * Informs the module about the given constraint. It should be tried to inform this
+			 * module about any constraint it could receive eventually before assertSubformula
+			 * is called (preferably for the first time, but at least before adding a formula
+			 * containing that constraint).
+			 * @param _constraint The constraint to inform about.
+			 * @return false, if it can be easily decided whether the given constraint is inconsistent;
+			 *		  true, otherwise.
+			 */
+			bool informCore( const FormulaT& _constraint );
+
+			/**
+			 * Informs all backends about the so far encountered constraints, which have not yet been communicated.
+			 * This method must not and will not be called more than once and only before the first runBackends call.
+			 */
+			void init();
+
+			/**
+			 * The module has to take the given sub-formula of the received formula into account.
+			 *
+			 * @param _subformula The sub-formula to take additionally into account.
+			 * @return false, if it can be easily decided that this sub-formula causes a conflict with
+			 *		  the already considered sub-formulas;
+			 *		  true, otherwise.
+			 */
+			bool addCore( ModuleInput::const_iterator _subformula );
+
+			/**
+			 * Removes the subformula of the received formula at the given position to the considered ones of this module.
+			 * Note that this includes every stored calculation which depended on this subformula, but should keep the other
+			 * stored calculation, if possible, untouched.
+			 *
+			 * @param _subformula The position of the subformula to remove.
+			 */
+			void removeCore( ModuleInput::const_iterator _subformula );
+
+			/**
+			 * Updates the current assignment into the model.
+			 * Note, that this is a unique but possibly symbolic assignment maybe containing newly introduced variables.
+			 */
+			void updateModel() const;
+
+			/**
+			 * Checks the received formula for consistency.
+			 * @param _full false, if this module should avoid too expensive procedures and rather return unknown instead.
+			 * @return True,	if the received formula is satisfiable;
+			 *		 False,   if the received formula is not satisfiable;
+			 *		 Unknown, otherwise.
+			 */
+			Answer checkCore( bool _full );
+	};
+}
