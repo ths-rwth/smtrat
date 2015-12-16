a79b7b7b09811ed44ff98e3daa54905797232d71
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-12-16 16:39:24 +0100

    Reorganized CAD Settings.

--- src/lib/modules/CADModule/CADSettings.cpp
+++ src/lib/modules/CADModule/CADSettings.cpp
@@ -1,7 +1,7 @@
 #include "CADSettings.h"
 
-constexpr carl::cad::IntegerHandling smtrat::CADSettings1::integerHandling;
+constexpr carl::cad::IntegerHandling smtrat::CADSettingsReal::integerHandling;
 
-const bool smtrat::CADSettings1::dummy = SettingsManager::addModule("CAD1",
-	"integer_handling", carl::cad::IntegerHandling::SPLIT_EARLY, smtrat::CADSettings1::integerHandling
+const bool smtrat::CADSettingsReal::dummy = SettingsManager::addModule("CAD1",
+	"integer_handling", carl::cad::IntegerHandling::SPLIT_EARLY, smtrat::CADSettingsReal::integerHandling
 );
