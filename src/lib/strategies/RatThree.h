4ef486ce10c4cf95b08d2c47cf28ffa6e06d4f3f
Florian Corzilius <corzilius@cs.rwth-aachen.de>
2016-01-12 20:06:32 +0100

    changed preprocessing to fpp in ratThree and ratFour

--- src/lib/strategies/RatThree.h
+++ src/lib/strategies/RatThree.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include "../solver/Manager.h"
-#include "../modules/PreprocessingModule/PreprocessingModule.h"
+#include "../modules/FPPModule/FPPModule.h"
 #include "../modules/SATModule/SATModule.h"
 #include "../modules/LRAModule/LRAModule.h"
 #include "../modules/VSModule/VSModule.h"
@@ -30,7 +30,7 @@ namespace smtrat
         {
             setStrategy(
             {
-                addBackend<PreprocessingModule<PreprocessingSettings1>>(
+                addBackend<FPPModule<FPPSettings1>>(
                 {
                     addBackend<SATModule<SATSettings1>>(
                     {
@@ -38,9 +38,9 @@ namespace smtrat
                         {
                             addBackend<VSModule<VSSettings234>>(
                             {
-                                addBackend<CADModule<CADSettings1>>()
+                                addBackend<CADModule<CADSettingsGuessAndSplit>>()
                             }),
-                            addBackend<CADModule<CADSettings1>>()
+                            addBackend<CADModule<CADSettingsGuessAndSplit>>()
                         })
                     })
                 })
