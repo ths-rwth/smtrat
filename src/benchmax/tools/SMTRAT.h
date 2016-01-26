dd48dffe7a147b0626511676e2d14b03a754a12c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-01-26 16:12:51 +0100

    Added parsing of smtlib statistics.

--- src/benchmax/tools/SMTRAT.h
+++ src/benchmax/tools/SMTRAT.h
@@ -34,8 +34,23 @@ public:
 		return res;
 	}
 	
-	virtual void additionalResults(const fs::path&, BenchmarkResult&) const override {
-		std::cout << "SMTRAT: Getting additional results" << std::endl;
+	virtual void additionalResults(const fs::path&, BenchmarkResult& results) const override {
+		regex topRegex("\\(\\:(\\S+)\\s*\\(((?:\\:\\S+\\s*\\S+\\s*)+)\\)\\)");
+		regex subRegex("\\s*\\:(\\S+)\\s*(\\S+)");
+
+		auto topBegin = sregex_iterator(results.stdout.begin(), results.stdout.end(), topRegex);
+		auto topEnd = sregex_iterator();
+		for (auto i = topBegin; i != topEnd; ++i) {
+			const std::string& prefix = (*i)[1];
+			const std::string& subStdout = (*i)[2];
+
+			auto subBegin = sregex_iterator(subStdout.begin(), subStdout.end(), subRegex);
+			auto subEnd = sregex_iterator();
+			for (auto j = subBegin; j != subEnd; ++j) {
+				std::string name = prefix + "_" + std::string((*j)[1]);
+				results.additional.emplace(name, (*j)[2]);
+			}
+		}
 	}
 };
 
