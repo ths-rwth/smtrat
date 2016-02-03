4f907378261f4387a81616de16c3cc640d3816e6
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-02-03 17:05:55 +0100

    Added a status string to BenchmarkResult.

--- src/benchmax/results/BenchmarkResult.h
+++ src/benchmax/results/BenchmarkResult.h
@@ -8,14 +8,24 @@ namespace benchmax {
 
 struct BenchmarkResult {
 	int exitCode;
+	std::string status;
 	std::chrono::milliseconds time;
 	std::string stdout;
 	std::string stderr;
 	std::map<std::string, std::string> additional;
+	
+	template<typename Tool, typename TimeLimit>
+	void cleanup(const Tool& tool, const TimeLimit& limit) {
+		if (time > limit) {
+			status = "timeout";
+		} else {
+			status = tool->getStatus(*this);
+		}
+	}
 };
 
 inline std::ostream& operator<<(std::ostream& os, const BenchmarkResult& results) {
-	os << "(" << results.exitCode << ", " << results.time.count() << "ms)" << std::endl;
+	os << "(" << results.status << ", " << results.exitCode << ", " << results.time.count() << "ms)" << std::endl;
 	os << results.stdout << std::endl;
 	os << results.stderr << std::endl;
 	return os;
