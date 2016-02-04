1cd4574c70d688453f2a280f4214bce3ca6663cd
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-02-04 11:54:34 +0100

    Using new status flag for xml output.

--- src/benchmax/results/XMLWriter.h
+++ src/benchmax/results/XMLWriter.h
@@ -10,7 +10,7 @@ namespace benchmax {
 class XMLWriter {
 private:
 	std::ofstream mFile;
-	
+
 	void replace(std::string& s, const std::string& pattern, const std::string& replacement) const {
 		std::size_t pos = 0;
 		while ((pos = s.find(pattern, pos)) != std::string::npos) {
@@ -29,14 +29,14 @@ public:
 		mFile << "<?xml version=\"1.0\"?>" << std::endl;
 		mFile << "<benchmarksets>" << std::endl;
 	}
-	
+
 	void write(const std::map<const Tool*, std::size_t>& tools, const std::map<fs::path, std::size_t>& files, const std::map<std::pair<std::size_t,std::size_t>, BenchmarkResult>& results) {
 		mFile << "\t<solvers>" << std::endl;
 		for (const auto& tool: tools) {
 			mFile << "\t\t<solver solver_id=\"" << sanitize(tool.first->binary().native()) << "\" />" << std::endl;
 		}
 		mFile << "\t</solvers>" << std::endl;
-		
+
 		mFile << "\t<benchmarkset name=\"all\">" << std::endl;
 		for (const auto& file: files) {
 			mFile << "\t\t<benchmarkfile name=\"" << sanitize(removePrefix(file.first.native(), Settings::pathPrefix)) << "\">" << std::endl;
@@ -57,19 +57,7 @@ public:
 				mFile << "\t\t\t\t<results>" << std::endl;
 				mFile << "\t\t\t\t\t<result name=\"runtime\" type=\"msec\">" << milliseconds(it->second.time).count() << "</result>" << std::endl;
 				mFile << "\t\t\t\t\t<result name=\"exitcode\" type=\"int\">" << it->second.exitCode << "</result>" << std::endl;
-				mFile << "\t\t\t\t\t<result name=\"answer\" type=\"\">";
-				int exitcode = it->second.exitCode;
-				if (it->second.time > Settings::timeLimit) exitcode = 11;
-				switch (exitcode) {
-					case 2: mFile << "sat"; break;
-					case 3: mFile << "unsat"; break;
-					case 4: mFile << "unknown"; break;
-					case 5: mFile << "error"; break;
-					case 11: mFile << "timeout"; break;
-					case 12: mFile << "memout"; break;
-					default: mFile << "segfault";
-				}
-				mFile << "</result>" << std::endl;
+				mFile << "\t\t\t\t\t<result name=\"answer\" type=\"\">" << it->second.status << "</result>" << std::endl;
 				mFile << "\t\t\t\t</results>" << std::endl;
 				mFile << "\t\t\t</run>" << std::endl;
 			}
@@ -77,11 +65,11 @@ public:
 		}
 		mFile << "\t</benchmarkset>" << std::endl;
 	}
-	
+
 	~XMLWriter() {
 		mFile << "</benchmarksets>" << std::endl;
 		mFile.close();
 	}
 };
-	
+
 }
