87deb6a462ed98256e3165c1ed2e54ee7ef24e5f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-03-02 16:30:14 +0100

    Added a first test for cad.

---
+++ src/tests/cad/Test_Projection.cpp
@@ -0,0 +1,52 @@
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+#include "../../lib/datastructures/cad/projection/Projection.h"
+#include "../../lib/datastructures/cad/lifting/Sample.h"
+#include "../../lib/modules/NewCADModule/NewCADSettings.h"
+#include "../../lib/datastructures/cad/CAD.h"
+
+using namespace smtrat;
+using namespace smtrat::cad;
+
+BOOST_AUTO_TEST_SUITE(Test_Projection);
+
+BOOST_AUTO_TEST_CASE(Test_Projection_NO)
+{
+	carl::Variable x = carl::freshRealVariable("x");
+	carl::Variable y = carl::freshRealVariable("y");
+	Poly p = Poly(x*y)+Poly(y)+Rational(1);
+	Poly q = Poly(y*y*y)+Poly(x*x*y)+Rational(2);
+	
+	ProjectionT<NewCADSettings1> projection;
+	projection.reset({x,y});
+	projection.addPolynomial(p.toUnivariatePolynomial(x));
+	projection.addPolynomial(q.toUnivariatePolynomial(x));
+	std::cout << projection << std::endl;
+	
+	Sample s(RAN(1));
+	while (OptionalPoly op = projection.getPolyForLifting(1, s.liftedWith())) {
+		std::cout << *op << std::endl;
+	}
+
+	CAD<NewCADSettings1> cad({x,y});
+	cad.addConstraint(ConstraintT(p, carl::Relation::GEQ));
+	cad.addConstraint(ConstraintT(q, carl::Relation::LEQ));
+	cad.check();
+}
+
+BOOST_AUTO_TEST_CASE(Test_CAD)
+{
+	carl::Variable x = carl::freshRealVariable("x");
+	carl::Variable y = carl::freshRealVariable("y");
+	Poly p = Poly(x*y)+Poly(y)+Rational(1);
+	Poly q = Poly(y*y*y)+Poly(x*x*y)+Rational(2);
+	
+	CAD<NewCADSettings1> cad({x,y});
+	cad.addConstraint(ConstraintT(p, carl::Relation::GEQ));
+	cad.addConstraint(ConstraintT(q, carl::Relation::LEQ));
+	cad.check();
+}
+
+BOOST_AUTO_TEST_SUITE_END();
