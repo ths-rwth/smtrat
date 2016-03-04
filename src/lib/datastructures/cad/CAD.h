32969ec341035a97fbf4ca9a3bb6dffd24212130
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-03-04 11:05:48 +0100

    First working version of CAD.

--- src/lib/datastructures/cad/CAD.h
+++ src/lib/datastructures/cad/CAD.h
@@ -2,8 +2,6 @@
 
 #include <vector>
 
-#include <carl/core/RealAlgebraicNumberEvaluation.h>
-
 #include "Common.h"
 #include "projection/Projection.h"
 #include "lifting/LiftingTree.h"
@@ -23,64 +21,75 @@ namespace cad {
 			return mVariables.size();
 		}
 	public:
-		CAD(const Variables& vars):
-			mVariables(vars),
-			mProjection(),
-			mLifting(std::move(Variables(vars.rbegin(), vars.rend())))
-		{
-			mProjection.reset(vars);
+		void reset(const Variables& vars) {
+			mVariables = vars;
+			mProjection.reset(mVariables);
+			mLifting.reset(Variables(vars.rbegin(), vars.rend()));
 		}
 		void addConstraint(const ConstraintT& c) {
 			SMTRAT_LOG_DEBUG("smtrat.cad", "Adding " << c);
+			assert(!mVariables.empty());
 			mConstraints.push_back(c);
-			mProjection.addPolynomial(c.lhs().toUnivariatePolynomial(mVariables[0]));
+			mProjection.addPolynomial(c.lhs().toUnivariatePolynomial(mVariables.front()));
 		}
 		void removeConstraint(const ConstraintT& c) {
 			SMTRAT_LOG_DEBUG("smtrat.cad", "Removing " << c);
+			assert(!mVariables.empty());
 			switch (Settings::backtracking) {
 				case Backtracking::ORDERED:
 					assert(mConstraints.back() == c);
 					mConstraints.pop_back();
-					mProjection.removePolynomial(c.lhs());
+					mProjection.removePolynomial(c.lhs().toUnivariatePolynomial(mVariables.front()));
 				default:
 					assert(false);
 			}
 		}
 		
-		Answer checkFullSamples() {
+		Answer checkFullSamples(Assignment& assignment) {
+			SMTRAT_LOG_TRACE("smtrat.cad", "Full sample queue:" << std::endl << mLifting.printFullSamples());
 			while (mLifting.hasFullSamples()) {
 				auto it = mLifting.getNextFullSample();
 				auto m = mLifting.extractSampleMap(it);
+				assert(m.size() == it.depth());
 				for (const auto& c: mConstraints) {
 					SMTRAT_LOG_DEBUG("smtrat.cad", "Checking " << m << " against " << c);
-					auto res = carl::RealAlgebraicNumberEvaluation::evaluate(c.lhs(), m);
+					// TODO: m is cleared by the call to evaluate() ... 
+					auto assignment = m;
+					auto res = carl::RealAlgebraicNumberEvaluation::evaluate(c.lhs(), assignment);
 					bool sat = carl::evaluate(res, c.relation());
 					if (!sat) return Answer::UNSAT;
 				}
 				SMTRAT_LOG_INFO("smtrat.cad", "Found satisfying sample " << m);
+				assignment = m;
 				return Answer::SAT;
 			}
 			return Answer::UNSAT;
 		}
 		
-		Answer check() {
+		Answer check(Assignment& assignment) {
+			SMTRAT_LOG_DEBUG("smtrat.cad", "Current projection:" << std::endl << mProjection);
 			while (true) {
-				Answer res = checkFullSamples();
+				Answer res = checkFullSamples(assignment);
 				if (res == Answer::SAT) return Answer::SAT;
 				
+				if (!mLifting.hasNextSample()) break;
 				auto it = mLifting.getNextSample();
 				assert(0 <= it.depth() && it.depth() < dim());
 				Sample& s = *it;
 				SMTRAT_LOG_DEBUG("smtrat.cad", "Sample " << s << " at depth " << it.depth());
+				SMTRAT_LOG_DEBUG("smtrat.cad", "Current sample: " << mLifting.printSample(it));
 				auto poly = mProjection.getPolyForLifting(dim() - 1 - it.depth(), s.liftedWith());
 				if (poly) {
-					std::cout << "Lifting " << s << " with " << *poly << std::endl;
-					bool gotNewSamples = mLifting.liftSample(it, *poly);
-					if (!gotNewSamples) {
-						mProjection.projectNewPolynomial(dim() - 1 - it.depth());
-					}
+					SMTRAT_LOG_DEBUG("smtrat.cad", "Lifting " << s << " with " << *poly);
+					mLifting.liftSample(it, *poly);
 				} else {
-					mLifting.removeNextSample();
+					SMTRAT_LOG_DEBUG("smtrat.cad", "Got no polynomial for " << s << ", projecting into level " << (dim() - 1 - it.depth()) << " ...");
+					bool gotNewPolys = mProjection.projectNewPolynomial(dim() - 1 - it.depth());
+					if (gotNewPolys) {
+						mLifting.restoreRemovedSamples();
+					} else {
+						mLifting.removeNextSample();
+					}
 				}
 			}
 			return Answer::UNSAT;
