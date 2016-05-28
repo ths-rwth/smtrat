0aa1b1640ae5c6451f1b63147b65606610d666b6
Florian Corzilius <flo@172.20.10.2>
2016-05-28 18:50:56 +0200

    bugfix in strategy for smt-comp

--- src/lib/strategies/RatComp2016.h
+++ src/lib/strategies/RatComp2016.h
@@ -5,28 +5,26 @@
 
 #include "../solver/Manager.h"
 #include "../modules/FPPModule/FPPModule.h"
-#include "../modules/SATModule/SATModule.h"
-#include "../modules/CubeLIAModule/CubeLIAModule.h"
-#include "../modules/LRAModule/LRAModule.h"
-#include "../modules/VSModule/VSModule.h"
-#include "../modules/CADModule/CADModule.h"
-#include "../modules/SATModule/SATModule.h"
-#include "../modules/LRAModule/LRAModule.h"
-#include "../modules/VSModule/VSModule.h"
-#include "../modules/CADModule/CADModule.h"
-#include "../modules/FPPModule/FPPModule.h"
-#include "../modules/FPPModule/FPPModule.h"
-#include "../modules/SATModule/SATModule.h"
-#include "../modules/LRAModule/LRAModule.h"
-#include "../modules/ICPModule/ICPModule.h"
-#include "../modules/VSModule/VSModule.h"
-#include "../modules/CADModule/CADModule.h"
 #include "../modules/IncWidthModule/IncWidthModule.h"
 #include "../modules/IntBlastModule/IntBlastModule.h"
 #include "../modules/SATModule/SATModule.h"
 #include "../modules/LRAModule/LRAModule.h"
 #include "../modules/VSModule/VSModule.h"
 #include "../modules/CADModule/CADModule.h"
+#include "../modules/FPPModule/FPPModule.h"
+#include "../modules/SATModule/SATModule.h"
+#include "../modules/ICPModule/ICPModule.h"
+#include "../modules/VSModule/VSModule.h"
+#include "../modules/CADModule/CADModule.h"
+#include "../modules/FPPModule/FPPModule.h"
+#include "../modules/SATModule/SATModule.h"
+#include "../modules/CubeLIAModule/CubeLIAModule.h"
+#include "../modules/LRAModule/LRAModule.h"
+#include "../modules/SATModule/SATModule.h"
+#include "../modules/LRAModule/LRAModule.h"
+#include "../modules/FPPModule/FPPModule.h"
+#include "../modules/SATModule/SATModule.h"
+#include "../modules/LRAModule/LRAModule.h"
 
 namespace smtrat
 {
@@ -41,9 +39,19 @@ namespace smtrat
     class RatComp2016:
         public Manager
     {
+        static bool conditionEvaluation6( carl::Condition _condition )
+        {
+            return ( (carl::PROP_CONTAINS_NONLINEAR_POLYNOMIAL <= _condition) && (carl::PROP_CONTAINS_INTEGER_VALUED_VARS <= _condition) );
+        }
+
+        static bool conditionEvaluation16( carl::Condition _condition )
+        {
+            return ( (carl::PROP_CONTAINS_NONLINEAR_POLYNOMIAL <= _condition) &&  !(carl::PROP_CONTAINS_INTEGER_VALUED_VARS <= _condition) );
+        }
+
         static bool conditionEvaluation0( carl::Condition _condition )
         {
-            return (  !(carl::PROP_CONTAINS_NONLINEAR_POLYNOMIAL <= _condition) );
+            return (  !(carl::PROP_CONTAINS_NONLINEAR_POLYNOMIAL <= _condition) && (carl::PROP_CONTAINS_INTEGER_VALUED_VARS <= _condition) );
         }
 
         static bool conditionEvaluation1( carl::Condition _condition )
@@ -56,19 +64,9 @@ namespace smtrat
             return (  !(carl::PROP_IS_LITERAL_CONJUNCTION <= _condition) );
         }
 
-        static bool conditionEvaluation10( carl::Condition _condition )
+        static bool conditionEvaluation13( carl::Condition _condition )
         {
-            return ( (carl::PROP_CONTAINS_NONLINEAR_POLYNOMIAL <= _condition) );
-        }
-
-        static bool conditionEvaluation17( carl::Condition _condition )
-        {
-            return (  !(carl::PROP_CONTAINS_INTEGER_VALUED_VARS <= _condition) );
-        }
-
-        static bool conditionEvaluation11( carl::Condition _condition )
-        {
-            return ( (carl::PROP_CONTAINS_INTEGER_VALUED_VARS <= _condition) );
+            return (  !(carl::PROP_CONTAINS_NONLINEAR_POLYNOMIAL <= _condition) &&  !(carl::PROP_CONTAINS_INTEGER_VALUED_VARS <= _condition) );
         }
 
         public:
@@ -79,48 +77,6 @@ namespace smtrat
             {
                 addBackend<FPPModule<FPPSettings1>>(
                 {
-                    addBackend<SATModule<SATSettings1>>(
-                    {
-                        addBackend<CubeLIAModule<CubeLIASettings1>>(
-                        {
-                            addBackend<LRAModule<LRASettings1>>(
-                            {
-                                addBackend<VSModule<VSSettings234>>(
-                                {
-                                    addBackend<CADModule<CADSettingsSplitFirst>>()
-                                })
-                            })
-                        })
-                    }).condition( &conditionEvaluation1 ),
-                    addBackend<SATModule<SATSettings1>>(
-                    {
-                        addBackend<LRAModule<LRASettings1>>(
-                        {
-                            addBackend<VSModule<VSSettings234>>(
-                            {
-                                addBackend<CADModule<CADSettingsSplitFirst>>()
-                            })
-                        })
-                    }).condition( &conditionEvaluation2 )
-                }).condition( &conditionEvaluation0 ),
-                addBackend<FPPModule<FPPSettings1>>(
-                {
-                    addBackend<FPPModule<FPPSettings1>>(
-                    {
-                        addBackend<SATModule<SATSettings1>>(
-                        {
-                            addBackend<LRAModule<LRASettings1>>(
-                            {
-                                addBackend<ICPModule<ICPSettings1>>(
-                                {
-                                    addBackend<VSModule<VSSettings234>>(
-                                    {
-                                        addBackend<CADModule<CADSettingsSplitFirst>>()
-                                    })
-                                })
-                            })
-                        })
-                    }).condition( &conditionEvaluation17 ),
                     addBackend<IncWidthModule<IncWidthSettings1>>(
                     {
                         addBackend<IntBlastModule<IntBlastSettings2>>(
@@ -136,8 +92,42 @@ namespace smtrat
                                 })
                             })
                         })
-                    }).condition( &conditionEvaluation11 )
-                }).condition( &conditionEvaluation10 )
+                    })
+                }).condition( &conditionEvaluation6 ),
+                addBackend<FPPModule<FPPSettings1>>(
+                {
+                    addBackend<SATModule<SATSettings1>>(
+                    {
+                        addBackend<ICPModule<ICPSettings1>>(
+                        {
+                            addBackend<VSModule<VSSettings234>>(
+                            {
+                                addBackend<CADModule<CADSettingsSplitFirst>>()
+                            })
+                        })
+                    })
+                }).condition( &conditionEvaluation16 ),
+                addBackend<FPPModule<FPPSettings1>>(
+                {
+                    addBackend<SATModule<SATSettings1>>(
+                    {
+                        addBackend<CubeLIAModule<CubeLIASettings1>>(
+                        {
+                            addBackend<LRAModule<LRASettings1>>()
+                        })
+                    }).condition( &conditionEvaluation1 ),
+                    addBackend<SATModule<SATSettings1>>(
+                    {
+                        addBackend<LRAModule<LRASettings1>>()
+                    }).condition( &conditionEvaluation2 )
+                }).condition( &conditionEvaluation0 ),
+                addBackend<FPPModule<FPPSettings1>>(
+                {
+                    addBackend<SATModule<SATSettings1>>(
+                    {
+                        addBackend<LRAModule<LRASettings1>>()
+                    })
+                }).condition( &conditionEvaluation13 )
             });
         }
     };
