ed44ae01d45e81882606b07148792d580b6e8df2
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-06-10 16:10:54 +0200

    Added debug class to easily perform a projection.

---
+++ src/lib/datastructures/cad/debug/Settings.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../Common.h"
+#include "../Settings.h"
+
+namespace smtrat {
+namespace cad {
+namespace debug {
+	
+	struct DebugSettings {
+		static constexpr Incrementality incrementality = Incrementality::NONE;
+		static constexpr Backtracking backtracking = Backtracking::ORDERED;
+		
+		static constexpr ProjectionType projectionOperator = cad::ProjectionType::Brown;
+		static constexpr CoreHeuristic coreHeuristic = cad::CoreHeuristic::PreferProjection;
+		
+		static constexpr MISHeuristic misHeuristic = cad::MISHeuristic::GREEDY;
+		static constexpr std::size_t trivialSampleRadius = 1;
+		static constexpr bool simplifyProjectionByBounds = true;
+		
+		static constexpr SampleCompareStrategy sampleComparator = cad::SampleCompareStrategy::Integer;
+		static constexpr FullSampleCompareStrategy fullSampleComparator = cad::FullSampleCompareStrategy::Integer;
+		static constexpr RootSplittingStrategy rootSplittingStrategy = cad::RootSplittingStrategy::DEFAULT;
+	};
+	
+}
+}
+}
