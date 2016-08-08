15be6833727ec398725593ee8204508cb0d92330
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-08-08 11:32:59 +0200

    Better handling for equalities in CAD module.

--- src/lib/modules/NewCADModule/NewCADModule.cpp
+++ src/lib/modules/NewCADModule/NewCADModule.cpp
@@ -43,9 +43,31 @@ namespace smtrat
 	template<class Settings>
 	bool NewCADModule<Settings>::addCore( ModuleInput::const_iterator _subformula )
 	{
-		assert(_subformula->formula().getType() == carl::FormulaType::CONSTRAINT);
+		switch (_subformula->formula().getType()) {
+			case carl::FormulaType::CONSTRAINT: {
+				const ConstraintT& c = _subformula->formula().constraint();
+				carl::Variable v;
+				Rational r;
+				if (c.getAssignment(v, r)) {
+					mReplacer.addAssignment(v, r);
+				} else {
+					mReplacer.addConstraint(c);
+				}
+				break;
+			}
+			case carl::FormulaType::VARCOMPARE: {
+				const auto& vc = _subformula->formula().variableComparison();
+				if (vc.relation() == carl::Relation::EQ) {
+					mReplacer.addAssignment(vc.var(), vc.value());
+				} else {
+					//mReplacer.addConstraint(vc);
+				}
+				break;
+			}
+			default:
+				SMTRAT_LOG_ERROR("smtrat.cad", "Unsupported formula type for CAD: " << _subformula->formula().getType());
+		}
 		//mCAD.addConstraint(_subformula->formula().constraint());
-		mReplacer.addConstraint(_subformula->formula().constraint());
 		return true; // This should be adapted according to your implementation.
 	}
 	
