348f7ccaa49380078a9c82981fab4e05afc2ded7
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-09-24 12:55:57 +0200

    Fixed handling of Assignments in EqualityReplacer.

--- src/lib/datastructures/cad/helper/EqualityReplacer.h
+++ src/lib/datastructures/cad/helper/EqualityReplacer.h
@@ -18,19 +18,25 @@ public:
 private:
 	CAD& mCAD;
 	std::map<ConstraintT, boost::optional<ConstraintT>> mConstraints;
+	std::set<ConstraintT> mForwardedConstraints;
 	std::map<carl::Variable, ConstraintT> mAssignments;
 	Model mModel;
 	boost::optional<ConstraintT> mConflict;
 public:
 	EqualityReplacer(CAD& cad): mCAD(cad) {}
 	void addConstraint(const ConstraintT& c) {
+		SMTRAT_LOG_DEBUG("smtrat.cad", "Adding " << c);
 		mConstraints.emplace(c, boost::none);
 	}
 	void removeConstraint(const ConstraintT& c) {
+		SMTRAT_LOG_DEBUG("smtrat.cad", "Removing " << c);
 		auto it = mConstraints.find(c);
 		assert(it != mConstraints.end());
-		assert(it->second != boost::none);
-		mCAD.removeConstraint(*it->second);
+		if (it->second) {
+			SMTRAT_LOG_DEBUG("smtrat.cad", "Actually Erasing " << *it->second);
+			mCAD.removeConstraint(*it->second);
+			mForwardedConstraints.erase(*it->second);
+		}
 		mConstraints.erase(it);
 	}
 	void addAssignment(carl::Variable v, const ModelValue& n, const ConstraintT& c) {
@@ -43,6 +49,7 @@ public:
 		mModel.erase(it);
 		auto ait = mAssignments.find(v);
 		assert(ait != mAssignments.end());
+		SMTRAT_LOG_DEBUG("smtrat.cad", "Removing assignment " << ait->second);
 		mCAD.removeConstraint(ait->second);
 		mAssignments.erase(ait);
 	}
@@ -51,21 +58,33 @@ public:
 		for (auto& c: mConstraints) {
 			auto res = carl::model::substitute(c.first, mModel);
 			if (c.second) {
+				assert(c.second != boost::none);
 				if (res == c.second) continue;
+				SMTRAT_LOG_DEBUG("smtrat.cad", "Actually Erasing " << *c.second);
 				mCAD.removeConstraint(*c.second);
+				mForwardedConstraints.erase(*c.second);
 			}
-			c.second = res;
-			SMTRAT_LOG_INFO("smtrat.cad", c.first << " -> " << *c.second);
-			if (c.second->isConsistent() == 0) {
+			if (mForwardedConstraints.find(res) != mForwardedConstraints.end()) {
+				c.second = boost::none;
+				continue;
+			}
+			SMTRAT_LOG_INFO("smtrat.cad", c.first << " -> " << res);
+			if (res.isConsistent() == 0) {
 				mConflict = c.first;
 				return false;
 			}
-			if (c.second->isConsistent() == 2) {
+			if (res.isConsistent() == 2) {
+				c.second = res;
+				SMTRAT_LOG_DEBUG("smtrat.cad", "Actually Adding " << *c.second);
 				mCAD.addConstraint(*c.second);
+				mForwardedConstraints.insert(*c.second);
 			}
 		}
 		for (const auto& ass: mAssignments) {
+			if (mForwardedConstraints.find(ass.second) != mForwardedConstraints.end()) continue;
+			SMTRAT_LOG_DEBUG("smtrat.cad", "Adding Assignment " << ass.second);
 			mCAD.addConstraint(ass.second);
+			mForwardedConstraints.insert(ass.second);
 		}
 		return true;
 	}
