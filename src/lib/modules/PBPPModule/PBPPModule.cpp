64419c3b200e012c74e2ae2d949b032a00f0267b
marta.grobelna <marta.grobelna@rwth-aachen.de>
2016-11-23 15:13:42 +0100

    Adding PBPPModule and its strategy.

---
+++ src/lib/modules/PBPPModule/PBPPModule.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file PBPP.cpp
+ * @author YOUR NAME <YOUR EMAIL ADDRESS>
+ *
+ * @version 2016-11-23
+ * Created on 2016-11-23.
+ */
+
+#include "PBPPModule.h"
+
+namespace smtrat
+{
+	template<class Settings>
+	PBPPModule<Settings>::PBPPModule(const ModuleInput* _formula, RuntimeSettings*, Conditionals& _conditionals, Manager* _manager):
+		Module( _formula, _conditionals, _manager )
+#ifdef SMTRAT_DEVOPTION_Statistics
+		, mStatistics(Settings::moduleName)
+#endif
+	{}
+	
+	template<class Settings>
+	PBPPModule<Settings>::~PBPPModule()
+	{}
+	
+	template<class Settings>
+	bool PBPPModule<Settings>::informCore( const FormulaT& _constraint )
+	{
+		// Your code.
+		return true; // This should be adapted according to your implementation.
+	}
+	
+	template<class Settings>
+	void PBPPModule<Settings>::init()
+	{}
+	
+	template<class Settings>
+	bool PBPPModule<Settings>::addCore( ModuleInput::const_iterator _subformula )
+	{
+		// Your code.
+		return true; // This should be adapted according to your implementation.
+	}
+	
+	template<class Settings>
+	void PBPPModule<Settings>::removeCore( ModuleInput::const_iterator _subformula )
+	{
+		// Your code.
+	}
+	
+	template<class Settings>
+	void PBPPModule<Settings>::updateModel() const
+	{
+		mModel.clear();
+		if( solverState() == Answer::SAT )
+		{
+			// Your code.
+		}
+	}
+	
+	template<class Settings>
+	Answer PBPPModule<Settings>::checkCore()
+	{
+		// Your code.
+		return Answer::UNKNOWN; // This should be adapted according to your implementation.
+	}
+}
+
+#include "Instantiation.h"
