7a3ae14da85281bdce6d54f8481cf6e5cae35651
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-02-06 15:40:52 +0100

    Added another PB strategy employing the ICP module.

---
+++ src/lib/strategies/PBPPStrategy2.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../solver/Manager.h"
+
+#include "../modules/LRAModule/LRAModule.h"
+#include "../modules/PBPPModule/PBPPModule.h"
+#include "../modules/SATModule/SATModule.h"
+
+
+namespace smtrat
+{
+    class PBPPStrategy2:
+        public Manager
+    {
+        public:
+            PBPPStrategy2(): Manager() {
+				setStrategy({
+					addBackend<PBPPModule<PBPPSettings1>>(
+						addBackend<SATModule<SATSettings1>>(
+							addBackend<ICPModule<ICPSettings1>>(
+								addBackend<LRAModule<LRASettings1>>()
+							)
+						)
+					),
+				});
+			}
+    };
+}    // namespace smtrat
