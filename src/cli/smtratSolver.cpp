5eb4960a42e7ebe6ebe819fe94f02839294bfcf7
muvlon <muvlon@hentzes.de>
2017-03-13 17:18:01 +0100

    filter logging for misgeneration debugging

--- src/cli/smtratSolver.cpp
+++ src/cli/smtratSolver.cpp
@@ -293,12 +293,7 @@ int main( int argc, char* argv[] )
 		("smtrat.preprocessing", carl::logging::LogLevel::LVL_DEBUG)
 	;
 	carl::logging::logger().filter("stdout")
-		("smtrat", carl::logging::LogLevel::LVL_DEBUG)
-		("smtrat.module", carl::logging::LogLevel::LVL_INFO)
-		("smtrat.parser", carl::logging::LogLevel::LVL_INFO)
-		("smtrat.cad", carl::logging::LogLevel::LVL_DEBUG)
-		("smtrat.preprocessing", carl::logging::LogLevel::LVL_DEBUG)
-		("smtrat.strategygraph", carl::logging::LogLevel::LVL_DEBUG)
+		("smtrat.mis", carl::logging::LogLevel::LVL_DEBUG)
 	;
 #endif
 	SMTRAT_LOG_INFO("smtrat", "Starting smtrat.");
