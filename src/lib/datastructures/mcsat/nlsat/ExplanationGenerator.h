7a2dc2418db1053066a70acf8db1948ae708e784
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-03-23 11:44:09 +0100

    Adding files at a new position, starting to restructure nlsat.

---
+++ src/lib/datastructures/mcsat/nlsat/ExplanationGenerator.h
@@ -0,0 +1,173 @@
+#pragma once
+
+#include "../../../Common.h"
+#include "../../cad/projection/Projection.h"
+
+#include <carl/util/Common.h>
+
+namespace smtrat {
+namespace nlsat {
+
+class ExplanationGenerator {
+private:
+	using RAN = carl::RealAlgebraicNumber<Rational>;
+	struct ProjectionSettings: public cad::BaseSettings {	
+		static constexpr cad::Incrementality incrementality = cad::Incrementality::NONE;
+		static constexpr cad::Backtracking backtracking = cad::Backtracking::ORDERED;
+	};
+
+	std::vector<FormulaT> mConstraints;
+	cad::CADConstraints<ProjectionSettings::backtracking> mCADConstraints;
+	cad::ProjectionT<ProjectionSettings> mProjection;
+	Model mModel;
+	carl::Variable mTmpVar = carl::freshRealVariable("__z");
+	
+	bool isEqual(const RAN& ran, const ModelValue& mv) const {
+		if (mv.isRational()) return ran == RAN(mv.asRational());
+		if (mv.isRAN()) return ran == mv.asRAN();
+		assert(false);
+		return false;
+	}
+	bool isGreater(const RAN& ran, const ModelValue& mv) const {
+		if (mv.isRational()) return ran > RAN(mv.asRational());
+		if (mv.isRAN()) return ran > mv.asRAN();
+		assert(false);
+		return false;
+	}
+	FormulaT buildFormulaFromVC(VariableComparisonT&& vc) const {
+		auto constraint = vc.asConstraint();
+		if (constraint) return FormulaT(*constraint);
+		else return FormulaT(std::move(vc));
+	}
+	template<typename MV>
+	FormulaT buildEquality(carl::Variable v, const MV& mv) const {
+		SMTRAT_LOG_TRACE("smtrat.nlsat", "building: " << v << " = " << MultivariateRootT(mv.first, mv.second, mTmpVar));
+		return buildFormulaFromVC(VariableComparisonT(v, MultivariateRootT(mv.first, mv.second, mTmpVar), carl::Relation::EQ));
+	}
+	template<typename MV>
+	FormulaT buildBelow(carl::Variable v, const MV& mv) const {
+		SMTRAT_LOG_TRACE("smtrat.nlsat", "building: " << v << " < " << MultivariateRootT(mv.first, mv.second, mTmpVar));
+		return buildFormulaFromVC(VariableComparisonT(v, MultivariateRootT(mv.first, mv.second, mTmpVar), carl::Relation::LESS));
+	}
+	template<typename MV>
+	FormulaT buildAbove(carl::Variable v, const MV& mv) const {
+		SMTRAT_LOG_TRACE("smtrat.nlsat", "building: " << v << " > " << MultivariateRootT(mv.first, mv.second, mTmpVar));
+		return buildFormulaFromVC(VariableComparisonT(v, MultivariateRootT(mv.first, mv.second, mTmpVar), carl::Relation::GREATER));
+	}
+	
+	void generateBoundsFor(FormulasT& res, carl::Variable var, std::size_t level, const Model& model) const {
+		auto val = mModel.evaluated(var);
+		assert(val.isRational() || val.isRAN());
+		RAN value = val.isRational() ? RAN(val.asRational()) : val.asRAN();
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Generating bounds for " << var << " = " << value);
+		boost::optional<std::pair<RAN,FormulaT>> lower;
+		boost::optional<std::pair<RAN,FormulaT>> upper;
+
+		for (std::size_t pid = 0; pid < mProjection.size(level); pid++) {
+			const auto& poly = mProjection.getPolynomialById(level, pid);
+			auto psubs = carl::model::substitute(poly, model);
+			if (psubs.isZero()) continue;
+			auto list = carl::model::realRoots(poly, model);
+			if (!list) {
+				// The polynomial vanished at this point
+				continue;
+			}
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Looking at " << poly << " with roots " << list);
+			std::size_t rootID = 1;
+			for (const auto& root: *list) {
+				auto param = std::make_pair(Poly(carl::UnivariatePolynomial<Poly>(mTmpVar, poly.coefficients())), rootID);
+				SMTRAT_LOG_TRACE("smtrat.nlsat", root << " -> " << param);
+				if (root < value) {
+					if (!lower || (root > lower->first)) {
+						lower = std::make_pair(root, buildAbove(var, param));
+						SMTRAT_LOG_TRACE("smtrat.nlsat", "new lower bound: " << lower->second);
+					}
+				} else if (root == value) {
+					lower = std::make_pair(root, buildEquality(var, param));
+					upper = lower;
+					SMTRAT_LOG_TRACE("smtrat.nlsat", "new exact root: " << lower->second);
+				} else {
+					if (!upper || (root < upper->first)) {
+						upper = std::make_pair(root, buildBelow(var, param));
+						SMTRAT_LOG_TRACE("smtrat.nlsat", "new upper bound: " << upper->second);
+					}
+				}
+				rootID++;
+			}
+		}
+		if (lower) {
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Lower bound:" << lower->second);
+			res.push_back(lower->second);
+		}
+		if (upper && lower != upper) {
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Upper bound:" << upper->second);
+			res.push_back(upper->second);
+		}
+	}
+public:
+	ExplanationGenerator(const std::vector<FormulaT>& constraints, const std::vector<carl::Variable>& vars, const Model& model):
+		mConstraints(constraints),
+		mCADConstraints(
+			[&](const auto& p, std::size_t cid, bool isBound){ mProjection.addPolynomial(mProjection.normalize(p), cid, isBound); },
+			[&](const auto& p, std::size_t cid, bool isBound){ mProjection.removePolynomial(mProjection.normalize(p), cid, isBound); }
+		),
+		mProjection(mCADConstraints), 
+		mModel(model)
+	{
+		SMTRAT_LOG_TRACE("smtrat.nlsat", "Reset to " << vars);
+		mCADConstraints.reset(vars);
+		mProjection.reset();
+		for (const auto& f: mConstraints) {
+			if (f.getType() == carl::FormulaType::CONSTRAINT) {
+				SMTRAT_LOG_DEBUG("smtrat.nlsat", "Adding " << f);
+				mCADConstraints.add(f.constraint());
+			} else if (f.getType() == carl::FormulaType::VARCOMPARE) {
+				SMTRAT_LOG_DEBUG("smtrat.nlsat", "Adding bound " << f);
+			} else if (f.getType() == carl::FormulaType::VARASSIGN) {
+				SMTRAT_LOG_DEBUG("smtrat.nlsat", "Adding assignment " << f);
+			} else {
+				SMTRAT_LOG_ERROR("smtrat.nlsat", "Unsupported formula type: " << f);
+				assert(false);
+			}
+		}
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Projection is" << std::endl << mProjection);
+	}
+	void generateExplanation(const FormulaT& f, std::vector<FormulasT>& explanation) const {
+		FormulasT subs;
+		Model m;
+		explanation.resize(mCADConstraints.vars().size());
+		// Start from the bottom to incrementally build up model m and generate bound constraints.
+		for (std::size_t level = mCADConstraints.vars().size() - 1; level > 0; level--) {
+			carl::Variable var = mCADConstraints.vars()[level];
+			generateBoundsFor(explanation[level-1], var, level+1, m);
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Cell bounds for " << var << ": " << explanation[level-1]);
+			m.emplace(var, mModel.evaluated(var));
+		}
+		for (const auto& c: mConstraints) {
+			if (c == f.negated()) continue;
+			explanation.back().emplace_back(c);
+		}
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Final: " << explanation.back() << " -> " << f);
+	}
+	
+	FormulaT getExplanation(const FormulaT& f) const {
+		std::vector<FormulasT> expl;
+		generateExplanation(f, expl);
+		FormulasT res;
+		for (const auto& e: expl) {
+			for (const auto& f: e) {
+				if (f.getType() == carl::FormulaType::VARCOMPARE) {
+					res.emplace_back(f.variableComparison().invertRelation());
+				} else {
+					res.emplace_back(f.negated());
+				}
+			}
+		}
+		//std::cout << res << " => " << f << std::endl;
+		res.emplace_back(f);
+		return FormulaT(carl::FormulaType::OR, std::move(res));
+	}
+};
+
+}
+}
---
+++ src/lib/datastructures/mcsat/nlsat/LemmaBuilder.h
@@ -0,0 +1,151 @@
+#pragma once
+
+#include "../../../Common.h"
+
+#include "ExplanationGenerator.h"
+
+#include <carl/util/Common.h>
+#include <carl/util/vector_util.h>
+
+namespace smtrat {
+namespace nlsat {
+
+enum class LemmaStrategy { ORIGINAL, NEW };
+
+namespace LemmaHelper {
+	using Assignment = std::vector<std::pair<carl::Variable,FormulaT>>;
+	
+	inline void collectAssignments(const Assignment& assignment, FormulasT& target, const carl::Variables& vars, bool negated = false) {
+		for (const auto& ass: assignment) {
+			if (vars.count(ass.first) > 0) {
+				target.push_back((negated ? ass.second.negated() : ass.second));
+			}
+		}
+	}
+	inline void collectAssignments(const Assignment& assignment, FormulasT& target, const FormulasT& formulas, bool negated = false) {
+		carl::Variables vars;
+		for (const auto& sub: formulas) sub.arithmeticVars(vars);
+		collectAssignments(assignment, target, vars, negated);
+	}
+	inline void collectAssignments(const Assignment& assignment, FormulasT& target, const std::vector<FormulasT>& formulas, bool negated = false) {
+		carl::Variables vars;
+		SMTRAT_LOG_TRACE("smtrat.nlsat", "Collecting variables from " << formulas);
+		for (const auto& sub: formulas) {
+			for (const auto& subsub: sub) {
+				subsub.arithmeticVars(vars);
+			}
+		}
+		SMTRAT_LOG_TRACE("smtrat.nlsat", "Collecting assignments for " << vars);
+		collectAssignments(assignment, target, vars, negated);
+	}
+	
+	inline FormulaT buildImplication(FormulasT&& premise, const FormulaT& conclusion, bool skipNegation = false) {
+		if (!skipNegation) {
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Adding " << premise << " -> " << conclusion);
+			for (auto& p: premise) p = p.negated();
+		} else {
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Adding lemma " << premise << ", " << conclusion);
+		}
+		premise.push_back(conclusion);
+		FormulaT res(carl::FormulaType::OR, std::move(premise));
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Clause: " << res);
+		return res;
+	}
+}
+
+class LemmaBuilder {
+private:
+	const LemmaHelper::Assignment& mAssignment;
+	std::vector<FormulasT> mExplanation;
+	FormulaT mConclusion;
+	
+	template<typename Callback>
+	void generate_new_assignmentPropagation(const FormulasT& borders, const Callback& cb) {
+		FormulasT base;
+		LemmaHelper::collectAssignments(mAssignment, base, borders, true);
+		FormulasT other = base;
+		other.back() = other.back().negated();
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Generating assignment propagators");
+		for (const auto& b: borders) {
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Propagating " << b);
+			auto tmpb = base;
+			auto tmpo = other;
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", tmpo << " -> " << b);
+			cb(LemmaHelper::buildImplication(std::move(tmpo), b));
+			FormulaT bneg = b.negated();
+			if (b.getType() == carl::FormulaType::VARCOMPARE) {
+				bneg = FormulaT(b.variableComparison().invertRelation());
+			}
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", tmpb << " -> " << bneg);
+			cb(LemmaHelper::buildImplication(std::move(tmpb), bneg));
+			//cb(LemmaHelper::buildImplication({b}, bneg));
+			//cb(LemmaHelper::buildImplication({bneg}, b));
+		}
+	}
+	template<typename Callback>
+	void generate_new_mainPropagation(const Callback& cb) {
+		cb(LemmaHelper::buildImplication(carl::vector_concat(mExplanation), mConclusion));
+	}
+	template<typename Callback>
+	void generate_new(const Callback& cb) {
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Generating explanation with new strategy");
+		for (const auto& expl: mExplanation) {
+			if (expl.empty()) continue;
+			SMTRAT_LOG_DEBUG("smtrat.nlsat", "Processing borders: " << expl);
+			generate_new_assignmentPropagation(expl, cb);
+		}
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Using " << mConclusion.negated() << " for R-propagation");
+		FormulasT base;
+		LemmaHelper::collectAssignments(mAssignment, base, mExplanation);
+		cb(LemmaHelper::buildImplication(std::move(base), mConclusion));
+	}
+	
+	template<typename Callback>
+	void generate_original(const Callback& cb) {
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Generating explanation with original strategy");
+		for (std::size_t i = 0; i < mExplanation.size() - 1; i++) {
+			const auto& expl = mExplanation[i];
+			if (expl.empty()) continue;
+			//SMTRAT_LOG_DEBUG("smtrat.nlsat", "Implying borders: " << expl);
+			//generate_new_assignmentPropagation(expl, cb);
+		}
+		// Explanation as detailed in the nlsat paper
+		//SMTRAT_LOG_DEBUG("smtrat.nlsat", "Generating actual explanation");
+		FormulasT base;
+		//LemmaHelper::collectAssignments(mAssignment, base, mExplanation, true);
+		//LemmaHelper::collectAssignments(mAssignment, base, { mConclusion }, true);
+		//SMTRAT_LOG_DEBUG("smtrat.nlsat", "Collected assignment: " << base);
+		SMTRAT_LOG_DEBUG("smtrat.nlsat", "Explanation: " << mExplanation);
+		for (const auto& expl: mExplanation) {
+			for (const auto& e: expl) {
+				if (e.getType() == carl::FormulaType::VARCOMPARE) {
+					base.emplace_back(e.variableComparison().invertRelation());
+				} else {
+					base.emplace_back(e.negated());
+				}
+			}
+		}
+		//carl::vector_concat(base, mExplanation);
+		cb(LemmaHelper::buildImplication(std::move(base), mConclusion, true));
+	}
+	
+public:
+	LemmaBuilder(const LemmaHelper::Assignment& assignment, const FormulaT& F, const ExplanationGenerator& eg): mAssignment(assignment), mConclusion(F) {
+		eg.generateExplanation(mConclusion, mExplanation);
+	}
+
+	template<typename Callback>
+	void generateLemmas(const Callback& cb, LemmaStrategy strategy) {
+		switch (strategy) {
+			case LemmaStrategy::NEW:
+				generate_new(cb);
+				break;
+			case LemmaStrategy::ORIGINAL:
+				generate_original(cb);
+				break;
+		}
+	}
+};
+	
+}
+}
