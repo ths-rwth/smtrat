e7ade29d51dad7bedd4608f9abf462cc2a77cff4
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-05-24 17:55:26 +0200

    Fixed warnings about unused parameters.

--- src/lib/modules/PBGaussModule/PBGaussModule.cpp
+++ src/lib/modules/PBGaussModule/PBGaussModule.cpp
@@ -23,7 +23,7 @@ namespace smtrat
 	{}
 	
 	template<class Settings>
-	bool PBGaussModule<Settings>::informCore( const FormulaT& _constraint )
+	bool PBGaussModule<Settings>::informCore( const FormulaT& )
 	{
 
 		return true;
@@ -34,13 +34,13 @@ namespace smtrat
 	{}
 	
 	template<class Settings>
-	bool PBGaussModule<Settings>::addCore( ModuleInput::const_iterator _subformula )
+	bool PBGaussModule<Settings>::addCore( ModuleInput::const_iterator )
 	{
 		return true;
 	}
 	
 	template<class Settings>
-	void PBGaussModule<Settings>::removeCore( ModuleInput::const_iterator _subformula )
+	void PBGaussModule<Settings>::removeCore( ModuleInput::const_iterator )
 	{}
 	
 	template<class Settings>
