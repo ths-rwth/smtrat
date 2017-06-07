05b2321aa328eb5fafdc1e70dd97d8e8072956c8
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-06-07 14:34:48 +0200

    cleanup in XMLWriter.

--- src/benchmax/results/XMLWriter.h
+++ src/benchmax/results/XMLWriter.h
@@ -4,30 +4,13 @@
 #include <map>
 #include <string>
 #include "../utils/durations.h"
+#include "../utils/strings.h"
 
 namespace benchmax {
 
 class XMLWriter {
 private:
 	std::ofstream mFile;
-	
-	struct Filename: public fs::path {
-		static std::string removeDanglingSlashes(const std::string& s) {
-			std::string res = s;
-			if (res.front() == '/') res = res.substr(1);
-			if (res.back() == '/') res = res.substr(0, res.length() - 1);
-			return res;
-		}
-		Filename(const fs::path& p): fs::path(p) {}
-		std::string filename() const {
-			return removeDanglingSlashes(fs::path::filename().native());
-		}
-		std::string removePrefix(const fs::path& baseDir) const {
-			std::string prefix = baseDir.native();
-			assert(prefix == native().substr(0, prefix.length()));
-			return removeDanglingSlashes(native().substr(prefix.length()));
-		}
-	};
 
 	void replace(std::string& s, const std::string& pattern, const std::string& replacement) const {
 		std::size_t pos = 0;
@@ -43,6 +26,12 @@ private:
 		if (eliminateSlashes) replace(res, "/", ".");
 		return res;
 	}
+	std::string sanitizeTool(const Tool* tool) const {
+		return sanitize(removePrefix(tool->binary().native(), Settings::toolsPrefix), true);
+	}
+	std::string sanitizeFile(const fs::path& file, const std::string& prefix) const {
+		return sanitize(removePrefix(file.native(), prefix), true);
+	}
 public:
 	XMLWriter(const std::string& filename): mFile(filename) {
 		mFile << "<?xml version=\"1.0\"?>" << std::endl;
@@ -53,23 +42,23 @@ public:
 	void write(const std::map<const Tool*, std::size_t>& tools, const Results& results) {
 		mFile << "\t<solvers>" << std::endl;
 		std::set<std::string> toolNames;
-		for (const auto& tool: tools) toolNames.insert(sanitize(Filename(tool.first->binary()).filename(), true));
+		for (const auto& tool: tools) toolNames.insert(sanitizeTool(tool.first));
 		for (const auto& tool: toolNames) {
 			mFile << "\t\t<solver solver_id=\"" << tool << "\" />" << std::endl;
 		}
 		mFile << "\t</solvers>" << std::endl;
 		
 		for (const auto& res: results) {
-			Filename setDir = res.first;
-			mFile << "\t<benchmarkset name=\"" << sanitize(setDir.removePrefix(Settings::pathPrefix), true) << "\">" << std::endl;
+			fs::path setBaseDir = res.first;
+			mFile << "\t<benchmarkset name=\"" << sanitizeFile(setBaseDir, Settings::pathPrefix) << "\">" << std::endl;
 			for (const auto& file: res.second.files) {
-				Filename filename = file.first;
-				mFile << "\t\t<benchmarkfile name=\"" << sanitize(filename.removePrefix(setDir)) << "\">" << std::endl;
+				fs::path filename = file.first;
+				mFile << "\t\t<benchmarkfile name=\"" << sanitizeFile(filename, setBaseDir.native()) << "\">" << std::endl;
 				for (const auto& tool: tools) {
 					std::pair<std::size_t, std::size_t> resultID(tool.second, file.second);
 					auto it = res.second.data.find(resultID);
 					if (it == res.second.data.end()) continue;
-					mFile << "\t\t\t<run solver_id=\"" << sanitize(Filename(tool.first->binary()).filename(), true) << "\" timeout=\"" << seconds(Settings::timeLimit).count() << "s\">" << std::endl;
+					mFile << "\t\t\t<run solver_id=\"" << sanitizeTool(tool.first) << "\" timeout=\"" << seconds(Settings::timeLimit).count() << "s\">" << std::endl;
 					if (!it->second.additional.empty()) {
 						mFile << "\t\t\t\t<runtimestats>" << std::endl;
 						mFile << "\t\t\t\t\t<module name=\"All\">" << std::endl;
