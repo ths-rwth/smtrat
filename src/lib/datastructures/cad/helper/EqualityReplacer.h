fa7b265fc8fb8326d54548a998a74e33f323fba1
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-06-15 12:00:16 +0200

    Changing logging channel for EqualityReplacer.

--- src/lib/datastructures/cad/helper/EqualityReplacer.h
+++ src/lib/datastructures/cad/helper/EqualityReplacer.h
@@ -38,7 +38,7 @@ public:
 	 * It is forwarded with the next call to commit().
 	 */
 	void addConstraint(const ConstraintT& c) {
-		SMTRAT_LOG_DEBUG("smtrat.cad", "Adding " << c);
+		SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Adding " << c);
 		mConstraints.emplace(c, boost::none);
 	}
 	/**
@@ -47,11 +47,11 @@ public:
 	 * If it has already been forwarded, it is removed immediately.
 	 */
 	void removeConstraint(const ConstraintT& c) {
-		SMTRAT_LOG_DEBUG("smtrat.cad", "Removing " << c);
+		SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Removing " << c);
 		auto it = mConstraints.find(c);
 		assert(it != mConstraints.end());
 		if (it->second) {
-			SMTRAT_LOG_DEBUG("smtrat.cad", "Actually Erasing " << *it->second);
+			SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Actually Erasing " << *it->second);
 			mCAD.removeConstraint(*it->second);
 			mForwardedConstraints.erase(*it->second);
 		}
@@ -63,13 +63,13 @@ public:
 	 * It is used for simplication in the next call to commit().
 	 */
 	void addAssignment(carl::Variable v, const ModelValue& n, const ConstraintT& c) {
-		SMTRAT_LOG_DEBUG("smtrat.cad", "Adding " << c);
+		SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Adding " << c);
 		auto it = mAssignments.find(v);
 		if (it == mAssignments.end()) {
 			mModel.emplace(v, n);
 			mAssignments.emplace(v, c);
 		} else {
-			SMTRAT_LOG_DEBUG("smtrat.cad", "Is duplicate " << c);
+			SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Is duplicate " << c);
 			mDuplicateAssignments.emplace(c, std::make_pair(v, n));
 		}
 	}
@@ -79,7 +79,7 @@ public:
 	 * Simplified constraints are updated in the next call to commit().
 	 */
 	void removeAssignment(carl::Variable v, const ConstraintT& c) {
-		SMTRAT_LOG_DEBUG("smtrat.cad", "Removing assignment for " << v);
+		SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Removing assignment for " << v);
 		auto dait = mDuplicateAssignments.find(c);
 		if (dait != mDuplicateAssignments.end()) {
 			mDuplicateAssignments.erase(dait);
@@ -90,7 +90,7 @@ public:
 			mModel.erase(it);
 			auto ait = mAssignments.find(v);
 			assert(ait != mAssignments.end());
-			SMTRAT_LOG_DEBUG("smtrat.cad", "Removing assignment " << ait->second);
+			SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Removing assignment " << ait->second);
 			mCAD.removeConstraint(ait->second);
 			mForwardedConstraints.erase(ait->second);
 			mAssignments.erase(ait);
@@ -100,29 +100,29 @@ public:
 	 * Actually commits new constraints and simplications to CAD.
 	 */
 	bool commit() {
-		SMTRAT_LOG_DEBUG("smtrat.cad", "Commit ");
+		SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Commit ");
 		for (auto it = mDuplicateAssignments.begin(); it != mDuplicateAssignments.end();) {
-			SMTRAT_LOG_DEBUG("smtrat.cad", "Looking at duplicate " << it->first);
+			SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Looking at duplicate " << it->first);
 			if (mAssignments.find(it->second.first) == mAssignments.end()) {
-				SMTRAT_LOG_DEBUG("smtrat.cad", "Duplicate is now first one, adding assignment");
+				SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Duplicate is now first one, adding assignment");
 				addAssignment(it->second.first, it->second.second, it->first);
 				it = mDuplicateAssignments.erase(it);
 			} else {
-				SMTRAT_LOG_DEBUG("smtrat.cad", "Model: " << mModel);
+				SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Model: " << mModel);
 				auto res = carl::model::evaluate(it->first, mModel);
-				SMTRAT_LOG_DEBUG("smtrat.cad", "Evaluated to " << res);
+				SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Evaluated to " << res);
 				assert(res.isBool());
 				if (!res.asBool()) {
-					SMTRAT_LOG_DEBUG("smtrat.cad", "Duplicate constraint is a conflict: " << it->first);
+					SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Duplicate constraint is a conflict: " << it->first);
 					mConflict = it->first;
 					return false;
 				} else {
-					SMTRAT_LOG_DEBUG("smtrat.cad", "Evaluated to true:" << res);
+					SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Evaluated to true:" << res);
 				}
 				it++;
 			}
 		}
-		SMTRAT_LOG_INFO("smtrat.cad", "Using " << mModel << " to simplify");
+		SMTRAT_LOG_INFO("smtrat.cad.eqreplacer", "Using " << mModel << " to simplify");
 		for (auto& c: mConstraints) {
 			auto res = carl::model::substitute(c.first, mModel);
 			if (c.second) {
@@ -130,17 +130,17 @@ public:
 				assert(c.second != boost::none);
 				if (res == c.second) continue;
 				// Old simplication is invalid, remove it.
-				SMTRAT_LOG_DEBUG("smtrat.cad", "Actually Erasing " << *c.second);
+				SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Actually Erasing " << *c.second);
 				mCAD.removeConstraint(*c.second);
 				mForwardedConstraints.erase(*c.second);
 				c.second = boost::none;
 			}
 			if (mForwardedConstraints.find(res) != mForwardedConstraints.end()) {
 				// The (simplified?) constraint has already been forwarded.
-				SMTRAT_LOG_DEBUG("smtrat.cad", "Not considering " << res);
+				SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Not considering " << res);
 				continue;
 			}
-			SMTRAT_LOG_INFO("smtrat.cad", c.first << " -> " << res);
+			SMTRAT_LOG_INFO("smtrat.cad.eqreplacer", c.first << " -> " << res);
 			if (res.isConsistent() == 0) {
 				// The constraint is trivially false under the model.
 				mConflict = c.first;
@@ -149,7 +149,7 @@ public:
 			if (res.isConsistent() == 2) {
 				// The constraint is forwarded to the CAD.
 				c.second = res;
-				SMTRAT_LOG_DEBUG("smtrat.cad", "Actually Adding " << *c.second);
+				SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Actually Adding " << *c.second);
 				mCAD.addConstraint(*c.second);
 				mForwardedConstraints.insert(*c.second);
 			}
@@ -157,7 +157,7 @@ public:
 		for (const auto& ass: mAssignments) {
 			// Forward the assignment to the CAD.
 			if (mForwardedConstraints.find(ass.second) != mForwardedConstraints.end()) continue;
-			SMTRAT_LOG_DEBUG("smtrat.cad", "Adding Assignment " << ass.second);
+			SMTRAT_LOG_DEBUG("smtrat.cad.eqreplacer", "Adding Assignment " << ass.second);
 			mCAD.addConstraint(ass.second);
 			mForwardedConstraints.insert(ass.second);
 		}
