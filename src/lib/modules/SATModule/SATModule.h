7787f091c75eb9d4c56ee14832a8bbb7d2cf3272
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-11-08 12:37:56 +0100

    Not negating.

--- src/lib/modules/SATModule/SATModule.h
+++ src/lib/modules/SATModule/SATModule.h
@@ -907,7 +907,7 @@ namespace smtrat
 				SMTRAT_LOG_DEBUG("smtrat.sat", "Handling theory conflict clause " << clause);
 				//CARL_CHECKPOINT("nlsat", "theory-conflict", clause);
 				for (const auto& c: clause) {
-					explanation.push(neg(createLiteral(c)));
+					explanation.push(createLiteral(c));
 				}
 				SMTRAT_LOG_DEBUG("smtrat.sat", "Adding clause " << explanation);
 				addClause(explanation, Minisat::LEMMA_CLAUSE);
