dd0a07aec83d951dc8fd4e0fed0dcb838cb42f75
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-11-30 10:34:24 +0100

    Adding a simple module using CoCoA for groebner bases.

---
+++ src/lib/modules/CoCoAGBModule/CoCoAGBSettings.h
@@ -0,0 +1,22 @@
+/**
+ * @file CoCoAGBSettings.h
+ * @author YOUR NAME <YOUR EMAIL ADDRESS>
+ *
+ * @version 2017-11-29
+ * Created on 2017-11-29.
+ */
+
+#pragma once
+
+namespace smtrat
+{
+	struct CoCoAGBSettings1
+	{
+		/// Name of the Module
+		static constexpr auto moduleName = "CoCoAGBModule<CoCoAGBSettings1>";
+		/**
+		 * Example for a setting.
+		 */
+		static const bool convert_inequalities = false;
+	};
+}
