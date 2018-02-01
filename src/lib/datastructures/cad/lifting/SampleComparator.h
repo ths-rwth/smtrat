651f3079d64c20dcad73e9adc5c6bd5fe764ff8b
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2018-02-01 11:37:22 +0100

    Restructured sample comparators, improved flexibility.

--- src/lib/datastructures/cad/lifting/SampleComparator.h
+++ src/lib/datastructures/cad/lifting/SampleComparator.h
@@ -6,53 +6,98 @@
 
 namespace smtrat {
 namespace cad {
+
+/**
+ * Contains comparison operators for samples and associated helpers.
+ * The comparators implement a `less-than` operators and large samples are considered first.
+ * Hence if `lhs` has greater priority than `rhs`, the result should be `false`.
+ */
+namespace sample_compare {
+
+	struct level {};
+	struct size {};
+	struct absvalue {};
+	struct numeric {};
+	struct integer {};
+
+	template<typename It>
+	auto get(const It& it, level) {
+		return it.depth();
+	}
+	template<typename It>
+	auto get(const It& it, size) {
+		return it->value().size();
+	}
+	template<typename It>
+	auto get(const It& it, absvalue) {
+		return it->value().abs();
+	}
+	template<typename It>
+	auto get(const It& it, numeric) {
+		return it->value().isNumeric();
+	}
+	template<typename It>
+	auto get(const It& it, integer) {
+		return it->value().isIntegral();
+	}
+
+	/**
+	 * Compares the criterion given by `t` of two samples `lhs` and `rhs` using a comparator `f`.
+	 * Returns `0` if the values are the same, `-1` if `lhs` should be used first and `1` if `rhs` should be used first.
+	 */
+	template<typename It, typename tag, typename F>
+	int compareCriterion(const It& lhs, const It& rhs, tag t, F&& f) {
+		auto l = get(lhs, t);
+		auto r = get(rhs, t);
+		if (l == r) {
+			SMTRAT_LOG_DEBUG("smtrat.cad.samplecompare", "Comparing " << l << " < " << r << " -> 0");
+			return 0;
+		}
+		SMTRAT_LOG_DEBUG("smtrat.cad.samplecompare", "Comparing " << l << " < " << r << " ? " << (f(l, r) ? -1 : 1));
+		return f(l, r) ? -1 : 1;
+	}
+	
+	template<typename It>
+	bool compare(const It& lhs, const It& rhs) {
+		return lhs < rhs;
+	}
+	template<typename It, typename tag, typename F, typename... Tail>
+	bool compare(const It& lhs, const It& rhs) {
+		int res = compareCriterion(lhs, rhs, tag{}, F{});
+		if (res != 0) return res < 0;
+		return compare<It, Tail...>(lhs, rhs);
+	}
+	
+	template<typename It, typename... Args>
+	struct SampleComparator_impl {
+		bool operator()(const It& lhs, const It& rhs) const {
+			auto res = compare<It, Args...>(lhs, rhs);
+			SMTRAT_LOG_DEBUG("smtrat.cad.samplecompare", *lhs << " < " << *rhs << " ? " << res);
+			return res;
+		}
+	};
+	
+	using lt = std::less<>;
+	using gt = std::greater<>;
+	
 	template<typename Iterator, SampleCompareStrategy Strategy>
 	struct SampleComparator {};
 	
 	template<typename Iterator>
-	struct SampleComparator<Iterator, SampleCompareStrategy::Value> {
-		bool operator()(const Iterator& lhs, const Iterator& rhs) const {
-			std::size_t lsize = lhs->value().size();
-			std::size_t rsize = rhs->value().size();
-			if (lsize != rsize) {
-				SMTRAT_LOG_TRACE("smtrat.cad.lifting", lhs->value() << " < " << rhs->value() << ": Size (" << lsize << " / " << rsize << ") " << (lsize > rsize));
-				return lsize > rsize;
-			}
-			SMTRAT_LOG_TRACE("smtrat.cad.lifting", lhs->value() << " < " << rhs->value() << ": Absolute " << (lhs->value().abs() > rhs->value().abs()));
-			return lhs->value().abs() > rhs->value().abs();
-		}
-	};
+	struct SampleComparator<Iterator, SampleCompareStrategy::Value>:
+		SampleComparator_impl<Iterator, size, lt> {};
 	
 	template<typename Iterator>
-	struct SampleComparator<Iterator, SampleCompareStrategy::Numeric> {
-		using Fallback = SampleComparator<Iterator, SampleCompareStrategy::Value>;
-		bool operator()(const Iterator& lhs, const Iterator& rhs) const {
-			bool lint = lhs->value().isNumeric();
-			bool rint = rhs->value().isNumeric();
-			if (lint && rint) return Fallback()(lhs, rhs);
-			if (lint || rint) {
-				SMTRAT_LOG_TRACE("smtrat.cad.lifting", lhs->value() << " < " << rhs->value() << ": Num " << rint);
-				return rint;
-			}
-			return Fallback()(lhs, rhs);
-		}
-	};
+	struct SampleComparator<Iterator, SampleCompareStrategy::Numeric>:
+		SampleComparator_impl<Iterator, numeric, gt, size, lt> {};
 
 	template<typename Iterator>
-	struct SampleComparator<Iterator, SampleCompareStrategy::Integer> {
-		using Fallback = SampleComparator<Iterator, SampleCompareStrategy::Numeric>;
-		bool operator()(const Iterator& lhs, const Iterator& rhs) const {
-			bool lint = lhs->value().isIntegral();
-			bool rint = rhs->value().isIntegral();
-			if (lint && rint) return Fallback()(lhs, rhs);
-			if (lint || rint) {
-				SMTRAT_LOG_TRACE("smtrat.cad.lifting", lhs->value() << " < " << rhs->value() << ": Int " << rint);
-				return rint;
-			}
-			return Fallback()(lhs, rhs);
-		}
-	};
-	
+	struct SampleComparator<Iterator, SampleCompareStrategy::Integer>:
+		SampleComparator_impl<Iterator, integer, gt, numeric, gt, size, lt> {};
+}
+
+	using sample_compare::SampleComparator;
+
 	template<typename Iterator, FullSampleCompareStrategy Strategy>
 	struct FullSampleComparator {};
 	
