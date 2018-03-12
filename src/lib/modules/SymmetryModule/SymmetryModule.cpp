c2cf87fc9efa94ce9104ceb9eeb0eb802f55b5aa
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2018-03-12 11:05:11 +0100

    Added a module that uses the carl symmetry breaking.

---
+++ src/lib/modules/SymmetryModule/SymmetryModule.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file Symmetry.cpp
+ * @author YOUR NAME <YOUR EMAIL ADDRESS>
+ *
+ * @version 2018-03-12
+ * Created on 2018-03-12.
+ */
+
+#include "SymmetryModule.h"
+
+#include <carl/formula/symmetry/symmetry.h>
+
+namespace smtrat
+{
+	template<class Settings>
+	SymmetryModule<Settings>::SymmetryModule(const ModuleInput* _formula, RuntimeSettings*, Conditionals& _conditionals, Manager* _manager):
+		PModule( _formula, _conditionals, _manager )
+#ifdef SMTRAT_DEVOPTION_Statistics
+		, mStatistics(Settings::moduleName)
+#endif
+	{}
+	
+	template<class Settings>
+	SymmetryModule<Settings>::~SymmetryModule()
+	{}
+	
+	template<class Settings>
+	Answer SymmetryModule<Settings>::checkCore()
+	{
+		auto symm = carl::formula::breakSymmetries(FormulaT(rPassedFormula()));
+		addSubformulaToPassedFormula(symm);
+		
+		Answer ans = runBackends();
+		if (ans == UNSAT) {
+			generateTrivialInfeasibleSubset();
+		}
+		return ans;
+	}
+}
+
+#include "Instantiation.h"
