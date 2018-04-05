71ad28b23db09785dc248db23fa8fa610f66ac1c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2018-04-05 16:50:50 +0200

    Disable .travis.yml target if python3 is not available.

--- cmake/prepare-ci.cmake
+++ cmake/prepare-ci.cmake
@@ -1,14 +1,15 @@
 # Misc stuff to setup ci tools
 
-find_package(PythonInterp 3 REQUIRED)
+find_package(PythonInterp 3)
 
-add_custom_target(.travis.yml)
-
-message(STATUS "CarL: ${carl_INCLUDE_DIR}")
-
-add_custom_command(
-	TARGET .travis.yml
-	BYPRODUCTS ${CMAKE_SOURCE_DIR}/.travis.yml
-	COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/.ci/travis_generate.py ${carl_INCLUDE_DIR}/../.ci/
-	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/.ci/
-)
+if(PYTHONINTERP_FOUND)
+	add_custom_target(.travis.yml)
+	add_custom_command(
+		TARGET .travis.yml
+		BYPRODUCTS ${CMAKE_SOURCE_DIR}/.travis.yml
+		COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/.ci/travis_generate.py ${carl_INCLUDE_DIR}/../.ci/
+		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/.ci/
+	)
+else()
+	message(STATUS "Did not find python3, target .travis.yml is not available.")
+endif()
