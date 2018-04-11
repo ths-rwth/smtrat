8067338972682bbbc9c4b79d45cbcb200f20c909
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-11 13:42:36 +0200

    Reduced the external interface of arithmetic and core theory.

--- src/cli/parser/theories/Core.h
+++ src/cli/parser/theories/Core.h
@@ -18,10 +18,6 @@ struct CoreTheory: public AbstractTheory {
 	static void addSimpleSorts(qi::symbols<char, carl::Sort>& sorts);
 	static void addConstants(qi::symbols<char, types::ConstType>& constants);
 	
-	static bool convertTerm(const types::TermType& term, FormulaT& result);
-	
-	static bool convertArguments(const std::vector<types::TermType>& arguments, std::vector<FormulaT>& result, TheoryError& errors);
-	
 	CoreTheory(ParserState* state);
 	
 	bool declareVariable(const std::string& name, const carl::Sort& sort, types::VariableType& result, TheoryError& errors);
