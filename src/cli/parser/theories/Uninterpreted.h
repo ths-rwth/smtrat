410505bb1abe09873eb53ed899cb459b73ce1623
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-11 13:55:08 +0200

    Reduced public interface of uninterpreted theory.

--- src/cli/parser/theories/Uninterpreted.h
+++ src/cli/parser/theories/Uninterpreted.h
@@ -16,30 +16,11 @@ struct UninterpretedTheory: public AbstractTheory {
 	carl::UVariable mTrue;
 	carl::UVariable mFalse;
 	
-	static bool convertTerm(const types::TermType& term, types::UninterpretedTheory::TermType& result);
-	static bool convertArguments(const std::vector<types::TermType>& arguments, std::vector<types::UninterpretedTheory::TermType>& result, TheoryError& errors);
-	
 	UninterpretedTheory(ParserState* state);
 	
 	bool declareVariable(const std::string& name, const carl::Sort& sort, types::VariableType& result, TheoryError& errors);
 	
 	bool handleITE(const FormulaT& ifterm, const types::TermType& thenterm, const types::TermType& elseterm, types::TermType& result, TheoryError& errors);
-	
-	template<bool negated>
-	struct EqualityGenerator: public boost::static_visitor<FormulaT> {
-		FormulaT operator()(const carl::UVariable& lhs, const carl::UVariable& rhs) {
-			return FormulaT(lhs, rhs, negated);
-		}
-		FormulaT operator()(const carl::UVariable& lhs, const carl::UFInstance& rhs) {
-			return FormulaT(lhs, rhs, negated);
-		}
-		FormulaT operator()(const carl::UFInstance& lhs, const carl::UVariable& rhs) {
-			return FormulaT(lhs, rhs, negated);
-		}
-		FormulaT operator()(const carl::UFInstance& lhs, const carl::UFInstance& rhs) {
-			return FormulaT(lhs, rhs, negated);
-		}
-	};
 	bool handleFunctionInstantiation(const carl::UninterpretedFunction& f, const std::vector<types::TermType>& arguments, types::TermType& result, TheoryError& errors);
 	
 	bool handleDistinct(const std::vector<types::TermType>&, types::TermType&, TheoryError& errors);
