b4a10c06b7f3a2ffa02a3b35022b10ddecd63568
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-13 13:23:05 +0200

    Remove carl from travis setup, use external project from now on.

--- .ci/setup_travis.sh
+++ .ci/setup_travis.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/env bash
 
-function keep_waiting() {
-  while true; do
-    echo -e "."
-    sleep 60
-  done
-}
-
 git fetch --unshallow
-
-pushd `pwd`
-cd ~/
-git clone https://github.com/smtrat/carl.git
-
-cd carl/
-echo "Checked out CArL version $(git describe --always)"
-mkdir -p build || return 1
-cd build/ || return 1
-cmake -D DEVELOPER=ON ../ || return 1
-
-keep_waiting &
-make resources -j1 || return 1
-make -j1 lib_carl || return 1
-kill $!
-
-popd
 
 export CC=$CC
 export CXX=$CXX
