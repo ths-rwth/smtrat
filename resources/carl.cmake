43726211398f73dc7e155d8016689ea03ae861f2
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-13 14:21:12 +0200

    Add boost as external project, forward it to carl EP.

--- resources/carl.cmake
+++ resources/carl.cmake
@@ -1,8 +1,10 @@
+get_include_dir(BOOST_INCLUDEDIR Boost_SHARED)
+
 ExternalProject_Add(
 	CArL-EP
 	GIT_REPOSITORY "https://github.com/smtrat/carl.git"
-		CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DBUILD_STATIC=ON
-		BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} lib_carl lib_carl_static
+	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DBUILD_STATIC=ON -DBOOST_INCLUDEDIR=${BOOST_INCLUDEDIR}
+	BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} lib_carl lib_carl_static
 )
 
 ExternalProject_Get_Property(CArL-EP INSTALL_DIR)
@@ -12,8 +14,10 @@ add_imported_library(CArL STATIC "${INSTALL_DIR}/lib/libcarl${STATIC_EXT}" "${IN
 
 add_library(lib_carl INTERFACE IMPORTED GLOBAL)
 add_dependencies(lib_carl CArL_SHARED)
+set_target_properties(lib_carl PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}/include")
 add_library(lib_carl_static INTERFACE IMPORTED GLOBAL)
 add_dependencies(lib_carl CArL_STATIC)
+set_target_properties(lib_carl_static PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}/include")
 
 add_dependencies(CArL_SHARED CArL-EP)
 add_dependencies(CArL_STATIC CArL-EP)
--- resources/resources.cmake
+++ resources/resources.cmake
@@ -14,8 +14,27 @@ set(CMAKE_FIND_FRAMEWORK "LAST")
 ##### Load resources
 ###############
 
+
+##### Boost
+set(BOOST_COMPONENTS "filesystem;system;program_options;regex")
+set(Boost_USE_DEBUG_RUNTIME "OFF")
+if(NOT FORCE_SHIPPED_RESOURCES)
+	load_library(smtrat Boost 1.55 COMPONENTS ${BOOST_COMPONENTS})
+endif()
+if(Boost_FOUND)
+	message(STATUS "Use system version of Boost ${Boost_VERSION}")
+else()
+	set(BOOST_VERSION "1.64.0")
+	set(BOOST_ZIPHASH "36093e4018aecd5b0e31e80457ac5fc1")
+	include(resources/boost.cmake)
+	message(STATUS "Use shipped version of Boost ${BOOST_VERSION}")
+endif()
+
 ##### CArL
-find_package(carl)
-if(NOT carl_FOUND)
+#find_package(carl)
+if(carl_FOUND)
+	message(STATUS "Use system version of carl ${carl_VERSION}")
+else()
 	include(resources/carl.cmake)
+	message(STATUS "Use shipped version of carl ${carl_VERSION}")
 endif()
