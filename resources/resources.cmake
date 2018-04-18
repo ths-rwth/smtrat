0e6501193a77768c98aeca9174566e65ffa983e2
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-18 14:41:34 +0200

    Take all resources from carl.

--- resources/resources.cmake
+++ resources/resources.cmake
@@ -14,53 +14,6 @@ set(CMAKE_FIND_FRAMEWORK "LAST")
 ##### Load resources
 ###############
 
-##### GMP / GMPXX
-if(NOT FORCE_SHIPPED_RESOURCES)
-	load_library(smtrat GMP 5.1)
-	load_library(smtrat GMPXX 5.1)
-endif()
-if(GMP_FOUND)
-	message(STATUS "Use system version of GMP/GMPXX ${GMP_VERSION}")
-else()
-	set(GMP_VERSION "6.1.0")
-	include(resources/gmp.cmake)
-	message(STATUS "Use shipped version of GMP/GMPXX ${GMP_VERSION}")
-endif()
-
-##### Boost
-set(BOOST_COMPONENTS "filesystem;system;program_options;regex")
-set(Boost_USE_DEBUG_RUNTIME "OFF")
-if(NOT FORCE_SHIPPED_RESOURCES)
-	load_library(smtrat Boost 1.55 COMPONENTS ${BOOST_COMPONENTS})
-endif()
-if(Boost_FOUND)
-	message(STATUS "Use system version of Boost ${Boost_VERSION}")
-else()
-	set(BOOST_VERSION "1.64.0")
-	set(BOOST_ZIPHASH "36093e4018aecd5b0e31e80457ac5fc1")
-	include(resources/boost.cmake)
-	message(STATUS "Use shipped version of Boost ${BOOST_VERSION}")
-endif()
-
-##### CoCoALib
-if(USE_COCOA)
-	if(NOT FORCE_SHIPPED_RESOURCES)
-		load_library(smtrat CoCoA 0.99542)
-	endif()
-	if(COCOA_FOUND)
-		message(STATUS "Use system version of CoCoA ${COCOA_VERSION}")
-	else()
-		# Depends on GMP
-		set(COCOA_VERSION "0.99551")
-		set(COCOA_TGZHASH "0e75ba96e627f955adbb17c037d5bcdf")
-		include(resources/cocoa.cmake)
-		unset(COCOA_TGZHASH)
-		message(STATUS "Use shipped version of CoCoA ${COCOA_VERSION}")
-	endif()
-else()
-	message(STATUS "Usage of CoCoA is disabled")
-endif()
-
 ##### CArL
 find_package(carl)
 if(carl_FOUND)
