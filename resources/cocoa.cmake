a63b95b475c68e29de819d0103b30b1e57fa1199
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-18 09:40:09 +0200

    Updated CoCoALib-EP to a allow for a proper make install.

--- resources/cocoa.cmake
+++ resources/cocoa.cmake
@@ -1,4 +1,4 @@
-if (TARGET GMP_EP)
+if (TARGET GMP-EP)
 	get_target_property(GMP_LIB GMP_STATIC IMPORTED_LOCATION)
 	set(GMP_LIB_ARG "--with-libgmp=${GMP_LIB}")
 endif()
@@ -9,16 +9,19 @@ ExternalProject_Add(
 	URL_MD5 ${COCOA_TGZHASH}
 	DOWNLOAD_NO_PROGRESS 1
 	BUILD_IN_SOURCE YES
-	CONFIGURE_COMMAND ./configure --threadsafe-hack ${GMP_LIB_ARG} --with-cxxflags=-Wno-deprecated-declarations\ -fPIC
+	CONFIGURE_COMMAND ./configure --prefix=<INSTALL_DIR> --threadsafe-hack ${GMP_LIB_ARG} --with-cxxflags=-Wno-deprecated-declarations\ -fPIC
 	BUILD_COMMAND make lib
-	INSTALL_COMMAND ""
+	INSTALL_COMMAND ${CMAKE_COMMAND} -E touch <SOURCE_DIR>/examples/index.html
+	COMMAND ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/include
+	COMMAND ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/lib
+	COMMAND ${CMAKE_MAKE_PROGRAM} install
 	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/resources/src/CoCoALib-EP/lib/libcocoa${STATIC_EXT}
 )
 
-ExternalProject_Get_Property(CoCoALib-EP SOURCE_DIR)
+ExternalProject_Get_Property(CoCoALib-EP INSTALL_DIR)
 
-add_imported_library(COCOA SHARED "${SOURCE_DIR}/lib/libcocoa${STATIC_EXT}" "${SOURCE_DIR}/include")
-add_imported_library(COCOA STATIC "${SOURCE_DIR}/lib/libcocoa${STATIC_EXT}" "${SOURCE_DIR}/include")
+add_imported_library(COCOA SHARED "${INSTALL_DIR}/lib/libcocoa${STATIC_EXT}" "${INSTALL_DIR}/include")
+add_imported_library(COCOA STATIC "${INSTALL_DIR}/lib/libcocoa${STATIC_EXT}" "${INSTALL_DIR}/include")
 
 add_dependencies(CoCoALib-EP GMP_STATIC)
 add_dependencies(COCOA_SHARED CoCoALib-EP GMPXX_SHARED)
