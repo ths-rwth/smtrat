edbd4a8fa1f5789782133048521a1ad07a151009
Jasper Nalbach <derjasper@posteo.de>
2018-04-24 17:46:31 +0200

    use VS for mcsat (to test this branch..)

--- src/lib/modules/SATModule/mcsat/BaseBackend.h
+++ src/lib/modules/SATModule/mcsat/BaseBackend.h
@@ -3,6 +3,7 @@
 #include "../../../datastructures/mcsat/Bookkeeping.h"
 #include "../../../datastructures/mcsat/arithmetic/AssignmentFinder_arithmetic.h"
 #include "../../../datastructures/mcsat/nlsat/Explanation.h"
+#include "../../../datastructures/mcsat/vs/Explanation.h"
 #include "../../../datastructures/mcsat/variableordering/VariableOrdering.h"
 
 #include "../../../datastructures/mcsat/explanations/ParallelExplanation.h"
@@ -89,7 +90,7 @@ public:
 struct BackendSettings1 {
 	static constexpr VariableOrdering variable_ordering = VariableOrdering::FeatureBased;
 	using AssignmentFinderBackend = arithmetic::AssignmentFinder;
-	using ExplanationBackend = nlsat::Explanation;
+	using ExplanationBackend = vs::Explanation;
 };
 
 } // namespace mcsat
