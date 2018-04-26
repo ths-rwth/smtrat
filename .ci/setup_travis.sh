b3be35fe60026481ad07686c9cb4d7e93ebd6912
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-26 12:51:34 +0200

    Using clang-6.0 for doxygen.

--- .ci/setup_travis.sh
+++ .ci/setup_travis.sh
@@ -7,10 +7,12 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
 	mkdir -p ~/usr/bin/
 	PREFIX=`cd ~/usr; pwd`
 	
-	if [ ! -f $PREFIX/bin/doxygen ]; then
-		wget -nv http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
-		tar -xzf doxygen-1.8.14.linux.bin.tar.gz -C $PREFIX/bin/
-		cp $PREFIX/bin/doxygen-*/bin/* $PREFIX/bin/
+	if [[ ${TASK} == "doxygen" ]]; then
+		if [ ! -f $PREFIX/bin/doxygen ]; then
+			wget -nv http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
+			tar -xzf doxygen-1.8.14.linux.bin.tar.gz -C $PREFIX/bin/
+			cp $PREFIX/bin/doxygen-*/bin/* $PREFIX/bin/
+		fi
 	fi
 
 	export PATH="$PREFIX/bin:$PATH"
