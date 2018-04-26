7b40ec1ed6a4534387693fe34b7fb22efcfe9372
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-26 15:27:21 +0200

    Removed download of doxygen in travis setup. Use doxygen from carl.

--- .ci/setup_travis.sh
+++ .ci/setup_travis.sh
@@ -2,22 +2,5 @@
 
 git fetch --unshallow
 
-if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-
-	mkdir -p ~/usr/bin/
-	PREFIX=`cd ~/usr; pwd`
-	
-	if [[ ${TASK} == "doxygen" ]]; then
-		if [ ! -f $PREFIX/bin/doxygen ]; then
-			wget -nv http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
-			tar -xzf doxygen-1.8.14.linux.bin.tar.gz -C $PREFIX/bin/
-			cp $PREFIX/bin/doxygen-*/bin/* $PREFIX/bin/
-		fi
-	fi
-
-	export PATH="$PREFIX/bin:$PATH"
-
-fi
-
 export CC=$CC
 export CXX=$CXX
