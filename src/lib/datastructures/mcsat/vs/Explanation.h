d00c8e19411f90468815b9ee246242058c723d38
Jasper Nalbach <derjasper@posteo.de>
2018-05-15 12:31:21 +0200

    remove superfluous debug messages

--- src/lib/datastructures/mcsat/vs/Explanation.h
+++ src/lib/datastructures/mcsat/vs/Explanation.h
@@ -10,8 +10,6 @@ namespace vs {
 
 struct Explanation {
 	boost::optional<FormulaT> operator()(const mcsat::Bookkeeping& data, const std::vector<carl::Variable>& variableOrdering, carl::Variable var, const FormulasT& reason, const FormulaT& implication) const {
-		std::cout << "**EXPLAIN CALL**" << std::endl; // TODO remove, for debugging
-		
 		SMTRAT_LOG_DEBUG("smtrat.mcsat.vs", "With " << reason << " explain " << implication);
 		// 'variableOrder' is ordered 'x0,.., xk, .., xn', the relevant variables that appear in the
 		// reason and implication end at 'var'=xk, so the relevant prefix is 'x0 ... xk'
@@ -20,15 +18,9 @@ struct Explanation {
 		SMTRAT_LOG_DEBUG("smtrat.mcsat.vs", "Bookkeep: " << data);
 		ExplanationGenerator eg(reason, variableOrdering, var, data.model());
 		auto ret = eg.getExplanation(implication);
-
-		// TODO remove, for debugging:
-		try {
-			std::cout << "**VS SUCCESS " << ret.value() << "**" << std::endl;
+		if (ret == boost::none) {
+			SMTRAT_LOG_DEBUG("smtrat.mcsat.vs", "Could not generate explanation");
 		}
-		catch (const boost::bad_optional_access&) {
-			std::cout << "**INCOMPLETE**" << std::endl;
-		}
-		
 		return ret;
 	}
 };
