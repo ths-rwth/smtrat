a30d32b8613209a9a775482a580f27b893ed520a
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-05-25 09:06:07 +0200

    Refactored fm explanation to properly work on nonlinear inputs.

--- src/lib/datastructures/mcsat/fm/Explanation.h
+++ src/lib/datastructures/mcsat/fm/Explanation.h
@@ -8,7 +8,18 @@ namespace mcsat {
 namespace fm {
 
 struct ConflictGenerator {
-	using Bound = std::tuple<ConstraintT,Poly,Rational>;
+	/**
+	 * The input is a constraint c: p*x+q~0 which can be used as a bound on x with p,q multivariate polynomials.
+	 * If x only occurs linearly in c, this decomposition is possible.
+	 * We evaluate c over the partial model and obtain x~r, where r is a rational.
+	 *
+	 * We store (c,p,q,r) for each bound.
+	 *
+	 * Given a lower bound l and an upper bound u, the elimination is as follows:
+	 *   Conflict if l.r >= u.r (or strict, if both relations from c are weak)
+	 *   l.q * u.p < u.q * l.p
+	 */
+	using Bound = std::tuple<ConstraintT,Poly,Poly,Rational>;
 private:
 	std::vector<Bound> mLower;
 	std::vector<Bound> mUpper;
@@ -22,39 +33,44 @@ public:
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Discarding bound " << b << " because " << v << " occurs nonlinearly");
 				continue;
 			}
-			auto coeff = b.coefficient(v, 1).constantPart();
-			if (carl::isZero(coeff)) {
+			auto p = b.coefficient(v, 1);
+			if (p.isZero()) {
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Discarding bound " << b << " because it does not contain " << v);
 				continue;
 			}
-			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Considering bound " << b << " for " << v);
-			auto poly = (v*coeff - b.lhs()) / coeff;
+			auto q = b.lhs() - p * v;
 			
-			auto res = carl::model::evaluate(poly, m);
-			assert(res.isRational());
+			auto evalp = carl::model::evaluate(p, m);
+			auto evalq = carl::model::evaluate(q, m);
+			assert(evalp.isRational() && evalq.isRational());
+			Rational r = - evalq.asRational() / evalp.asRational();
+			
+			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Considering bound " << b << " for " << v);
+			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Decomposed into " << p << " * " << v << " + " << q << ", " << v << " ~ " << r);
+			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Coefficient is " << evalp.asRational());
 			
 			switch (b.relation()) {
 				case carl::Relation::LESS:
 				case carl::Relation::LEQ:
-					if (coeff > 0) {
-						mUpper.emplace_back(b, poly, res.asRational());
+					if (evalp.asRational() > 0) {
+						mUpper.emplace_back(b, p, q, r);
 					} else {
-						mLower.emplace_back(b, poly, res.asRational());
+						mLower.emplace_back(b, p, q, r);
 					}
 					break;
 				case carl::Relation::EQ:
-					mLower.emplace_back(b, poly, res.asRational());
-					mUpper.emplace_back(b, poly, res.asRational());
+					mLower.emplace_back(b, p, q, r);
+					mUpper.emplace_back(b, p, q, r);
 					break;
 				case carl::Relation::NEQ:
-					mInequalities.emplace(res.asRational(), b);
+					mInequalities.emplace(r, b);
 					break;
 				case carl::Relation::GEQ:
 				case carl::Relation::GREATER:
-					if (coeff > 0) {
-						mLower.emplace_back(b, poly, res.asRational());
+					if (evalp.asRational() > 0) {
+						mLower.emplace_back(b, p, q, r);
 					} else {
-						mUpper.emplace_back(b, poly, res.asRational());
+						mUpper.emplace_back(b, p, q, r);
 					}
 					break;
 			}
@@ -81,13 +97,17 @@ public:
 		
 		const Bound& lower = mLower[mNext.first];
 		const Bound& upper = mUpper[mNext.second];
+		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Combining " << lower << " and " << upper);
 		bool strict = carl::isStrict(std::get<0>(lower).relation()) || carl::isStrict(std::get<0>(upper).relation());
 		const auto& lp = std::get<1>(lower);
 		const auto& up = std::get<1>(upper);
-		const auto& lr = std::get<2>(lower);
-		const auto& ur = std::get<2>(upper);
+		const auto& lq = std::get<2>(lower);
+		const auto& uq = std::get<2>(upper);
+		const auto& lr = std::get<3>(lower);
+		const auto& ur = std::get<3>(upper);
 		
 		if (lr < ur) {
+			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Not in conflict");
 			return boost::none;
 		}
 		FormulasT res;
@@ -97,13 +117,16 @@ public:
 				// Only allows for a point solution which is excluded by an inequality.
 				strict = true;
 				res.emplace_back(it->second);
+				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "In conflict due to inequality " << it->second);
 			} else {
+				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Not in conflict");
 				return boost::none;
 			}
 		}
 		
-		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "-> " << std::get<1>(lower) << " vs. " << std::get<1>(upper));
-		res.emplace_back(ConstraintT(lp - up, strict ? carl::Relation::LESS : carl::Relation::LEQ));
+		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "-> " << lp << " * " << uq << " " << (strict ? carl::Relation::LESS : carl::Relation::LEQ) << " " << up << " * " << lq);
+		
+		res.emplace_back(ConstraintT(lp*uq - up*lq, strict ? carl::Relation::LESS : carl::Relation::LEQ));
 		return res;
 	}
 };
