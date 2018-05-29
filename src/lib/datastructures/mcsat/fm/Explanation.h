5ec428d4a9f5cc14003c9cea0b96cf016686dd68
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-05-29 09:34:51 +0200

    Reworked FM explanations.

--- src/lib/datastructures/mcsat/fm/Explanation.h
+++ src/lib/datastructures/mcsat/fm/Explanation.h
@@ -9,18 +9,21 @@ namespace fm {
 
 struct ConflictGenerator {
 	/**
-	 * The input is a constraint c: p*x+q~0 which can be used as a bound on x with p,q multivariate polynomials.
+	 * The input is a constraint c: p*x~q which can be used as a bound on x with p,q multivariate polynomials.
 	 * If x only occurs linearly in c, this decomposition is possible.
 	 * We evaluate c over the partial model and obtain x~r, where r is a rational.
-	 * To properly perform the elimination step detailed below, we additionally store the sign condition (as relation) of p over the current assignment.
+	 * To properly perform the elimination step detailed below, we additionally store whether p is negative over the current assignment as a Boolean.
 	 *
-	 * We store (c,p,q,r,rel) for each bound.
+	 * We store (c,p,q,r,n) for each bound.
 	 *
 	 * Given a lower bound l and an upper bound u, the elimination is as follows:
 	 *   Conflict if l.r >= u.r (or strict, if both relations from c are weak)
 	 *   l.q * u.p < u.q * l.p
+	 *
+	 * If exactly one of u.p and l.p was found to be negative, the relation has to be inverted.
+	 * If u.p or l.p are not constants, we additionally have to add a literal stating that their sign does not change.
 	 */
-	using Bound = std::tuple<ConstraintT,Poly,Poly,Rational,carl::Relation>;
+	using Bound = std::tuple<ConstraintT,Poly,Poly,Rational,bool>;
 private:
 	std::vector<Bound> mLower;
 	std::vector<Bound> mUpper;
@@ -40,7 +43,6 @@ public:
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Discarding bound " << b << " because it does not contain " << v);
 				continue;
 			}
-			auto q = b.lhs() - p * v;
 			
 			auto evalp = carl::model::evaluate(p, m);
 			if (!evalp.isRational()) {
@@ -52,15 +54,16 @@ public:
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Discarding bound " << b << " because it does not contain " << v << " after we evaluate it");
 				continue;
 			}
+			bool negated = evalp.asRational() < 0;
 			
+			auto q = p * v - b.lhs();
 			auto evalq = carl::model::evaluate(q, m);
 			if (!evalq.isRational()) {
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Discarding bound " << b << " because " << q << " did not evaluate to a rational");
 				continue;
 			}
 			assert(evalq.isRational());
-			Rational r = - evalq.asRational() / evalp.asRational();
-			carl::Relation rel = (evalp.asRational() >= 0 ? carl::Relation::GREATER : carl::Relation::LESS);
+			Rational r = evalq.asRational() / evalp.asRational();
 			
 			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Considering bound " << b << " for " << v);
 			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Decomposed into " << p << " * " << v << " + " << q << ", " << v << " ~ " << r);
@@ -69,26 +72,25 @@ public:
 			switch (b.relation()) {
 				case carl::Relation::LESS:
 				case carl::Relation::LEQ:
-					if (rel == carl::Relation::GREATER) {
-						mUpper.emplace_back(b, p, q, r, rel);
+					if (negated) {
+						mLower.emplace_back(b, p, q, r, negated);
 					} else {
-						mLower.emplace_back(b, p, q, r, rel);
+						mUpper.emplace_back(b, p, q, r, negated);
 					}
 					break;
-				case carl::Relation::EQ: {
-					mLower.emplace_back(b, -p, -q, -r, carl::Relation::LESS);
-					mUpper.emplace_back(b, p, q, -r, carl::Relation::GREATER);
+				case carl::Relation::EQ:
+					mLower.emplace_back(b, p, q, r, negated);
+					mUpper.emplace_back(b, p, q, r, negated);
 					break;
-				}
 				case carl::Relation::NEQ:
 					mInequalities.emplace(r, b);
 					break;
 				case carl::Relation::GEQ:
 				case carl::Relation::GREATER:
-					if (rel == carl::Relation::GREATER) {
-						mLower.emplace_back(b, p, q, r, rel);
+					if (negated) {
+						mUpper.emplace_back(b, p, q, r, negated);
 					} else {
-						mUpper.emplace_back(b, p, q, r, rel);
+						mLower.emplace_back(b, p, q, r, negated);
 					}
 					break;
 			}
@@ -123,20 +125,22 @@ public:
 		const auto& uq = std::get<2>(upper);
 		const auto& lr = std::get<3>(lower);
 		const auto& ur = std::get<3>(upper);
-		const auto& lrel = std::get<4>(lower);
-		const auto& urel = std::get<4>(upper);
+		const auto& lneg = std::get<4>(lower);
+		const auto& uneg = std::get<4>(upper);
 		
 		if (lr < ur) {
 			SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Not in conflict");
 			return boost::none;
 		}
 		FormulasT res;
+		res.emplace_back(std::get<0>(lower).negation());
+		res.emplace_back(std::get<0>(upper).negation());
 		if (lr == ur && !strict) {
 			auto it = mInequalities.find(lr);
 			if (it != mInequalities.end()) {
 				// Only allows for a point solution which is excluded by an inequality.
 				strict = true;
-				res.emplace_back(it->second);
+				res.emplace_back(it->second.negation());
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "In conflict due to inequality " << it->second);
 			} else {
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Not in conflict");
@@ -144,15 +148,16 @@ public:
 			}
 		}
 		
-		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "-> " << lq << " * " << up << " " << (strict ? carl::Relation::LESS : carl::Relation::LEQ) << " " << uq << " * " << lp);
+		carl::Relation rel = (lneg xor uneg) ? (strict ? carl::Relation::GREATER : carl::Relation::GEQ) : (strict ? carl::Relation::LESS : carl::Relation::LEQ);
 		
-		res.emplace_back(ConstraintT(lq*up - uq*lp, strict ? carl::Relation::LESS : carl::Relation::LEQ));
-		ConstraintT sidecond_l(lp, lrel);
-		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "-> " << lp << " " << lrel << " 0");
-		res.emplace_back(sidecond_l.negation());
-		ConstraintT sidecond_u(up, urel);
-		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "-> " << up << " " << urel << " 0");
-		res.emplace_back(sidecond_u.negation());
+		res.emplace_back(ConstraintT(lq*up - uq*lp, rel));
+		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Conflict: " << lq << " * " << up << " " << rel << " " << uq << " * " << lp);
+		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "-> " << res.back());
+		
+		res.emplace_back(ConstraintT(lp, lneg ? carl::Relation::LESS : carl::Relation::GREATER).negation());
+		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Side condition on " << lp << ": " << res.back().negated());
+		res.emplace_back(ConstraintT(up, uneg ? carl::Relation::LESS : carl::Relation::GREATER).negation());
+		SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Side condition on " << up << ": " << res.back().negated());
 		return res;
 	}
 };
@@ -176,7 +181,6 @@ struct Explanation {
 			auto res = cg();
 			if (res) {
 				SMTRAT_LOG_DEBUG("smtrat.mcsat.fm", "Found conflict " << *res);
-				for (const auto& r: reason) res->emplace_back(r.negated());
 				return FormulaT(carl::FormulaType::OR, std::move(*res));
 			}
 			cg.next();
