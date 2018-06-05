3b6b9a1f892d89364f5f3cc17ccecbecdccedd51
Johannes Neuhaus <johannes.neuhaus@entenhausen.io>
2018-06-05 16:21:27 +0200

    Add Groebner Preprocessing Strategy.

---
+++ src/lib/strategies/PBPPStrategyGroebner.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../solver/Manager.h"
+
+#include "../modules/LRAModule/LRAModule.h"
+#include "../modules/FPPModule/FPPModule.h"
+#include "../modules/PBPPModule/PBPPModule.h"
+#include "../modules/SATModule/SATModule.h"
+
+
+namespace smtrat
+{
+    class PBPPStrategyGroebner:
+        public Manager
+    {
+        public:
+            PBPPStrategyGroebner(): Manager() {
+				setStrategy({
+					addBackend<FPPModule<FPPSettings1>>(
+						addBackend<PBPPModule<PBPPSettings1>>(
+							addBackend<SATModule<SATSettings1>>(
+								addBackend<LRAModule<LRASettings1>>()
+							)
+						)
+					),
+				});
+			}
+    };
+}    // namespace smtrat
