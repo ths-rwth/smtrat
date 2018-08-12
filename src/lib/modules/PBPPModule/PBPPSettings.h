37ff2542821eb3a3821b09dada5e2e8e3b801858
Johannes Neuhaus <johannes.neuhaus@entenhausen.io>
2018-08-12 22:30:04 +0200

    Disable forcing encoding of constraints.

--- src/lib/modules/PBPPModule/PBPPSettings.h
+++ src/lib/modules/PBPPModule/PBPPSettings.h
@@ -48,7 +48,7 @@ namespace smtrat
 		static constexpr double MAX_NEW_RELATIVE_FORMULA_SIZE = 0.5;
 		static constexpr bool USE_LIA_MIXED = true;
 		static constexpr bool USE_LIA_ONLY = false;
-		static constexpr bool ENCODE_IF_POSSIBLE = true;
+		static constexpr bool ENCODE_IF_POSSIBLE = false;
 
 
 	};
