d8b8f4c2e24960c4c98f342bb7fb1ac29eb8d559
Johannes Neuhaus <johannes.neuhaus@entenhausen.io>
2018-08-19 19:05:01 +0200

    Implement different mixed sign approach[...]
    Divide into sets of variables which occur positively and
    negatively. Then encode an implication depending on how
    the left (and right) hand-side may evaluate.

--- src/lib/modules/PBPPModule/MixedSignEncoder.h
+++ src/lib/modules/PBPPModule/MixedSignEncoder.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "PseudoBoolEncoder.h"
+#include "PseudoBoolNormalizer.h"
 #include "LongFormulaEncoder.h"
+#include "ShortFormulaEncoder.h"
+#include "CardinalityEncoder.h"
 
 namespace smtrat {
 	class MixedSignEncoder : public PseudoBoolEncoder {
@@ -9,12 +12,23 @@ namespace smtrat {
 			MixedSignEncoder() : PseudoBoolEncoder () {}
 
 			bool canEncode(const ConstraintT& constraint);
+			Rational encodingSize(const ConstraintT& constraint);
+
 
 		protected:
 			boost::optional<FormulaT> doEncode(const ConstraintT& constraint);
 
 		private:
 			LongFormulaEncoder mLongFormulaEncoder;
+			ShortFormulaEncoder mShortFormulaEncoder;
+			CardinalityEncoder mCardinalityEncoder;
+			PseudoBoolNormalizer mNormalizer;
+
+
+			FormulaT findSubEncoding(const ConstraintT& constraint);
+			std::vector<Rational> calculateSubsetsums(const std::vector<TermT>& terms);
+			void calculateSubsetsums(const std::vector<TermT>& terms, size_t leftIndex, std::set<Rational>& result, Rational sum = 0);
+			
 
 	};
 }
