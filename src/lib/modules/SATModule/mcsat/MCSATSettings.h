8d0a5c08c103fa784161d2cec5a3339ca3bb9c6a
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-09-14 13:04:23 +0200

    Adding settings to FM explanations.

--- src/lib/modules/SATModule/mcsat/MCSATSettings.h
+++ src/lib/modules/SATModule/mcsat/MCSATSettings.h
@@ -24,7 +24,7 @@ struct MCSATSettingsFMNL {
 	static constexpr VariableOrdering variable_ordering = VariableOrdering::FeatureBased;
 	// using AssignmentFinderBackend = arithmetic::AssignmentFinder;
 	using AssignmentFinderBackend = SequentialAssignment<smtaf::AssignmentFinder<smtaf::DefaultSettings>,arithmetic::AssignmentFinder>;
-	using ExplanationBackend = SequentialExplanation<fm::Explanation,nlsat::Explanation>;
+	using ExplanationBackend = SequentialExplanation<fm::Explanation<fm::DefaultSettings>,nlsat::Explanation>;
 };
 
 struct MCSATSettingsVSNL {
