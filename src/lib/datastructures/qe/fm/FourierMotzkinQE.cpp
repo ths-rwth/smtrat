79eb8689a60eb1169f0d4a38480009991ce0702f
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-11-16 16:00:47 +0100

    added assembly of new formula to Fourier-Motzkin QE procedure.

--- src/lib/datastructures/qe/fm/FourierMotzkinQE.cpp
+++ src/lib/datastructures/qe/fm/FourierMotzkinQE.cpp
@@ -3,7 +3,6 @@
 namespace smtrat::qe::fm {
 
     FormulaT FourierMotzkinQE::eliminateQuantifiers() {
-
         // iterate over query
         for(const auto& QuantifierVariablesPair : mQuery) {
             // we are ignoring the quantifier type
@@ -12,66 +11,74 @@ namespace smtrat::qe::fm {
             // eliminate one variable after the other
             for(const auto& var : QuantifierVariablesPair.second) {
                 auto bounds = findBounds(var);
-
+                // combine all lower-upper bound pairs.
                 auto newConstraints = createNewConstraints(bounds, var);
 
-                removeOldConstraints(bounds);
-            }
+                // add all constraints which are not containing var to newConstraints
+                for(const auto formulaIt : bounds[2]) {
+                    assert((*formulaIt).getType() == carl::FormulaType::CONSTRAINT);
+                    newConstraints.emplace_back(FormulaT(*formulaIt));
+                }
 
+                // assemble new formula
+                mFormula = FormulaT(carl::FormulaType::AND, newConstraints);
+            }
         }
 
         return mFormula;
     }
 
-    typename FourierMotzkinQE::boundIteratorPair FourierMotzkinQE::findBounds(const carl::Variable& variable) {
-        // result vector
-        typename FourierMotzkinQE::boundIteratorPair res;
+    typename FourierMotzkinQE::FormulaPartition FourierMotzkinQE::findBounds(const carl::Variable& variable) {
+        // result vector initialized with three subsets
+        typename FourierMotzkinQE::FormulaPartition res{3,std::vector<FormulaT::const_iterator>()};
 
         // if the formula only contains one constraint, check for occurence of the variable.
-        if(mFormula.getType() == carl::FormulaType::CONSTRAINT && mFormula.constraint().hasVariable(variable)) {
-            if(isLinearLowerBound(mFormula.constraint(), variable)){
-                res.first.emplace_back(mFormula.begin());
+        if(mFormula.getType() == carl::FormulaType::CONSTRAINT) {
+            if(!mFormula.constraint().hasVariable(variable)) {
+                res[2].emplace_back(mFormula.begin());
             } else {
-                res.second.emplace_back(mFormula.begin());
+                if(isLinearLowerBound(mFormula.constraint(), variable)){
+                    res[0].emplace_back(mFormula.begin());
+                } else {
+                    res[1].emplace_back(mFormula.begin());
+                }
             }
             return res;
         }
 
         // More than one constaint: search formula to find bounds
         for(auto formulaIt = mFormula.begin(); formulaIt != mFormula.end(); ++formulaIt) {
-            assert((*formulaIt)->type() == FormulaType::CONSTRAINT);
+            assert((*formulaIt).getType() == carl::FormulaType::CONSTRAINT);
             if((*formulaIt).constraint().hasVariable(variable)) {
                 if(isLinearLowerBound((*formulaIt).constraint(), variable)) {
-                    res.first.push_back(formulaIt);
+                    res[0].push_back(formulaIt);
                 } else {
-                    res.second.push_back(formulaIt);
+                    res[1].push_back(formulaIt);
                 }
+            } else {
+                res[2].push_back(formulaIt);
             }
         }
 
         return res;
     }
 
-    std::vector<ConstraintT> FourierMotzkinQE::createNewConstraints(const typename FourierMotzkinQE::boundIteratorPair& bounds, carl::Variable v) {
+    carl::Formulas<Poly> FourierMotzkinQE::createNewConstraints(const typename FourierMotzkinQE::FormulaPartition& bounds, carl::Variable v) {
 
-        std::vector<ConstraintT> constraints;
+        carl::Formulas<Poly> constraints;
 
         // combine all pairs of lower and upper bounds.
-        for(const auto lowerBndIt : bounds.first) {
-            for(const auto upperBndIt : bounds.second) {
+        for(const auto lowerBndIt : bounds[0]) {
+            for(const auto upperBndIt : bounds[1]) {
                 Poly lhs = (*lowerBndIt).constraint().coefficient(v,1)*v-(*lowerBndIt).constraint().lhs();
 
                 Poly rhs = (*upperBndIt).constraint().coefficient(v,1)*v-(*upperBndIt).constraint().lhs();
 
-                constraints.emplace_back(ConstraintT(lhs-rhs, carl::Relation::LEQ));
+                constraints.emplace_back(FormulaT(ConstraintT(lhs-rhs, carl::Relation::LEQ)));
             }
         }
 
         return constraints;
-    }
-
-    void FourierMotzkinQE::removeOldConstraints(const typename FourierMotzkinQE::boundIteratorPair& bounds) {
-
     }
 
     bool FourierMotzkinQE::isLinearLowerBound(const ConstraintT& c, carl::Variable v) {
--- src/lib/datastructures/qe/fm/FourierMotzkinQE.h
+++ src/lib/datastructures/qe/fm/FourierMotzkinQE.h
@@ -8,31 +8,28 @@ namespace smtrat::qe::fm{
  *
  */
 class FourierMotzkinQE {
+public:
+    // we use three vectors, one for the discovered lower bounds, one for the upper bounds and one for unrelated constraints.
+    using FormulaPartition = std::vector<std::vector<FormulaT::const_iterator>>;
+
 private:
     QEQuery mQuery;
     FormulaT mFormula;
 public:
 
-    // a vector of iterators on single bounds
-    using boundIteratorVector = std::vector<FormulaT::const_iterator>;
-    // a pair of vectors containing sets of iterators to lower, respectively upper bounds.
-    using boundIteratorPair = std::pair<boundIteratorVector, boundIteratorVector>;
-
     FourierMotzkinQE(const FormulaT& qfree, const QEQuery& quantifiers)
         : mQuery(quantifiers)
         , mFormula(qfree)
     {
-        assert(qfree.type() == FormulaType::CONSTRAINT || qfree.isRealConstraintConjunction());
+        assert(qfree.getType() == carl::FormulaType::CONSTRAINT || qfree.isRealConstraintConjunction());
     }
 
     FormulaT eliminateQuantifiers();
 
 private:
-    boundIteratorPair findBounds(const carl::Variable& variable);
+    FormulaPartition findBounds(const carl::Variable& variable);
 
-    std::vector<ConstraintT> createNewConstraints(const boundIteratorPair& bounds, carl::Variable v);
-
-    void removeOldConstraints(const boundIteratorPair& bounds);
+    carl::Formulas<Poly> createNewConstraints(const FormulaPartition& bounds, carl::Variable v);
 
     bool isLinearLowerBound(const ConstraintT& f, carl::Variable v);
 };
