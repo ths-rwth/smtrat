906775ce5e1fb648e7fe31d586cd3bdd42603b81
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-20 10:25:59 +0100

    Removed "old" qe code that was moved to smtrat-qe.

--- src/lib/datastructures/qe/cad/qe.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../../../Common.h"
-
-namespace smtrat::qe::cad {
-
-FormulaT eliminateQuantifiers(const FormulaT& qfree, const QEQuery& quantifiers);
-
-}
--- src/lib/datastructures/qe/fm/qe.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../../../Common.h"
-
-namespace smtrat::qe::fm {
-
-FormulaT eliminateQuantifiers(const FormulaT& qfree, const QEQuery& quantifiers);
-
-}
