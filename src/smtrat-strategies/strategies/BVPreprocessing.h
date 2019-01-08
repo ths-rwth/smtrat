593c959c3bb1709c243c910753045ae13452c25d
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-01-08 15:45:37 +0100

    Removing unused includes.

--- src/smtrat-strategies/strategies/BVPreprocessing.h
+++ src/smtrat-strategies/strategies/BVPreprocessing.h
@@ -5,13 +5,7 @@
 #pragma once
 
 #include <smtrat-solver/Manager.h>
-#include <smtrat-modules/EMModule/EMModule.h>
-#include <smtrat-modules/PFEModule/PFEModule.h>
-#include <smtrat-modules/SplitSOSModule/SplitSOSModule.h>
 #include <smtrat-modules/ESModule/ESModule.h>
-#include <smtrat-modules/ICEModule/ICEModule.h>
-#include <smtrat-modules/MCBModule/MCBModule.h>
-#include <smtrat-modules/GBPPModule/GBPPModule.h>
 #include <smtrat-modules/SymmetryModule/SymmetryModule.h>
 
 namespace smtrat
