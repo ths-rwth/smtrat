d115e006e002e1cb8c201c6a050bc45ff25a04d2
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-01-16 18:11:12 +0100

    Made answer a first-level citizen of benchmark results, changed how statistics are rendered in xml.

--- src/benchmax/tools/SMTRAT.h
+++ src/benchmax/tools/SMTRAT.h
@@ -44,15 +44,15 @@ public:
 	
 	virtual void additionalResults(const fs::path&, BenchmarkResult& result) const override {
 		switch (result.exitCode) {
-			case 2: result.additional["answer"] = "sat"; break;
-			case 3: result.additional["answer"] = "unsat"; break;
-			case 4: result.additional["answer"] = "unknown"; break;
-			case 5: result.additional["answer"] = "wrong"; break;
-			case 9: result.additional["answer"] = "nosuchfile"; break;
-			case 10: result.additional["answer"] = "parsererror"; break;
-			case 11: result.additional["answer"] = "timeout"; break;
-			case 12: result.additional["answer"] = "memout"; break;
-			default: result.additional["answer"] = getStatusFromOutput(result);
+			case 2: result.answer = "sat"; break;
+			case 3: result.answer = "unsat"; break;
+			case 4: result.answer = "unknown"; break;
+			case 5: result.answer = "wrong"; break;
+			case 9: result.answer = "nosuchfile"; break;
+			case 10: result.answer = "parsererror"; break;
+			case 11: result.answer = "timeout"; break;
+			case 12: result.answer = "memout"; break;
+			default: result.answer = getStatusFromOutput(result);
 		}
 		regex topRegex("\\(\\:(\\S+)\\s*\\(\\s*((?:\\:\\S+\\s*\\S+\\s*)+)\\)\\)");
 		regex subRegex("\\s*\\:(\\S+)\\s*(\\S+)");
