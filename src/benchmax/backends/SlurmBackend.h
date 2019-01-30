8c4ad09b31d98bd8c81fed9c598584aa651d778d
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-01-30 17:26:17 +0100

    Insert correct number of tasks.

--- src/benchmax/backends/SlurmBackend.h
+++ src/benchmax/backends/SlurmBackend.h
@@ -10,7 +10,6 @@ namespace benchmax {
 
 class SlurmBackend: public Backend {
 private:
-	static constexpr std::size_t num_slices = 1000;
 	void generateJobFile(const std::string& jobfile, std::size_t num_input) {
 		std::ofstream out("job_" + Settings::fileSuffix + ".job");
 		out << "#!/usr/bin/env zsh" << std::endl;
@@ -34,7 +33,7 @@ private:
 		out << "min=$SLURM_ARRAY_TASK_MIN" << std::endl;
 		out << "max=$SLURM_ARRAY_TASK_MAX" << std::endl;
 		out << "cur=$SLURM_ARRAY_TASK_ID" << std::endl;
-		out << "tasks=" << num_slices << std::endl;
+		out << "tasks=" << num_input << std::endl;
 		out << "jobcount=$(( max - min + 1 ))" << std::endl;
 		out << "slicesize=$(( (tasks + jobcount + 1) / jobcount ))" << std::endl;
 		out << "start=$(( (cur - 1) * slicesize + min ))" << std::endl;
