3e3e85f2e8f9f68906cbb79d6d9bc211e3c7b254
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-01-31 09:30:59 +0100

    Working on Slurm backend, starting to collect results.

--- src/benchmax/backends/SlurmBackend.h
+++ src/benchmax/backends/SlurmBackend.h
@@ -4,14 +4,53 @@
 
 #include "../logging.h"
 #include "../Settings.h"
+#include "../utils/Execute.h"
 #include "../utils/durations.h"
+
+#include <filesystem>
+#include <regex>
 
 namespace benchmax {
 
 class SlurmBackend: public Backend {
 private:
-	void generateJobFile(const std::string& jobfile, std::size_t num_input) {
-		std::ofstream out("job_" + Settings::fileSuffix + ".job");
+	int getJobID(const std::string& output) const {
+		std::regex r("Submitted batch job ([0-9]+)");
+		std::smatch m;
+		if (std::regex_search(output, m, r)) {
+			BENCHMAX_LOG_DEBUG("smtrat.slurm", "Job ID is " << m[1]);
+			return std::stoi(m[1]);
+		} else {
+			BENCHMAX_LOG_ERROR("smtrat.slurm", "Unable to obtain job id from slurm output.");
+			return -1;
+		}
+	}
+
+	void parse_result_file(const fs::path& file) const {
+
+	}
+
+	void parse_result_files(const fs::path& basedir) const {
+		BENCHMAX_LOG_DEBUG("smtrat.slurm", "Collecting results from " << basedir);
+
+		std::regex filere("# START ([0-9]+) #(.*)# END \1 #(?:(.*)# END DATA \1 #)?");
+
+		for (const auto& f: std::filesystem::directory_iterator(basedir)) {
+			std::cout << f.path() << std::endl;
+			std::ifstream in(f.path());
+			std::string str((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+			auto reBegin = sregex_iterator(str.begin(), str.end(), filere);
+			auto reEnd = sregex_iterator();
+			for (auto i = reBegin; i != reEnd; ++i) {
+				std::cout << "Found data for " << (*i)[1] << std::endl;
+			}
+		}
+	}
+
+	std::string generateSubmitFile(const std::string& jobfile, std::size_t num_input) {
+		std::string filename = Settings::outputDir + "/job_" + Settings::fileSuffix + ".job";
+		BENCHMAX_LOG_DEBUG("smtrat.slurm", "Writing submit file to " << filename);
+		std::ofstream out(filename);
 		out << "#!/usr/bin/env zsh" << std::endl;
 		out << "### Job name" << std::endl;
 		// Job name
@@ -44,19 +83,27 @@ private:
 		out << "for i in `seq ${start} ${end}`; do" << std::endl;
 		out << "\tcmd=$(sed -n \"${i}p\" < " << jobfile << ")" << std::endl;
 		out << "\techo \"Executing $cmd\"" << std::endl;
-		out << "\tdate +\"Start: %s%3N\"" << std::endl;
-		out << "\tulimit -S -v " << (Settings::memoryLimit * 1024) << " && ulimit -S -t " << timeout << " && time $cmd ; rc=$?" << std::endl;
-		out << "\tdate +\"End: %s%3N\"" << std::endl;
-		out << "\techo \"Exit code: $rc\"" << std::endl;
+		out << "\techo \"# START ${i} #\"" << std::endl;
+		out << "\techo \"# START ${i} #\" >&2" << std::endl;
+		out << "\tstart=`date +\"Start: %s%3N\"`" << std::endl;
+		out << "\tulimit -S -v " << (Settings::memoryLimit * 1024) << " && ulimit -S -t " << timeout << " && $cmd ; rc=$?" << std::endl;
+		out << "\tend=`date +\"End: %s%3N\"`" << std::endl;
+		out << "\techo \"# END ${i} #\"" << std::endl;
+		out << "\techo \"# END ${i} #\" >&2" << std::endl;
+		out << "\techo \"time: $(( end - start ))\"" << std::endl;
+		out << "\techo \"exitcode: $rc\"" << std::endl;
+		out << "\techo \"# END DATA ${i} #\"" << std::endl;
 		out << "done" << std::endl;
 		out.close();
+
+		return filename;
 	}
 public:
 	void run(const std::vector<Tool*>& tools, const std::vector<BenchmarkSet>& benchmarks) {
-		BENCHMAX_LOG_INFO("benchmax.slurm", "Generating ");
 
-		std::string jobfile = Settings::outputDir + "/jobs_" + Settings::fileSuffix + ".jobs";
-		std::ofstream jobs(jobfile);
+		std::string jobsfile = Settings::outputDir + "/jobs_" + Settings::fileSuffix + ".jobs";
+		BENCHMAX_LOG_INFO("benchmax.slurm", "Writing job file to " << jobsfile);
+		std::ofstream jobs(jobsfile);
 		std::size_t count = 0;
 		for (const Tool* tool: tools) {
 			for (const BenchmarkSet& set: benchmarks) {
@@ -67,7 +114,15 @@ public:
 			}
 		}
 		jobs.close();
-		generateJobFile(jobfile, count);
+		auto submitfile = generateSubmitFile(jobsfile, count);
+
+		BENCHMAX_LOG_INFO("benchmax.slurm", "Submitting job now.");
+		std::string output;
+		callProgram("sbatch --wait --array=1-1000 -N1 " + submitfile, output);
+		BENCHMAX_LOG_INFO("benchmax.slurm", "Job terminated.");
+
+		parse_result_files(Settings::outputDir);
+		BENCHMAX_LOG_INFO("benchmax.slurm", "Finished.");
 	}
 };
 
