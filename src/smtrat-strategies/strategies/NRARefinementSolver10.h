70564f53940c84014c8267068c0b6e98311f4f54
Barsha Zaman <mineme.barsha@gmail.com>
2019-02-05 11:29:57 +0100

    Add 21 solver for 21 diffrent settings.

---
+++ src/smtrat-strategies/strategies/NRARefinementSolver10.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-modules/LRAModule/LRAModule.h>
+#include <smtrat-modules/NRAILModule/NRAILModule.h>
+
+namespace smtrat
+{
+    /**
+     * Strategy description.
+     *
+     * @author
+     * @since
+     * @version
+     *
+     */
+    class NRARefinementSolver:
+            public Manager
+    {
+    public:
+        NRARefinementSolver(): Manager()
+        {
+            setStrategy(
+            {
+                addBackend<NRAILModule<NRAILSettings10>>(
+                {
+                    addBackend<SATModule<SATSettings1>>(
+                    {
+                        addBackend<LRAModule<LRASettings1>>()
+                    })
+                })
+            });
+        }
+    };
+} // namespace smtrat
