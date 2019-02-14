dd6c52fed5b2d3f4623c889515eaa47e0f7da7f1
Hanna Franzen <hanna.franzen@rwth-aachen.de>
2019-02-14 13:24:16 +0100

    WIP: restructuring of choosing sample

--- src/smtrat-cad/lifting/LiftingLevel.h
+++ src/smtrat-cad/lifting/LiftingLevel.h
@@ -29,20 +29,42 @@ namespace cad {
 			levelintervals.insert(inter.upper());
 		}
 
+		// @note check whether an unsat cover has been found before calling this
 		Sample chooseSample() {
 			std::set<RAN>::iterator it = levelintervals.begin();
-			if(it.getLowerBoundType == BoundType::INFTY) {
+			// go to the next bound (may equal last sample)
+			while(it->value > curSample) {
 				it.next();
 			}
 
-			//make sample here and check whether it is in some interval (open/closed!)
-			// first sample before first bound (in bound is not minf), 
-			//then between bounds, last one after last bound if not pinf 
+			if(it == levelintervals.begin() && it.getLowerBoundType == BoundType::INFTY) {
+				it.next();
+				if(it == levelintervals.end() && it.getUpperBoundType == BoundType::INFTY) {
+					//@todo case levelintervals = {-inf, +inf}: get some random value in between
+				}
+			}
+			else {
+
+			}
+
+			//make sample here and check whether it is in some interval (isInUnsatInterval)
+			// first sample before first bound (if bound is not minf), 
+			//then on & between bounds, last one after last bound if not pinf 
 		
 			for(auto interval : intervals) {
 				//@todo cases: minf, lower, equal, higher, pinf
 				//
 			}
+		}
+
+		// check whether the given value is in the list of unsat intervals
+		bool isInUnsatInterval(RAN val) {
+			for(auto inter : intervals) {
+				if(inter.contains(val)) {
+					return true;
+				}
+			}
+			return false;
 		}
 
 	public:
