8629c04e9535cf00e84f20c9d66933ae9ac397da
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-09-25 09:46:32 +0200

    Adding some strategies for NRA

---
+++ src/smtrat-strategies/strategies/NRA_ICPVSCAD.h
@@ -0,0 +1,40 @@
+/**
+ * @file NRASolver.h
+ */
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+
+#include <smtrat-modules/FPPModule/FPPModule.h>
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-modules/ICPModule/ICPModule.h>
+#include <smtrat-modules/VSModule/VSModule.h>
+#include <smtrat-modules/NewCADModule/NewCADModule.h>
+
+namespace smtrat
+{
+	/**
+	 * Strategy description.
+	 *
+	 * @author
+	 * @since
+	 * @version
+	 *
+	 */
+	class NRA_CAD: public Manager {
+	public:
+		NRA_CAD(): Manager() {
+			setStrategy({
+				addBackend<FPPModule<FPPSettings1>>(
+					addBackend<SATModule<SATSettings1>>(
+						addBackend<ICPModule<ICPSettings1>>(
+							addBackend<VSModule<VSSettings234>>(
+								addBackend<NewCADModule<NewCADSettingsFOS>>()
+							)
+						)
+					)
+				)
+			});
+		}
+	};
+} // namespace smtrat
