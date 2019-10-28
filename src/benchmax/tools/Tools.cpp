53e79fc10b48f9ed496810ed9ca8680144374cf0
Jasper Nalbach <derjasper@posteo.de>
2019-10-28 12:09:22 +0100

    fix bug

--- src/benchmax/tools/Tools.cpp
+++ src/benchmax/tools/Tools.cpp
@@ -54,7 +54,7 @@ namespace settings {
 /// Postprocess settings to compute common prefix.
 bool finalize_tool_settings(ToolSettings& s, const boost::program_options::variables_map&) {
 	s.tools_common_prefix = common_prefix({
-		s.tools_generic, s.tools_smtrat, s.tools_smtrat_opb,
+		s.tools_generic, s.tools_smtrat, s.tools_smtrat_opb, s.tools_smtrat_analyzer,
 		s.tools_minisatp, s.tools_z3
 	});
 	BENCHMAX_LOG_DEBUG("benchmax.tools", "Common tool prefix is " << s.tools_common_prefix);
