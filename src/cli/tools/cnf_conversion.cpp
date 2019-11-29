c8b7e335a27633b85bb3b31a5dade2df9d9ad616
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-11-29 15:31:39 +0100

    Adding new cli tool for stand-alone cnf conversion.

---
+++ src/cli/tools/cnf_conversion.cpp
@@ -0,0 +1,70 @@
+#include "cnf_conversion.h"
+
+#include "config.h"
+#include <smtrat-common/smtrat-common.h>
+
+#ifdef CLI_ENABLE_CNF_CONVERSION
+
+#include "parser_smtlib_utils.h"
+#include "execute_smtlib.h"
+
+#include <carl/formula/helpers/to_cnf.h>
+#include <carl-io/DIMACSExporter.h>
+#include <carl-io/SMTLIBStream.h>
+
+namespace smtrat {
+
+int convert_to_cnf_dimacs(const std::string& filename, const std::string& outfile) {
+	auto e = parseformula::FormulaCollector();
+	executeFile(filename, e);
+
+	carl::DIMACSExporter<Poly> exporter;
+	exporter(carl::to_cnf(e.getFormula()));
+
+	if (outfile.empty()) {
+		e.regular() << exporter;
+	} else {
+		std::ofstream file(outfile);
+		file << exporter;
+		file.close();
+	}
+
+	return 0;
+}
+
+int convert_to_cnf_smtlib(const std::string& filename, const std::string& outfile) {
+	auto e = parseformula::FormulaCollector();
+	executeFile(filename, e);
+
+	auto f_in_cnf = carl::to_cnf(e.getFormula());
+
+	if (outfile.empty()) {
+		e.regular() << carl::outputSMTLIB(e.getLogic(), { f_in_cnf });
+	} else {
+		std::ofstream file(outfile);
+		file << carl::outputSMTLIB(e.getLogic(), { f_in_cnf });
+		file.close();
+	}
+
+	return 0;
+}
+
+}
+
+#else
+
+namespace smtrat {
+
+int convert_to_cnf_dimacs(const std::string&, const std::string&) {
+	SMTRAT_LOG_ERROR("smtrat", "This version of SMT-RAT was compiled without support for stand-alone CNF conversion.");
+	return 0;
+}
+
+int convert_to_cnf_smtlib(const std::string&, const std::string&) {
+	SMTRAT_LOG_ERROR("smtrat", "This version of SMT-RAT was compiled without support for stand-alone CNF conversion.");
+	return 0;
+}
+
+}
+
+#endif
