a43c06e8b257d30f549100d12a6354e8298cff80
Gereon <gereon.kremer@cs.rwth-aachen.de>
2020-01-22 08:55:57 +0100

    Migrating to latest macos version.

--- .ci/travis_generate.py
+++ .ci/travis_generate.py
@@ -32,8 +32,8 @@ jobs = [
 	#job("2-macos", ["build", "xcode10", "build.sh", "mayfail"]),
 	#job("2-macos", ["dependencies", "xcode10.1", "build.sh"]),
 	#job("2-macos", ["build", "xcode10.1", "build.sh", "mayfail"]),
-	job("2-macos", ["dependencies", "xcode10.2", "build.sh"]),
-	job("2-macos", ["build", "xcode10.2", "build.sh", "mayfail"]),
+	job("2-macos", ["dependencies", "xcode11.3", "build.sh"]),
+	job("2-macos", ["build", "xcode11.3", "build.sh", "mayfail"]),
 	job("3-docs", ["build", "linux", "g++-9", "task.documentation", "j1", "build.sh"]),
 	job("4-tidy", ["build", "linux", "clang-9.0", "task.tidy", "build.sh", "mayfail"]),
 ]
