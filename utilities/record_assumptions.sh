9bac759e7be86d95ea3869f62295fd11bb76eaf0
Jasper Nalbach <derjasper@posteo.de>
2020-04-02 12:37:23 +0200

    remove obsolete utilities

--- utilities/record_assumptions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-OUTPUTFILE=$1
-SOLVER=$2
-LOGFILE="$OUTPUTFILE.log"
-echo -ne "Recording certifications from $SOLVER in certification\_$OUTPUTFILE\_*.smt2."
-echo "" > $LOGFILE
-for f in ~/Dokumente/InformatikStudium/Doktorarbeit/reals_diss/software/benchmarks/sets/smtlib/qf_nra/bugs/wrong_results/*; do echo -ne "Checking $f..." >> $LOGFILE && ./smtratSolver $f >> $LOGFILE 2>>$LOGFILE && mv -v assumptions_to_check.smt2 certification\_$OUTPUTFILE\_$(basename $f) >>$LOGFILE; done
-
--- utilities/verify_assumptions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for f in certification_*.smt2; do echo "Verifying $f..." && (z3 $f|grep "error"); done
-
