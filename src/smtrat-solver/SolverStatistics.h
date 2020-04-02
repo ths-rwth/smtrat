0ea17e275d93342529333cc98dde66e3b1cc0c11
Jasper Nalbach <derjasper@posteo.de>
2020-04-02 14:08:16 +0200

    cleanup solverstatistics

--- src/smtrat-solver/SolverStatistics.h
+++ src/smtrat-solver/SolverStatistics.h
@@ -16,9 +16,6 @@ namespace smtrat
             addKeyValuePair( "boolean_variables", carl::VariablePool::getInstance().nrVariables(carl::VariableType::VT_BOOL) );
             addKeyValuePair( "real_variables", carl::VariablePool::getInstance().nrVariables(carl::VariableType::VT_REAL) );
             addKeyValuePair( "integer_variables", carl::VariablePool::getInstance().nrVariables(carl::VariableType::VT_INT) );
-            addKeyValuePair( "constraints", carl::constraintPool<Poly>().size() );
-            addKeyValuePair( "non-linear_constraints", carl::constraintPool<Poly>().nrNonLinearConstraints() );
-            addKeyValuePair( "maximal_degree", carl::constraintPool<Poly>().maxDegree() );
             addKeyValuePair( "number_of_learned_branching_lemmas", mNumberOfBranchingLemmas );
          }
         
