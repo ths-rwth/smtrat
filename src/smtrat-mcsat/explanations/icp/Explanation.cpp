12417c5968c3681f6fc0d773286b3cd21f680bbb
Jasper Nalbach <derjasper@posteo.de>
2020-07-29 10:30:52 +0200

    mcsat icp statistics

--- src/smtrat-mcsat/explanations/icp/Explanation.cpp
+++ src/smtrat-mcsat/explanations/icp/Explanation.cpp
@@ -7,11 +7,18 @@ namespace mcsat {
 namespace icp {
 
 boost::optional<mcsat::Explanation> Explanation::operator()(const mcsat::Bookkeeping& data, carl::Variable var, const FormulasT& reason) const {
+	#ifdef SMTRAT_DEVOPTION_Statistics
+    mStatistics.explanationCalled();
+    #endif
+
 	SMTRAT_LOG_DEBUG("smtrat.mcsat.icp", "Explain conflict " << data.constraints());
 
 	IntervalPropagation ip(std::vector<carl::Variable>(data.variables().begin(), data.variables().end()), data.constraints(), data.model());
 	auto res = ip.execute();
 	if (res) {
+		#ifdef SMTRAT_DEVOPTION_Statistics
+        mStatistics.explanationSuccess();
+        #endif
 		SMTRAT_LOG_DEBUG("smtrat.mcsat.icp", "Got conflict " << *res);
 		return mcsat::Explanation(*res);
 	} else {
--- src/smtrat-mcsat/explanations/icp/Explanation.h
+++ src/smtrat-mcsat/explanations/icp/Explanation.h
@@ -3,11 +3,17 @@
 #include <smtrat-common/smtrat-common.h>
 #include <smtrat-mcsat/smtrat-mcsat.h>
 
+#include "ICPStatistics.h"
+
 namespace smtrat {
 namespace mcsat {
 namespace icp {
 
 struct Explanation {
+#ifdef SMTRAT_DEVOPTION_Statistics
+	ICPStatistics& mStatistics = statistics_get<ICPStatistics>("mcsat-explanation-icp");
+#endif
+
 	boost::optional<mcsat::Explanation> operator()(const mcsat::Bookkeeping& data, carl::Variable var, const FormulasT& reason) const;
 };
 
