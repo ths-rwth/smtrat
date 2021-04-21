d89e99df38dd76f9e3aa9150d2919bde04e058bc
Jasper Nalbach <derjasper@posteo.de>
2021-04-21 17:44:16 +0200

    do not consider whether a sample is a root or not

--- src/smtrat-mcsat/assignments/arithmetic/AssignmentFinder_arithmetic.h
+++ src/smtrat-mcsat/assignments/arithmetic/AssignmentFinder_arithmetic.h
@@ -57,7 +57,10 @@ private:
 		const auto& l = mRI.sampleFrom(lhs);
 		const auto& r = mRI.sampleFrom(rhs);
 		SMTRAT_LOG_DEBUG("smtrat.mcsat.assignmentfinder", l << " (" << mRI.is_root(lhs) << ") < " << r << " (" << mRI.is_root(rhs) << ") ?");
-		if ((l.is_integral() || l.is_numeric()) && (r.is_integral() || r.is_numeric()) && (mRI.is_root(lhs) != mRI.is_root(rhs))) return !mRI.is_root(lhs);
+		// this is more like z3, but performs worse:
+		// if ((l.is_integral() || l.is_numeric()) && (r.is_integral() || r.is_numeric()) && (mRI.is_root(lhs) != mRI.is_root(rhs))) return !mRI.is_root(lhs);
+		// even the opposite performs better (but not better than not respecting samples being a root):
+		// if ((l.is_integral() || l.is_numeric()) && (r.is_integral() || r.is_numeric()) && (mRI.is_root(lhs) != mRI.is_root(rhs))) return mRI.is_root(lhs);
 		if (l.is_integral() != r.is_integral()) return l.is_integral();
 		if (l.is_numeric() != r.is_numeric()) return l.is_numeric();
 		if (l.size() != r.size()) return l.size() < r.size();
