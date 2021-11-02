c68aa730ea107bd9e2d4430dff11ae4a4f3003ac
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2021-11-02 15:07:58 +0100

    benchmax evaluation utility

---
+++ utilities/benchmax/evaluation/__init__.py
@@ -0,0 +1,4 @@
+from .load import xml_to_pandas, xmls_to_pandas
+from .data import filter_solved, cumulate_by_column, virtual_best
+from .plots import performance_profile, scatter_plot
+from .inspect import *
--- utilities/evaluation/__init__.py
+++ utilities/evaluation/__init__.py
@@ -1,9 +1,4 @@
-from .load import xml_to_pandas, xmls_to_pandas
-from .data import filter_solved, cumulate_by_column, virtual_best
-from .plots import performance_profile, scatter_plot
-from .inspect import *
-
 # TODO remove:
-from .legacy.collect import load_file
-from .legacy.pdadaptor import get_table,get_table_by_example
-from .legacy.database import reset,get_solvers,conn
+from .collect import load_file
+from .pdadaptor import get_table,get_table_by_example
+from .database import reset,get_solvers,conn
