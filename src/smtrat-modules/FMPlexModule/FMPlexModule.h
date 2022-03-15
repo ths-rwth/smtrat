1e9213596ae4e891636b62ad4da48e4c3c275367
sstein <svenja.stein@rwth-aachen.de>
2022-03-15 17:21:47 +0100

    Basic File Structure Created

---
+++ src/smtrat-modules/FMPlexModule/FMPlexModule.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <smtrat-solver/Module.h>
+#include "FMPlexSettings.h"
+namespace smtrat {
+	template<typename Settings>
+	class FMPlexModule : public Module {};
+}
---
+++ src/smtrat-modules/FMPlexModule/FMPlexSettings.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace smtrat {
+	struct FMPlexSettings1 {
+		static constexpr auto moduleName = "FMPlexModule<FMPlexSettings1>";
+	};
+}
