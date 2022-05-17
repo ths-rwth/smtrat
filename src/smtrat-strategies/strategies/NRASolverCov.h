c244e70f3ece93814136ea65b57869c5c9ed5a88
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-05-17 15:37:28 +0200

    add NRASolver with covering module

---
+++ src/smtrat-strategies/strategies/NRASolverCov.h
@@ -0,0 +1,54 @@
+/**
+ * @file NRASolver.h
+ */
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+
+#include <smtrat-modules/FPPModule/FPPModule.h>
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-modules/STropModule/STropModule.h>
+#include <smtrat-modules/ICPModule/ICPModule.h>
+#include <smtrat-modules/VSModule/VSModule.h>
+#include <smtrat-modules/NewCADModule/NewCADModule.h>
+
+namespace smtrat
+{
+    /**
+     * Strategy description.
+     *
+     * @author
+     * @since
+     * @version
+     *
+     */
+    class NRASolver:
+        public Manager
+    {
+        public:
+            NRASolver(): Manager()
+            {
+                setStrategy(
+                {
+                    addBackend<FPPModule<FPPSettings1>>(
+                    {
+                        addBackend<SATModule<SATSettings1>>(
+                        {
+                            addBackend<STropModule<STropSettings1>>(
+                            {
+                                addBackend<ICPModule<ICPSettings1>>(
+                                {
+                                    addBackend<VSModule<VSSettings234>>(
+                                    {
+                                        addBackend<NewCoveringModule<NewCoveringSettings1>>({
+                                            addBackend<NewCADModule<NewCADSettingsFOS>>()
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                });
+            }
+    };
+} // namespace smtrat
