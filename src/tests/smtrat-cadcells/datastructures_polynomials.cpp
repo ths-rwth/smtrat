f5255e572c9b1718fd3ca72396055e70e62b2d71
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-08-04 14:37:05 +0200

    adapt unit test

--- src/tests/smtrat-cadcells/datastructures_polynomials.cpp
+++ src/tests/smtrat-cadcells/datastructures_polynomials.cpp
@@ -6,7 +6,7 @@ TEST(smtrat_cadcells, datastructures_polynomials)
 {
 	smtrat::cadcells::VariableOrdering vrs;
 	smtrat::cadcells::Polynomial::ContextType ctx(vrs);
-	smtrat::cadcells::Polynomial poly(ctx, 2);
+	smtrat::cadcells::Polynomial poly(ctx, 1);
 	smtrat::cadcells::datastructures::PolyPool pool(ctx);
 	ASSERT_EQ(pool(pool(poly)), poly);
 }
