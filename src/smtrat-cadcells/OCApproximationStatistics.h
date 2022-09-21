75777c1cd7f1ab2b198961962ebe57ba07117f19
Valentin Promies <valentin.promies@rwth-aachen.de>
2022-09-21 10:29:14 +0200

    refactor approximation statistics

--- src/smtrat-cadcells/OCApproximationStatistics.h
+++ src/smtrat-cadcells/OCApproximationStatistics.h
@@ -11,30 +11,30 @@ class OCApproximationStatistics : public Statistics {
 private:
 	using Counter = std::map<std::size_t, std::size_t>;
 
-	bool mCurrentlyApproximated = false; // helper variable: is the current cell approximated?
+	bool m_currently_approximated = false; // helper variable: is the current cell approximated?
 
-	std::size_t mConsidered = 0; 			// #calls using the approximation heuristic
-	std::size_t mApproximated = 0; 			// #calls actually approximating
-	std::size_t mApproximatedSuccess = 0; 	// #successful calls actually approximating
+	std::size_t m_considered_cells = 0; 			// #calls using the approximation heuristic
+	std::size_t m_approximated_cells = 0; 			// #calls actually approximating
+	std::size_t m_approximated_cells_success = 0; 	// #successful calls actually approximating
 
-	Counter mApproximatedDegrees;	// Counts approximations for each degree
+	Counter m_approximated_degree_counter;	// Counts approximations for each degree
 
-	std::vector<std::pair<std::size_t,std::size_t>>  mTaylorIgnoredVars; // For each taylor-approximated polynomial, #variables left out and # variables
-	std::size_t mTaylorFailure = 0; // #times the taylor method fails because of gradient 0
+	std::vector<std::pair<std::size_t,std::size_t>>  m_taylor_ignored; // For each taylor-approximated polynomial, #variables left out and # variables
+	std::size_t m_taylor_grad_zero = 0; // #times the taylor method fails because of gradient 0
 
-	std::size_t mUnboundedLevels = 0; 			// #unbounded levels in the constructed cells
-	std::size_t mHalfUnboundedLevels = 0; 		// #one side unbounded levels in the constructed cells
-	std::vector<std::size_t> mCellDimensions;	// Dimension of the constructed cells
+	std::size_t m_unbounded_levels = 0; 		// #unbounded levels in the constructed cells
+	std::size_t m_half_unbounded_levels = 0; 	// #one side unbounded levels in the constructed cells
+	std::vector<std::size_t> m_cell_dimensions;	// Dimension of the constructed cells
 
-	std::size_t mResultants = 0; 	// #computed resultants
-	std::size_t mDiscriminants = 0; // #computed discriminants
-	std::size_t mCoefficients = 0; 	// #computed leading coefficients
-	Counter mDegrees; 				// Counts the occuring degrees of all constructed polynomials
+	std::size_t m_resultants = 0; 			// #computed resultants
+	std::size_t m_discriminants = 0; 		// #computed discriminants
+	std::size_t m_ldcfs = 0; 				// #computed leading coefficients
+	Counter m_constructed_degree_counter; 	// Counts the occuring degrees of all constructed polynomials
 
-	std::size_t mInvolvedTooOften = 0; 	// #times a constraint was involved in too many conflicts
-	std::size_t mPolyApxTooOften = 0; 	// #times a poly was approximated too often
-	bool mMaxConsideredReached = false; // flag whether the max number of apx considered is reached
-	bool mMaxApxReached = false; 		// flag whether the max number of apx is reached
+	std::size_t m_involved_too_often = 0; 	// #times a constraint was involved in too many conflicts
+	std::size_t m_apx_too_often = 0; 		// #times a poly was approximated too often
+	bool m_max_considered_reached = false; 	// flag whether the max number of apx considered is reached
+	bool m_max_apx_reached = false; 		// flag whether the max number of apx is reached
 
 	void collectCounterStats(Counter c, const std::string& name) {
 		std::size_t max = 0;
@@ -57,76 +57,76 @@ public:
 	}
 
 	void collect() {
-		Statistics::addKeyValuePair("considered", mConsidered);
-		Statistics::addKeyValuePair("approximated", mApproximated);
-		Statistics::addKeyValuePair("success", mApproximatedSuccess);
+		Statistics::addKeyValuePair("considered", m_considered_cells);
+		Statistics::addKeyValuePair("approximated", m_approximated_cells);
+		Statistics::addKeyValuePair("success", m_approximated_cells_success);
 
-		collectCounterStats(mApproximatedDegrees, "apx_degrees");
+		collectCounterStats(m_approximated_degree_counter, "apx_degrees");
 
-		Statistics::addKeyValuePair("unbounded_levels", mUnboundedLevels);
-		Statistics::addKeyValuePair("half_unbounded_levels", mHalfUnboundedLevels);
+		Statistics::addKeyValuePair("unbounded_levels", m_unbounded_levels);
+		Statistics::addKeyValuePair("half_unbounded_levels", m_half_unbounded_levels);
 		std::size_t sumDimensions = 0;
-		for (const std::size_t d : mCellDimensions) sumDimensions += d;
-		Statistics::addKeyValuePair("mean_cell_dimension", ((double) sumDimensions) / ((double) mCellDimensions.size()));
+		for (const std::size_t d : m_cell_dimensions) sumDimensions += d;
+		Statistics::addKeyValuePair("mean_cell_dimension", ((double) sumDimensions) / ((double) m_cell_dimensions.size()));
 
 		std::size_t maxIgnoredVars = 0;
 		double sum = 0.0;
 		std::size_t total = 0;
-		for (const auto& item : mTaylorIgnoredVars) {
+		for (const auto& item : m_taylor_ignored) {
 			sum += ((double) item.first) / ((double) item.second);
 			total += item.first;
 			if (item.first > maxIgnoredVars) maxIgnoredVars = item.first;
 		}
-		double meanIgnoredVars = sum/((double) mTaylorIgnoredVars.size());
+		double meanIgnoredVars = sum/((double) m_taylor_ignored.size());
 		Statistics::addKeyValuePair("max_taylor_ignored_vars", maxIgnoredVars);
 		Statistics::addKeyValuePair("total_taylor_ignored_vars", total);
 		Statistics::addKeyValuePair("mean_taylor_ignored_vars", meanIgnoredVars);
-		Statistics::addKeyValuePair("taylor_failure", mTaylorFailure);
+		Statistics::addKeyValuePair("taylor_failure", m_taylor_grad_zero);
 
-		Statistics::addKeyValuePair("resultants", mResultants);
-		Statistics::addKeyValuePair("discriminants", mDiscriminants);
-		Statistics::addKeyValuePair("leading_coefficients", mCoefficients);
-		collectCounterStats(mDegrees, "construction_degrees");
+		Statistics::addKeyValuePair("resultants", m_resultants);
+		Statistics::addKeyValuePair("discriminants", m_discriminants);
+		Statistics::addKeyValuePair("leading_coefficients", m_ldcfs);
+		collectCounterStats(m_constructed_degree_counter, "construction_degrees");
 
-		Statistics::addKeyValuePair("involved_to_often", mInvolvedTooOften);
-		Statistics::addKeyValuePair("apx_to_often", mPolyApxTooOften);
-		Statistics::addKeyValuePair("max_considered_reached", mMaxConsideredReached);
-		Statistics::addKeyValuePair("max_apx_reached", mMaxApxReached);
+		Statistics::addKeyValuePair("involved_too_often", m_involved_too_often);
+		Statistics::addKeyValuePair("apx_too_often", m_apx_too_often);
+		Statistics::addKeyValuePair("max_considered_reached", m_max_considered_reached);
+		Statistics::addKeyValuePair("max_apx_reached", m_max_apx_reached);
 	}
 
 	void newCell() {
-		mCurrentlyApproximated = false;
+		m_currently_approximated = false;
 	}
 
 	void success(std::size_t d) {
-		if (mCurrentlyApproximated) ++mApproximatedSuccess;
-		mCellDimensions.push_back(d);
+		if (m_currently_approximated) ++m_approximated_cells_success;
+		m_cell_dimensions.push_back(d);
 	}
-	void approximationConsidered() {++mConsidered;}
+	void approximationConsidered() {++m_considered_cells;}
 	void approximated(std::size_t d) {
-		++mApproximatedDegrees[d];
-		if (!mCurrentlyApproximated) {
-			++mApproximated;
-			 mCurrentlyApproximated = true;
+		++m_approximated_degree_counter[d];
+		if (!m_currently_approximated) {
+			++m_approximated_cells;
+			 m_currently_approximated = true;
 		}
 	}
 
-	void taylorIgnoredVars(std::size_t ignored, std::size_t total) {mTaylorIgnoredVars.emplace_back(ignored, total);}
-	void taylorFailure() {++mTaylorFailure;}
+	void taylorIgnoredVars(std::size_t ignored, std::size_t total) {m_taylor_ignored.emplace_back(ignored, total);}
+	void taylorFailure() {++m_taylor_grad_zero;}
 
-	void unboundedLevel() {++mUnboundedLevels;}
-	void halfUnboundedLevel() {++mHalfUnboundedLevels;}
-	void cellDimension(std::size_t d) {mCellDimensions.push_back(d);}
+	void unboundedLevel() {++m_unbounded_levels;}
+	void halfUnboundedLevel() {++m_half_unbounded_levels;}
+	void cellDimension(std::size_t d) {m_cell_dimensions.push_back(d);}
 
-	void resultant() {++mResultants;}
-	void discriminant() {++mDiscriminants;}
-	void coefficient() {++mCoefficients;}
-	void degree(std::size_t d) {++mDegrees[d];}
+	void resultant() {++m_resultants;}
+	void discriminant() {++m_discriminants;}
+	void coefficient() {++m_ldcfs;}
+	void degree(std::size_t d) {++m_constructed_degree_counter[d];}
 
-	void involvedTooOften() {++mInvolvedTooOften;}
-	void apxTooOften() {++mPolyApxTooOften;}
-	void maxConsideredReached() {mMaxConsideredReached = true;}
-	void maxApxReached() {mMaxApxReached = true;}
+	void involvedTooOften() {++m_involved_too_often;}
+	void apxTooOften() {++m_apx_too_often;}
+	void maxConsideredReached() {m_max_considered_reached = true;}
+	void maxApxReached() {m_max_apx_reached = true;}
 
 	static OCApproximationStatistics& get_instance() {
 		static OCApproximationStatistics& statistics = statistics_get<OCApproximationStatistics>("onecell-approximation");
