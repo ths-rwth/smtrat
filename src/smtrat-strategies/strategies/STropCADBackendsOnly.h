17c89eb291cb567e8c993d08ffe9212f6cc62004
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-10-24 14:31:07 +0200

    add CAD only strategies

---
+++ src/smtrat-strategies/strategies/STropCADBackendsOnly.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+
+#include <smtrat-modules/FPPModule/FPPModule.h>
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-modules/NewCoveringModule/NewCoveringModule.h>
+#include <smtrat-modules/NewCADModule/NewCADModule.h>
+
+namespace smtrat
+{
+    /**
+     * Strategy description.
+     *
+     * @author
+     * @since
+     * @version
+     *
+     */
+    class STropCADBackendsOnly:
+        public Manager
+    {
+        public:
+            STropCADBackendsOnly(): Manager()
+            {
+                setStrategy(
+                {
+                    addBackend<FPPModule<FPPSettings1>>(
+                    {
+                        addBackend<SATModule<SATSettings1>>(
+                        {
+                            addBackend<NewCoveringModule<NewCoveringSettings2>>({
+                                addBackend<NewCADModule<NewCADSettingsFOS>>()
+                            })
+                        })
+                    })
+                });
+            }
+    };
+} // namespace smtrat
