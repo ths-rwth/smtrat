cf23510ea234b3c0ddec95e1baddc4dff99a040f
Valentin Promies <valentin.promies@rwth-aachen.de>
2022-11-09 08:54:11 +0100

    add fmplex statistics

---
+++ src/smtrat-modules/NewFMPlexModule/NewFMPlexStatistics.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <smtrat-common/statistics/Statistics.h>
+
+#ifdef SMTRAT_DEVOPTION_Statistics
+
+namespace smtrat {
+
+class FMPlexStatistics : public Statistics {
+		private:
+			std::size_t m_global_conflicts = 0;
+			std::size_t m_local_conflicts = 0;
+			std::size_t m_generated_constraints = 0;
+
+		public:
+			void collect() { // called after the solving process to collect statistics
+				Statistics::addKeyValuePair("gConflicts", mGlobalConflicts);
+				Statistics::addKeyValuePair("lConflicts", mLocalConflicts);
+				Statistics::addKeyValuePair("tConflicts", mGlobalConflicts + mLocalConflicts);
+				Statistics::addKeyValuePair("generatedConstraints", mGeneratedConstraints);
+			}
+		};
+} // namespace smtrat
+
+#endif
