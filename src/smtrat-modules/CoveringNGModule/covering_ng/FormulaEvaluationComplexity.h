5481a06464ba5c32c8b01b27b3fa553ae47b9f74
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-12-07 11:30:26 +0100

    adapt ordering

--- src/smtrat-modules/CoveringNGModule/covering_ng/FormulaEvaluationComplexity.h
+++ src/smtrat-modules/CoveringNGModule/covering_ng/FormulaEvaluationComplexity.h
@@ -24,10 +24,22 @@ inline bool min_tdeg_min_size_implicant(const boost::container::flat_set<cadcell
 }
 
 inline bool min_size_min_tdeg_implicant(const boost::container::flat_set<cadcells::Constraint>& a, const boost::container::flat_set<cadcells::Constraint>& b) {
-    if (a.size() < b.size()) return true;
+    // TODO this is rather inefficient
+    std::vector<std::size_t> a_levels;
+    for (const auto& el : a) a_levels.push_back(carl::level_of(el.lhs()));
+    std::vector<std::size_t> b_levels;
+    for (const auto& el : b) b_levels.push_back(carl::level_of(el.lhs()));
+    auto level = *std::max_element(a_levels.begin(), a_levels.end());
+    assert(level == *std::max_element(b_levels.begin(), b_levels.end()));
+
+    auto a_ml_size = std::count(a_levels.begin(), a_levels.end(), level);
+    auto b_ml_size = std::count(b_levels.begin(), b_levels.end(), level);
+
+    if (a_ml_size < b_ml_size) return true;
+    else if (a_ml_size > b_ml_size) return false;
+    else if (a.size() < b.size()) return true;
     else if (a.size() > b.size()) return false;
     else {
-        // TODO this is rather inefficient
         std::size_t a_max_total_degree = 0;
         for (const auto& el : a) {
             a_max_total_degree = std::max(a_max_total_degree, el.lhs().total_degree());
