ff4bcd97a55d473761928e7071f83df6064f0729
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2023-10-05 16:49:45 +0200

    minor compier warning

--- src/smtrat-cadcells/operators/rules_pdel.h
+++ src/smtrat-cadcells/operators/rules_pdel.h
@@ -78,7 +78,7 @@ void root_ordering_holds_pdel(datastructures::SampledDerivation<P>& deriv, const
 }
 
 template<typename P>
-void poly_irreducible_sgn_inv_pdel(datastructures::SampledDerivation<P>& deriv, const datastructures::SymbolicInterval& cell, const datastructures::IndexedRootOrdering& ordering, const boost::container::flat_set<datastructures::PolyRef>& ordering_non_projective_polys, datastructures::PolyRef poly) {
+void poly_irreducible_sgn_inv_pdel(datastructures::SampledDerivation<P>& deriv, const datastructures::SymbolicInterval& cell, const datastructures::IndexedRootOrdering& /*ordering*/, const boost::container::flat_set<datastructures::PolyRef>& ordering_non_projective_polys, datastructures::PolyRef poly) {
     SMTRAT_LOG_TRACE("smtrat.cadcells.operators.rules", "sgn_inv(" << poly << "), " << poly << " irreducible");
     if (ordering_non_projective_polys.find(poly) != ordering_non_projective_polys.end()) {
         deriv.insert(properties::poly_del{ poly });
