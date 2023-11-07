19dae8a7b25893eb6c54f3276413e93df9b3cca9
Valentin Promies <valentin.promies@rwth-aachen.de>
2023-11-07 13:20:56 +0100

    fix

--- src/smtrat-qe/fmplex/qe.cpp
+++ src/smtrat-qe/fmplex/qe.cpp
@@ -5,10 +5,10 @@
 namespace smtrat::qe::fmplex {
 
 FormulaT eliminateQuantifiers(const FormulaT& qfree, const QEQuery& quantifiers) {
-	SMTRAT_TIME_START(t)
+	SMTRAT_TIME_START(t);
 	FMplexQE elim(qfree, quantifiers);
 	FormulaT result = elim.eliminate_quantifiers();
-	SMTRAT_TIME_FINISH(FMplexQEStatistics::get_instance().timer(), start);
+	SMTRAT_TIME_FINISH(FMplexQEStatistics::get_instance().timer(), t);
 	SMTRAT_VALIDATION_ADD("smtrat.qe.fmplex","output", result, true);
 	SMTRAT_STATISTICS_CALL(
 		FMplexQEStatistics::get_instance().output(result.is_nary() ? result.subformulas().size() : 1)
