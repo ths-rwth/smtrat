d08a767f9fa32c515dee13e0ea2057759bf37f6c
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2023-12-11 12:33:35 +0100

    strategies

--- src/smtrat-strategies/strategies/CoveringNG/PPGBDefault.h
+++ src/smtrat-strategies/strategies/CoveringNG/PPGBDefault.h
@@ -1,16 +1,20 @@
 #pragma once
 
 #include <smtrat-modules/CoveringNGModule/CoveringNGModule.h>
+#include <smtrat-modules/NewGBPPModule/NewGBPPModule.h>
 #include <smtrat-modules/FPPModule/FPPModule.h>
+
 #include <smtrat-solver/Manager.h>
 
 namespace smtrat {
-class CoveringNG_PP: public Manager {
+class CoveringNG_PPGBDefault: public Manager {
 public:
-	CoveringNG_PP() : Manager() {
+	CoveringNG_PPGBDefault() : Manager() {
 		setStrategy(
 			addBackend<FPPModule<FPPSettings1>>({
-                addBackend<CoveringNGModule<CoveringNGSettingsDefault>>()
+				addBackend<NewGBPPModule<NewGBPPSettings1>>({
+                	addBackend<CoveringNGModule<CoveringNGSettingsDefault>>()
+				})
             })
         );
 	}
