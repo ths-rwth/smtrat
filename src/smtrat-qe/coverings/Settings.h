1b2a3b50bf7591cd7e40587e43d10159868236ed
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2024-01-08 11:15:54 +0100

    qe covering settings

--- src/smtrat-qe/coverings/Settings.h
+++ src/smtrat-qe/coverings/Settings.h
@@ -8,19 +8,26 @@
 namespace smtrat::qe::coverings {
 struct DefaultSettings {
 	// Variable ordering
-	static constexpr covering_ng::variables::VariableOrderingHeuristics variable_ordering_heuristic = covering_ng::variables::VariableOrderingHeuristics::GreedyMaxUnivariate;
+    static constexpr covering_ng::variables::VariableOrderingHeuristics variable_ordering_heuristic = covering_ng::variables::VariableOrderingHeuristics::GreedyMaxUnivariate;
 
-	// Projection operator
-	using op = cadcells::operators::MccallumComplete;
-	static constexpr cadcells::representation::CellHeuristic cell_heuristic = cadcells::representation::BIGGEST_CELL;
-	static constexpr cadcells::representation::CoveringHeuristic covering_heuristic = cadcells::representation::BIGGEST_CELL_COVERING_MIN_TDEG;
-	static constexpr covering_ng::SamplingAlgorithm sampling_algorithm = covering_ng::SamplingAlgorithm::LOWER_UPPER_BETWEEN_SAMPLING;
+    // Projection operator
+    using op = cadcells::operators::MccallumComplete;
+    static constexpr cadcells::representation::CellHeuristic cell_heuristic = cadcells::representation::BIGGEST_CELL;
+    static constexpr cadcells::representation::CoveringHeuristic covering_heuristic = cadcells::representation::BIGGEST_CELL_COVERING_MIN_TDEG;
+    static constexpr covering_ng::SamplingAlgorithm sampling_algorithm = covering_ng::SamplingAlgorithm::LOWER_UPPER_BETWEEN_SAMPLING_AVOID_RAN;
 
-	// Implicant computation
+    // Implicant computation
     struct formula_evaluation {
         using Type = covering_ng::formula::GraphEvaluation;
         static auto create(cadcells::datastructures::Projections& proj) {
-            return Type(std::bind_front(covering_ng::formula::complexity::sotd, proj), 1, covering_ng::formula::complexity::min_tdeg, false, true, false, Type::PROPAGATION);
+            auto fe_implicant_ordering = covering_ng::formula::complexity::sotd;
+            std::size_t fe_results = 1;
+            auto fe_constraint_ordering = covering_ng::formula::complexity::min_tdeg;
+            bool fe_stop_evaluation_on_conflict = false;
+            bool fe_preprocess = true;
+            bool fe_postprocess = false;
+            auto fe_boolean_exploration = covering_ng::formula::GraphEvaluation::PROPAGATION;
+            return Type(proj, fe_implicant_ordering, fe_results, fe_constraint_ordering, fe_stop_evaluation_on_conflict, fe_preprocess, fe_postprocess, fe_boolean_exploration);
         }
     };
 };
