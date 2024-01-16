1ab89682bb878c69c5612e11f1120c536cc056da
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2024-01-16 16:23:14 +0100

    fix approximation

--- src/smtrat-cadcells/representation/heuristics_approximation.h
+++ src/smtrat-cadcells/representation/heuristics_approximation.h
@@ -12,29 +12,33 @@ template <>
 struct cell<CellHeuristic::BIGGEST_CELL_APPROXIMATION> {
     template<typename T>
     static datastructures::CellRepresentation<T> compute(datastructures::SampledDerivationRef<T>& der) {
-        bool enable_weak = false;
-
-        approximation::CellApproximator apx(der);
+        bool enable_weak = true;
+        LocalDelMode ldel_mode = LocalDelMode::ALL;
 
         datastructures::CellRepresentation<T> response(der);
+        datastructures::Delineation reduced_delineation = der->delin();
+        if (ldel_mode == LocalDelMode::ONLY_INDEPENDENT) {
+            handle_local_del_simplify_non_independent(reduced_delineation);
+        } else if (ldel_mode == LocalDelMode::SIMPLIFY) {
+            handle_local_del_simplify_all(reduced_delineation);
+        }
+        auto reduced_cell = reduced_delineation.delineate_cell(der->main_var_sample());
+
+        approximation::CellApproximator apx(der);
         response.description = apx.compute_cell();
 
+        response.ordering.biggest_cell_wrt = response.description;
         if (der->cell().is_section()) {
-            handle_section_all_equational(der, response);
+            handle_local_del_simplify_non_independent(reduced_delineation);
+            handle_local_del(der, reduced_delineation, response);
+            handle_section_all_equational(reduced_delineation, response);
         } else { // sector
-            datastructures::Delineation reduced_delineation = der->delin();
-            auto reduced_cell = reduced_delineation.delineate_cell(der->main_var_sample());
-            for (const auto poly : util::get_local_del_polys(reduced_delineation)) {
-                util::local_del_ordering(der->proj(), poly, der->underlying_sample(), der->main_var_sample(), reduced_delineation, response.description, response.ordering);
-            }
-            util::PolyDelineations poly_delins;
-            util::decompose(reduced_delineation, reduced_cell, poly_delins);
+            handle_local_del(der, reduced_delineation, response);
+            handle_cell_reduction(reduced_delineation, reduced_cell, response);
             util::simplest_biggest_cell_ordering(der->proj(), reduced_delineation, reduced_cell, response.description, response.ordering, enable_weak);
-            for (const auto& poly_delin : poly_delins.data) {
-                chain_ordering(poly_delin.first, poly_delin.second, response.ordering);
-            }
         }
         handle_connectedness(der, response, enable_weak);
+        handle_ordering_polys(der, response);
         return response;
     }
 };
