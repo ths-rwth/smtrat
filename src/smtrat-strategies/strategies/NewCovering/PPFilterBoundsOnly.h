06451a6e36e00c72f3c59a2b066171a498d6d088
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2024-01-16 10:27:21 +0100

    NewCovering strategies

---
+++ src/smtrat-strategies/strategies/NewCovering/PPFilterBoundsOnly.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <smtrat-modules/FPPModule/FPPModule.h>
+#include <smtrat-modules/NewCoveringModule/NewCoveringModule.tpp>
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-solver/Manager.h>
+
+namespace smtrat {
+
+namespace internal {
+struct OpSettings : cadcells::operators::MccallumFilteredSettings {
+	static constexpr DelineationFunction delineation_function = BOUNDS_ONLY;
+    static constexpr bool enable_weak = true;
+};
+
+struct NewCoveringSettings : NewCoveringSettings2 {
+	using op = cadcells::operators::MccallumFiltered<OpSettings>;
+    constexpr static auto covering_heuristic = cadcells::representation::BIGGEST_CELL_COVERING_FILTER;
+};
+} // namespace internal
+
+class NewCovering_PPFilterBoundsOnly : public Manager {
+public:
+	NewCovering_PPFilterBoundsOnly()
+		: Manager() {
+		setStrategy(
+			addBackend<FPPModule<FPPSettings1>>(
+				addBackend<SATModule<SATSettings1>>(
+					addBackend<NewCoveringModule<internal::NewCoveringSettings>>())));
+	}
+};
+} // namespace smtrat
