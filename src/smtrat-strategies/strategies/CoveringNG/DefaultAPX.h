82e6c004db03c063f884c3e010205cc85ed1c39f
Valentin Promies <valentin.promies@rwth-aachen.de>
2024-07-24 09:56:10 +0200

    fix

--- src/smtrat-strategies/strategies/CoveringNG/DefaultAPX.h
+++ src/smtrat-strategies/strategies/CoveringNG/DefaultAPX.h
@@ -8,9 +8,14 @@ namespace smtrat {
 
 namespace internal {
 
+struct mcf_settings : cadcells::operators::MccallumFilteredSettings {
+    static constexpr DelineationFunction delineation_function = NOOP;
+};
+
 struct CoveringNGSettings : CoveringNGSettingsDefault  {
     static constexpr cadcells::representation::CellHeuristic cell_heuristic = cadcells::representation::BIGGEST_CELL_FILTER;
     static constexpr cadcells::representation::CoveringHeuristic covering_heuristic = cadcells::representation::BIGGEST_CELL_APX;
+    using op = cadcells::operators::MccallumFiltered<mcf_settings>;
 };
 
 }
