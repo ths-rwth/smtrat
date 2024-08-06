e1c3ff0fb1013213b96328cf941390282e479a5d
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2024-08-06 13:44:41 +0200

    Pdel strategies

---
+++ src/smtrat-strategies/strategies/MCSAT/OCNewBCincomplete.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+
+// #include "../modules/FPPModule/FPPModule.h"
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-modules/SATModule/SATModule.tpp>
+
+
+namespace smtrat {
+
+namespace internal {
+struct OCSettings : smtrat::mcsat::onecell::BaseSettings {
+	constexpr static bool exploit_strict_constraints = false;
+
+	using cell_heuristic = cadcells::representation::cell_heuristics::BiggestCell;
+	using covering_heuristic = cadcells::representation::covering_heuristics::BiggestCellCovering;
+
+	using op = cadcells::operators::Mccallum<cadcells::operators::MccallumSettings>;
+};
+
+struct SATSettings : smtrat::SATSettingsMCSAT {
+	struct MCSATSettings : mcsat::Base {
+		using ExplanationBackend = mcsat::SequentialExplanation<mcsat::onecell::Explanation<OCSettings>,
+																mcsat::nlsat::Explanation>;
+	};
+};
+} // namespace internal
+
+class OCNewBCincomplete : public Manager {
+public:
+	OCNewBCincomplete() : Manager() {
+		setStrategy(addBackend<SATModule<internal::SATSettings>>());
+	}
+};
+
+} // namespace smtrat
