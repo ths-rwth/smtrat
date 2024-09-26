2c43db7bdead82a8ce3c31c01dbbff044ca12bf8
Valentin Promies <valentin.promies@rwth-aachen.de>
2024-09-26 10:44:06 +0200

    add strategy

---
+++ src/smtrat-strategies/strategies/Approximation/Simple2.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+
+#include <smtrat-modules/FPPModule/FPPModule.h>
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-modules/SATModule/SATModule.tpp>
+#include <smtrat-modules/STropModule/STropModule.h>
+
+#include "common.h"
+
+namespace smtrat {
+
+namespace internal {
+
+struct ApxSettings {
+    using method = apx::Simple<apx::SimpleSettings>;
+    struct CriteriaSettings : apx::BaseCriteriaSettings {
+        static constexpr std::size_t approximated_cells_limit = 10;
+        static constexpr std::size_t single_degree_threshold  = 5;
+        static constexpr std::size_t pair_degree_threshold    = 5;
+
+        static constexpr bool crit_pair_degree_enabled        = true;
+        static constexpr bool crit_side_enabled               = true;
+    };
+    using Criteria = apx::Criteria<CriteriaSettings>;
+};
+
+struct OCSettings : smtrat::strategies::approximation::BaseOCSettings {
+    using Criteria = ApxSettings::Criteria;
+	using cell_apx_heuristic = cadcells::representation::cell_heuristics::BiggestCellApproximation<ApxSettings>;
+};
+
+} // namespace internal
+
+class Approximation_Simple : public Manager {
+public:
+	Approximation_Simple() : Manager() {
+        setStrategy(
+            addBackend<FPPModule<FPPSettings1>>({
+                addBackend<STropModule<STropSettings3>>({
+                    addBackend<SATModule<strategies::approximation::SATSettings<internal::OCSettings>>>()
+                })
+            })
+        );
+	}
+};
+
+} // namespace smtrat
