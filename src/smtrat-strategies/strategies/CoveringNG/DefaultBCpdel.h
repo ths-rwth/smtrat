8082612f443cc36408d43007744d2e9916a6425c
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2025-02-05 16:09:16 +0100

    cng pdel

---
+++ src/smtrat-strategies/strategies/CoveringNG/DefaultBCpdel.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <smtrat-modules/CoveringNGModule/CoveringNGModule.h>
+#include <smtrat-modules/CoveringNGModule/CoveringNGModule.tpp>
+#include <smtrat-solver/Manager.h>
+
+namespace smtrat {
+
+namespace internal {
+
+struct CoveringNGSettings : CoveringNGSettingsDefault  {
+    using cell_heuristic = cadcells::representation::cell_heuristics::BiggestCellPdel;
+    using covering_heuristic = cadcells::representation::covering_heuristics::BiggestCellCoveringPdel;
+};
+
+}
+
+class CoveringNG_DefaultBCpdel: public Manager {
+public:
+	CoveringNG_DefaultBCpdel() : Manager() {
+		setStrategy(
+            addBackend<CoveringNGModule<internal::CoveringNGSettings>>()
+        );
+	}
+};
+} // namespace smtrat
