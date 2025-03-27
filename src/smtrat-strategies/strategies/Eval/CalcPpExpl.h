d3d5b50da86360e36e0168fb76b035389546c3d3
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2025-03-27 12:19:38 +0100

    fix

--- src/smtrat-strategies/strategies/Eval/CalcPpExpl.h
+++ src/smtrat-strategies/strategies/Eval/CalcPpExpl.h
@@ -27,7 +27,7 @@ struct CoveringNGSettings : CoveringNGSettingsDefault  {
             bool fe_stop_evaluation_on_conflict = false;
             bool fe_preprocess = true;
             bool fe_postprocess = false;
-            auto fe_boolean_exploration = covering_ng::formula::GraphEvaluation::Exploration;
+            auto fe_boolean_exploration = covering_ng::formula::GraphEvaluation::EXPLORATION;
             return Type(proj, fe_implicant_ordering, fe_results, fe_constraint_ordering, fe_stop_evaluation_on_conflict, fe_preprocess, fe_postprocess, fe_boolean_exploration);
         }
     };
--- src/smtrat-strategies/strategies/Eval/CalcPpRevsotd.h
+++ src/smtrat-strategies/strategies/Eval/CalcPpRevsotd.h
@@ -27,7 +27,7 @@ struct CoveringNGSettings : CoveringNGSettingsDefault  {
             bool fe_stop_evaluation_on_conflict = false;
             bool fe_preprocess = true;
             bool fe_postprocess = false;
-            auto fe_boolean_exploration = covering_ng::formula::GraphEvaluation::Evaluation;
+            auto fe_boolean_exploration = covering_ng::formula::GraphEvaluation::PROPAGATION;
             return Type(proj, fe_implicant_ordering, fe_results, fe_constraint_ordering, fe_stop_evaluation_on_conflict, fe_preprocess, fe_postprocess, fe_boolean_exploration);
         }
     };
