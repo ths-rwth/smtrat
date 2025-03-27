2966ba10c639dcac980cdf9c17ad898a93c0b76b
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2025-03-27 11:08:32 +0100

    more strategies

---
+++ src/smtrat-strategies/strategies/Eval/CalcPpRevsotd.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+
+#include <smtrat-modules/FPPModule/FPPModule.h>
+#include <smtrat-modules/CoveringNGModule/CoveringNGModule.h>
+#include <smtrat-modules/CoveringNGModule/CoveringNGModule.tpp>
+#include <smtrat-cadcells/operators/operator_mccallum_unified.h>
+
+
+namespace smtrat {
+
+namespace internal {
+struct CoveringNGSettings : CoveringNGSettingsDefault  {
+	using cell_heuristic = cadcells::representation::cell_heuristics::BiggestCellFilter;
+	using covering_heuristic = cadcells::representation::covering_heuristics::BiggestCellCoveringFilter;
+	using op = cadcells::operators::MccallumUnified<cadcells::operators::MccallumUnifiedSettingsComplete>;
+	static constexpr covering_ng::variables::VariableOrderingHeuristics variable_ordering_heuristic = covering_ng::variables::VariableOrderingHeuristics::GreedyMaxUnivariate;
+	static constexpr covering_ng::SamplingAlgorithm sampling_algorithm = covering_ng::SamplingAlgorithm::SIZE_SAMPLING;
+
+	struct formula_evaluation {
+        using Type = covering_ng::formula::GraphEvaluation;
+        static auto create(cadcells::datastructures::Projections& proj) {
+            auto fe_implicant_ordering = covering_ng::formula::complexity::sotd_reverse;
+            std::size_t fe_results = 1;
+            auto fe_constraint_ordering = covering_ng::formula::complexity::min_tdeg;
+            bool fe_stop_evaluation_on_conflict = false;
+            bool fe_preprocess = true;
+            bool fe_postprocess = false;
+            auto fe_boolean_exploration = covering_ng::formula::GraphEvaluation::Evaluation;
+            return Type(proj, fe_implicant_ordering, fe_results, fe_constraint_ordering, fe_stop_evaluation_on_conflict, fe_preprocess, fe_postprocess, fe_boolean_exploration);
+        }
+    };
+};
+}
+
+class Eval_CalcPpRevsotd : public Manager {
+public:
+	Eval_CalcPpRevsotd() : Manager() {
+		setStrategy(
+			addBackend<FPPModule<FPPSettings1>>({
+                addBackend<CoveringNGModule<internal::CoveringNGSettings>>()
+            })
+        );
+	}
+};
+
+} // namespace smtrat
