5e7bb1df63322702904b558ca0f3b83f9348f1e4
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2025-04-08 11:16:05 +0200

    mcsatavsids

---
+++ src/smtrat-strategies/strategies/Eval/McsatVsids.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <smtrat-solver/Manager.h>
+
+
+#include <smtrat-modules/SATModule/SATModule.h>
+#include <smtrat-modules/SATModule/SATModule.tpp>
+#include <smtrat-cadcells/operators/operator_mccallum_unified.h>
+
+
+namespace smtrat {
+
+namespace internal {
+struct OCSettings : smtrat::mcsat::onecell::BaseSettings {
+	constexpr static bool exploit_strict_constraints = false;
+	static constexpr bool enforce_tarski = false;
+    constexpr static bool use_approximation = false;
+	using cell_heuristic = cadcells::representation::cell_heuristics::BiggestCellFilter;
+	using covering_heuristic = cadcells::representation::covering_heuristics::BiggestCellCoveringFilter;
+	using op = cadcells::operators::MccallumUnified<cadcells::operators::MccallumUnifiedSettingsComplete>;
+};
+
+struct SATSettings : smtrat::SATSettingsMCSAT {
+	struct MCSATSettings : mcsat::Base {
+		using ExplanationBackend = mcsat::onecell::Explanation<OCSettings>;
+	};
+	using VarScheduler = VarSchedulerMinisat;
+};
+} // namespace internal
+
+class Eval_McsatVsids : public Manager {
+public:
+	Eval_McsatVsids() : Manager() {
+		setStrategy(
+			
+				addBackend<SATModule<internal::SATSettings>>()
+			
+		);
+	}
+};
+
+} // namespace smtrat
